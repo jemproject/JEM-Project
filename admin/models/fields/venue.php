2b282bc7e51010d85a4366914147dd9d5a1c414a
JKoelman <jaapkoelman@hotmail.com>
2013-04-05 20:33:21 +0200

    altered selectbox for venue
    altered the selectbox for specific venue ( venueevents view )..

--- admin/models/fields/venue.php
+++ admin/models/fields/venue.php
@@ -49,29 +49,83 @@ class JFormFieldVenue extends JFormFieldList
         protected $type = 'Venue';
  
         /**
-         * Method to get a list of options for a list input.
-         *
-         * @return      array           An array of JHtml options.
-         */
-        protected function getOptions() 
-        {
-                $db = JFactory::getDBO();
-                $query = $db->getQuery(true);
-                $query->select('id,venue');
-                $query->from('#__eventlist_venues');
-                $db->setQuery((string)$query);
-                $venues = $db->loadObjectList();
-                $options = array();
-                if ($venues)
-                {
-                        foreach($venues as $venue) 
-                        {
-                                $options[] = JHtml::_('select.option', $venue->id, $venue->venue);
-                        }
-                }
-                $options = array_merge(parent::getOptions(), $options);
-                return $options;
-        }
-}
+	 * Method to get the field input markup.
+	 *
+	 * @return	string	The field input markup.
+	 * @since	1.6
+	 */
+	protected function getInput()
+	{
+		// Load the modal behavior script.
+		JHtml::_('behavior.modal', 'a.modal');
 
+		// Build the script.
+		$script = array();
+		$script[] = '	function elSelectVenue(id, venue, object) {';
+		$script[] = '		document.getElementById("'.$this->id.'_id").value = id;';
+		$script[] = '		document.getElementById("'.$this->id.'_name").value = venue;';
+		$script[] = '		SqueezeBox.close();';
+		$script[] = '	}';
+
+		// Add the script to the document head.
+		JFactory::getDocument()->addScriptDeclaration(implode("\n", $script));
+
+
+		// Setup variables for display.
+		$html	= array();
+		$link	= 'index.php?option=com_eventlist&amp;view=venueelement&amp;tmpl=component&amp;object='.$this->id;
+
+		$db	= JFactory::getDBO();
+		$db->setQuery(
+			'SELECT venue' .
+			' FROM #__eventlist_venues' .
+			' WHERE id = '.(int) $this->value
+		);
+		$title = $db->loadResult();
+
+		if ($error = $db->getErrorMsg()) {
+			JError::raiseWarning(500, $error);
+		}
+
+		if (empty($title)) {
+			$title = JText::_('COM_EVENTLIST_SELECT_VENUE');
+		}
+		$title = htmlspecialchars($title, ENT_QUOTES, 'UTF-8');
+
+		// The current user display field.
+		$html[] = '<div class="fltlft">';
+		$html[] = '  <input type="text" id="'.$this->id.'_name" value="'.$title.'" disabled="disabled" size="35" />';
+		$html[] = '</div>';
+
+		// The user select button.
+		$html[] = '<div class="button2-left">';
+		$html[] = '  <div class="blank">';
+		$html[] = '	<a class="modal" title="'.JText::_('COM_EVENTLIST_SELECT_VENUE').'"  href="'.$link.'&amp;'.JSession::getFormToken().'=1" rel="{handler: \'iframe\', size: {x: 800, y: 450}}">'.JText::_('COM_EVENTLIST_SELECT_VENUE').'</a>';
+		$html[] = '  </div>';
+		$html[] = '</div>';
+
+		// The active article id field.
+		if (0 == (int)$this->value) {
+			$value = '';
+		} else {
+			$value = (int)$this->value;
+		}
+
+		// class='required' for client side validation
+		$class = '';
+		if ($this->required) {
+			$class = ' class="required modal-value"';
+		}
+
+		$html[] = '<input type="hidden" id="'.$this->id.'_id"'.$class.' name="'.$this->name.'" value="'.$value.'" />';
+
+		return implode("\n", $html);
+	}
+        
+        
+        
+	
+	
+}	
+	
 ?>
