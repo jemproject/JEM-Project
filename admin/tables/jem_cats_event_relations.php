427a086d4eb48e514e2f549bbe93c3b1dd44b123
Jaap Koelman <jaapkoelman@hotmail.com>
2013-04-18 21:49:50 +0200

    Adding cats_events import option to backend
    The cats_events_import is added to backend, import.  Related to Issue
    #60

--- admin/tables/jem_cats_event_relations.php
+++ admin/tables/jem_cats_event_relations.php
@@ -54,7 +54,72 @@ class jem_cats_event_relations extends JTable
 	// overloaded check function
 	function check()
 	{
-		return;
+		return true;
 	}
+	
+		  /**
+   * try to insert first, update if fails
+   *
+   * Can be overloaded/supplemented by the child class
+   *
+   * @access public
+   * @param boolean If false, null object variables are not updated
+   * @return null|string null if successful otherwise returns and error message
+   */
+  function insertIgnore( $updateNulls=false )
+  {
+    $k = $this->_tbl_key;
+
+    $ret = $this->_insertIgnoreObject( $this->_tbl, $this, $this->_tbl_key );
+    if( !$ret )
+    {
+      $this->setError(get_class( $this ).'::store failed - '.$this->_db->getErrorMsg());
+      return false;
+    }
+    return true;
+  }
+
+  /**
+   * Inserts a row into a table based on an objects properties, ignore if already exists
+   *
+   * @access  public
+   * @param string  The name of the table
+   * @param object  An object whose properties match table fields
+   * @param string  The name of the primary key. If provided the object property is updated.
+   * @return int number of affected row
+   */
+  function _insertIgnoreObject( $table, &$object, $keyName = NULL )
+  {
+    $fmtsql = 'INSERT IGNORE INTO '.$this->_db->nameQuote($table).' ( %s ) VALUES ( %s ) ';
+    $fields = array();
+    foreach (get_object_vars( $object ) as $k => $v) {
+      if (is_array($v) or is_object($v) or $v === NULL) {
+        continue;
+      }
+      if ($k[0] == '_') { // internal field
+        continue;
+      }
+      $fields[] = $this->_db->nameQuote( $k );
+      $values[] = $this->_db->isQuoted( $k ) ? $this->_db->Quote( $v ) : (int) $v;
+    }
+    $this->_db->setQuery( sprintf( $fmtsql, implode( ",", $fields ) ,  implode( ",", $values ) ) );
+    if (!$this->_db->query()) {
+      return false;
+    }
+    $id = $this->_db->insertid();
+    if ($keyName && $id) {
+      $object->$keyName = $id;
+    }
+    return $this->_db->getAffectedRows();
+  }
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
 ?>
