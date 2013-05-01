jemproject/JEM-Project

commit 518a7d7a70f21eea930c7e5d04eddd6cc3681533

Refactored some deprecated code. Some methods in Joomla are deprecated and will be removed in Joomla 3.0

admin/models/import.php
@@ -86,7 +86,7 @@ class JEMModelImport extends JModelLegacy {
 	 */
 	private function getFields($tablename) {
 		$tables = array ($tablename);
-		$tablesfields = $this->_db->getTableFields($tables);
+		$tablesfields = $this->_db->getTableColumns($tables);
 		
 		return array_keys($tablesfields[$tablename]);
 	}

admin/admin.jem.php
@@ -1,56 +1,0 @@
-<?php
-/**
- * @version 1.1 $Id$
- * @package JEM
- * @copyright (C) 2013-2013 joomlaeventmanager.net
- * @copyright (C) 2005-2009 Christoph Lukes
- * @license GNU/GPL, see LICENSE.php
- 
- * JEM is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License 2
- * as published by the Free Software Foundation.
- *
- * JEM is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with JEM; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- */
-
-defined( '_JEXEC' ) or die;
-
-//Require classes
-require_once (JPATH_COMPONENT_SITE.'/helpers/helper.php');
-require_once (JPATH_COMPONENT_SITE.'/helpers/countries.php');
-require_once (JPATH_COMPONENT_SITE.'/classes/image.class.php');
-require_once (JPATH_COMPONENT_SITE.'/classes/attachment.class.php');
-require_once (JPATH_COMPONENT_SITE.'/classes/categories.class.php');
-require_once (JPATH_COMPONENT_ADMINISTRATOR.'/classes/admin.class.php');
-
-// Set the table directory
-JTable::addIncludePath(JPATH_COMPONENT.'/tables');
-
-// Require the base controller
-require_once (JPATH_COMPONENT.'/controller.php');
-
-// Require specific controller if requested
-if( $controller = JRequest::getWord('controller') ) {
-	$path = JPATH_COMPONENT.'/controllers/'.$controller.'.php';
-	if (file_exists($path)) {
-		require_once $path;
-	} else {
-		$controller = '';
-	}
-}
-
-//Create the controller
-$classname  = 'JEMController'.$controller;
-$controller = new $classname( );
-
-// Perform the Request task
-$controller->execute( JRequest::getWord('task'));
-$controller->redirect();
-?>

admin/jem.php
@@ -1,0 +1,56 @@
+<?php
+/**
+ * @version 1.1 $Id$
+ * @package JEM
+ * @copyright (C) 2013-2013 joomlaeventmanager.net
+ * @copyright (C) 2005-2009 Christoph Lukes
+ * @license GNU/GPL, see LICENSE.php
+ 
+ * JEM is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 2
+ * as published by the Free Software Foundation.
+ *
+ * JEM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with JEM; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+defined( '_JEXEC' ) or die;
+
+//Require classes
+require_once (JPATH_COMPONENT_SITE.'/helpers/helper.php');
+require_once (JPATH_COMPONENT_SITE.'/helpers/countries.php');
+require_once (JPATH_COMPONENT_SITE.'/classes/image.class.php');
+require_once (JPATH_COMPONENT_SITE.'/classes/attachment.class.php');
+require_once (JPATH_COMPONENT_SITE.'/classes/categories.class.php');
+require_once (JPATH_COMPONENT_ADMINISTRATOR.'/classes/admin.class.php');
+
+// Set the table directory
+JTable::addIncludePath(JPATH_COMPONENT.'/tables');
+
+// Require the base controller
+require_once (JPATH_COMPONENT.'/controller.php');
+
+// Require specific controller if requested
+if( $controller = JRequest::getWord('controller') ) {
+	$path = JPATH_COMPONENT.'/controllers/'.$controller.'.php';
+	if (file_exists($path)) {
+		require_once $path;
+	} else {
+		$controller = '';
+	}
+}
+
+//Create the controller
+$classname  = 'JEMController'.$controller;
+$controller = new $classname( );
+
+// Perform the Request task
+$controller->execute( JRequest::getWord('task'));
+$controller->redirect();
+?>

admin/models/archive.php
@@ -218,24 +218,24 @@ class JEMModelArchive extends JModelLegacy
 
 		$filter 			= $app->getUserStateFromRequest( 'com_jem.archive.filter', 'filter', '', 'int' );
 		$search 			= $app->getUserStateFromRequest( 'com_jem.archive.search', 'search', '', 'string' );
-		$search 			= $this->_db->getEscaped( trim(JString::strtolower( $search ) ) );
+		$search 			= $this->_db->escape( trim(JString::strtolower( $search ) ) );
 
 		$where = array('a.published 	= -1',);
 
 		if ($search && $filter == 1) {
-			$where[] = ' LOWER(a.title) LIKE \'%'.$this->_db->getEscaped($search).'%\' ';
+			$where[] = ' LOWER(a.title) LIKE \'%'.$this->_db->escape($search).'%\' ';
 		}
 
 		if ($search && $filter == 2) {
-			$where[] = ' LOWER(loc.venue) LIKE \'%'.$this->_db->getEscaped($search).'%\' ';
+			$where[] = ' LOWER(loc.venue) LIKE \'%'.$this->_db->escape($search).'%\' ';
 		}
 
 		if ($search && $filter == 3) {
-			$where[] = ' LOWER(loc.city) LIKE \'%'.$this->_db->getEscaped($search).'%\' ';
+			$where[] = ' LOWER(loc.city) LIKE \'%'.$this->_db->escape($search).'%\' ';
 		}
 /*
 		if ($search && $filter == 4) {
-			$where[] = ' LOWER(cat.catname) LIKE \'%'.$this->_db->getEscaped($search).'%\' ';
+			$where[] = ' LOWER(cat.catname) LIKE \'%'.$this->_db->escape($search).'%\' ';
 		}
 */
 

admin/models/attendees.php
@@ -226,7 +226,7 @@ class JEMModelAttendees extends JModelLegacy
 
 		$filter 			= $app->getUserStateFromRequest( 'com_jem.attendees.filter', 'filter', '', 'int' );
 		$search 			= $app->getUserStateFromRequest( 'com_jem.attendees.search', 'search', '', 'string' );
-		$search 			= $this->_db->getEscaped( trim(JString::strtolower( $search ) ) );
+		$search 			= $this->_db->escape( trim(JString::strtolower( $search ) ) );
 		$filter_waiting	= $app->getUserStateFromRequest( 'com_jem.attendees.waiting',	'filter_waiting',	0, 'int' );
 
 		$where = array();

admin/models/categories.php
@@ -111,7 +111,7 @@ class JEMModelCategories extends JModelLegacy
 		$filter_order_Dir	= $app->getUserStateFromRequest( 'com_jem.categories.filter_order_Dir',	'filter_order_Dir',	'', 'word' );
 		$filter_state 		= $app->getUserStateFromRequest( 'com_jem.categories.filter_state', 'filter_state', '', 'word' );
 		$search 			= $app->getUserStateFromRequest( 'com_jem.categories.search', 'search', '', 'string' );
-		$search 			= $this->_db->getEscaped( trim(JString::strtolower( $search ) ) );
+		$search 			= $this->_db->escape( trim(JString::strtolower( $search ) ) );
 		
 		 
 		$filter_order		= JFilterInput::getInstance()->clean($filter_order, 'cmd');
@@ -137,11 +137,11 @@ class JEMModelCategories extends JModelLegacy
 			
 			$query = 'SELECT c.id'
 					. ' FROM #__jem_categories AS c'
-					. ' WHERE LOWER(c.catname) LIKE '.$this->_db->Quote( '%'.$this->_db->getEscaped( $search, true ).'%', false )
+					. ' WHERE LOWER(c.catname) LIKE '.$this->_db->Quote( '%'.$this->_db->escape( $search, true ).'%', false )
 					. $where
 					;
 			$this->_db->setQuery( $query );
-			$search_rows = $this->_db->loadResultArray();					
+			$search_rows = $this->_db->loadColumn();
 		}
 		
 		$query = 'SELECT c.*, c.catname AS name, c.parent_id AS parent, u.name AS editor, g.title AS groupname, gr.name AS catgroup'

admin/models/categoryelement.php
@@ -94,7 +94,7 @@ class JEMModelCategoryelement extends JModelLegacy
 		$filter_order_Dir	= $app->getUserStateFromRequest( 'com_jem.categoryelement.filter_order_Dir',	'filter_order_Dir',	'', 'word' );
 		$filter_state 		= $app->getUserStateFromRequest( 'com_jem.categoryelement.filter_state', 'filter_state', '', 'word' );
 		$search 			= $app->getUserStateFromRequest( 'com_jem.categoryelement.search', 'search', '', 'string' );
-		$search 			= $this->_db->getEscaped( trim(JString::strtolower( $search ) ) );
+		$search 			= $this->_db->escape( trim(JString::strtolower( $search ) ) );
 		
 		$filter_order		= JFilterInput::getinstance()->clean($filter_order, 'cmd');
 		$filter_order_Dir	= JFilterInput::getinstance()->clean($filter_order_Dir, 'word');
@@ -119,11 +119,11 @@ class JEMModelCategoryelement extends JModelLegacy
 			
 			$query = 'SELECT c.id'
 					. ' FROM #__jem_categories AS c'
-					. ' WHERE LOWER(c.catname) LIKE '.$this->_db->Quote( '%'.$this->_db->getEscaped( $search, true ).'%', false )
+					. ' WHERE LOWER(c.catname) LIKE '.$this->_db->Quote( '%'.$this->_db->escape( $search, true ).'%', false )
 					. $where
 					;
 			$this->_db->setQuery( $query );
-			$search_rows = $this->_db->loadResultArray();					
+			$search_rows = $this->_db->loadColumn();
 		}
 		
 		$query = 'SELECT c.*, u.name AS editor, g.title AS groupname, gr.name AS catgroup'

admin/models/cleanup.php
@@ -186,7 +186,7 @@ class JEMModelCleanup extends JModelLegacy
 
 		$this->_db->setQuery($query);
 
-		$this->_assigned = $this->_db->loadResultArray();
+		$this->_assigned = $this->_db->loadColumn();
 
 		return $this->_assigned;
 	}

commit 317424778c2e63317c54cecad406989302c90eda

Fixed issue #107 Bug was introduced in commit 518a7d7a70f21eea930c7e5d04eddd6cc3681533

admin/models/import.php
@@ -5,7 +5,7 @@
  * @copyright (C) 2013-2013 joomlaeventmanager.net
  * @copyright (C) 2005-2009 Christoph Lukes
  * @license GNU/GPL, see LICENSE.php
- 
+ *
  * JEM is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License 2
  * as published by the Free Software Foundation.
@@ -20,7 +20,6 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
-// no direct access
 defined('_JEXEC') or die;
 
 jimport('joomla.application.component.model');
@@ -43,7 +42,7 @@ class JEMModelImport extends JModelLegacy {
 
 	/**
 	 * Get the table fields of the events table
-	 * 
+	 *
 	 * @return  array  An array with the fields of the events table
 	 */
 	function getEventFields() {
@@ -52,16 +51,16 @@ class JEMModelImport extends JModelLegacy {
 
 	/**
 	* Get the table fields of the venues table
-	* 
+	*
 	* @return  array  An array with the fields of the venues table
-	*/  
+	*/
 	function getVenueFields() {
 		return $this->getFields('#__jem_venues');
 	}
 
 	/**
 	 * Get the table fields of the categories table
-	 * 
+	 *
 	 * @return  array  An array with the fields of the categories table
 	 */
 	function getCategoryFields() {
@@ -70,25 +69,22 @@ class JEMModelImport extends JModelLegacy {
 
 	/**
 	 * Get the table fields of the cats_event_relations table
-	 * 
+	 *
 	 * @return  array  An array with the fields of the cats_event_relations table
 	 */
 	function getCateventsFields() {
 		return $this->getFields('#__jem_cats_event_relations');
 	}
-	
+
 	/**
 	 * Helper function to return table fields of a given table
-	 * 
+	 *
 	 * @param   string  $tablename  The name of the table we want to get fields from
-	 * 
+	 *
 	 * @return  array  An array with the fields of the table
 	 */
 	private function getFields($tablename) {
-		$tables = array ($tablename);
-		$tablesfields = $this->_db->getTableColumns($tables);
-		
-		return array_keys($tablesfields[$tablename]);
+		return array_keys($this->_db->getTableColumns($tablename));
 	}
 
 	/**
@@ -277,7 +273,7 @@ class JEMModelImport extends JModelLegacy {
 				}
 			}
 		}
-	
+
 		return $rec;
 	}
 }