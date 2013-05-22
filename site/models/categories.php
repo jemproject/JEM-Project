f7554a8fbac153b295ceac44492c929f3b784ddd
diesl <meine.anmeldungen.github@online.de>
2013-05-22 17:29:38 +0200

    Show selected category in category view too and not only the subcategories. (#154)

--- site/models/categories.php
+++ site/models/categories.php
@@ -5,7 +5,7 @@
  * @copyright (C) 2013-2013 joomlaeventmanager.net
  * @copyright (C) 2005-2009 Christoph Lukes
  * @license GNU/GPL, see LICENSE.php
- 
+ *
  * JEM is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License 2
  * as published by the Free Software Foundation.
@@ -20,7 +20,6 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
-// no direct access
 defined('_JEXEC') or die;
 
 jimport('joomla.application.component.model');
@@ -34,12 +33,12 @@ jimport('joomla.application.component.model');
 class JEMModelCategories extends JModelLegacy
 {
 	/**
-   	 * Top category id
-   	 *
-   	 * @var int
-   	 */
-  	var $_id = 0;
-  
+	 * Top category id
+	 *
+	 * @var int
+	 */
+	var $_id = 0;
+
 	/**
 	 * Event data array
 	 *
@@ -57,7 +56,7 @@ class JEMModelCategories extends JModelLegacy
 	/**
 	 * Categories data array
 	 *
-	 * @var integer
+	 * @var array
 	 */
 	var $_categories = null;
 
@@ -77,15 +76,15 @@ class JEMModelCategories extends JModelLegacy
 	{
 		parent::__construct();
 
-		$app =  JFactory::getApplication();
+		$app = JFactory::getApplication();
 
 		// Get the paramaters of the active menu item
-		$params 	=  $app->getParams('com_jem');
+		$params = $app->getParams('com_jem');
 
 		//get	the number of events from database
-		$limit			= JRequest::getInt('limit', $params->get('cat_num'));
-		$limitstart		= JRequest::getInt('limitstart');
-		
+		$limit 		= JRequest::getInt('limit', $params->get('cat_num'));
+		$limitstart = JRequest::getInt('limitstart');
+
 		$id = $params->get('catid',0);
 		$this->_id = $id;
 
@@ -99,27 +98,27 @@ class JEMModelCategories extends JModelLegacy
 	 * @access public
 	 * @return array
 	 */
-	function &getData( )
+	function &getData()
 	{
-		$app 		=  JFactory::getApplication();
-		$params 	=  $app->getParams();
-		$jemsettings =  JEMHelper::config();
+		$app = JFactory::getApplication();
+		$params = $app->getParams();
+		$jemsettings = JEMHelper::config();
 
 		// Lets load the content if it doesn't already exist
 		if (empty($this->_categories))
 		{
-			$query = $this->_buildQuery();
-      		$pagination = $this->getPagination();
-      		$this->_categories = $this->_getList( $query, $pagination->limitstart,  $pagination->limit );
+			$parentCategory = $this->_getList($this->_buildQueryParentCategory());
+			$query = $this->_buildQuerySubCategories();
+			$pagination = $this->getPagination();
+			$this->_categories = $this->_getList($query, $pagination->limitstart,  $pagination->limit);
 
-			$k = 0;
-			$count = count($this->_categories);
-			for($i = 0; $i < $count; $i++)
+			// Include parent category itself
+			$this->_categories = array_merge($parentCategory, $this->_categories);
+
+			foreach($this->_categories as $category)
 			{
-				$category = $this->_categories[$i];
-				
-		/*		if ($category->image != '') {
-
+				/*
+				if ($category->image != '') {
 					$attribs['width'] = $jemsettings->imagewidth;
 					$attribs['height'] = $jemsettings->imagehight;
 
@@ -127,47 +126,40 @@ class JEMModelCategories extends JModelLegacy
 				} else {
 					$category->image = JHTML::image('media/com_jem/images/noimage.png', $category->catname);
 				}
-			*/	
-				
-				
-				
-				if ($params->get('usecat',1))
-				{
+				*/
+
+				if ($params->get('usecat',1)) {
 					//child categories
-					$query	= $this->_buildQuery( $category->id );
+					$query = $this->_buildQuerySubCategories($category->id);
 					$this->_db->setQuery($query);
 					$category->subcats = $this->_db->loadObjectList();
-				}
-				else {
+				} else {
 					$category->subcats = array();
 				}
-				
+
 				//Generate description
 				if (empty ($category->catdescription)) {
-					$category->catdescription = JText::_( 'COM_JEM_NO_DESCRIPTION' );
+					$category->catdescription = JText::_('COM_JEM_NO_DESCRIPTION');
 				} else {
 					//execute plugins
-					$category->text		= $category->catdescription;
+					$category->text 	= $category->catdescription;
 					$category->title 	= $category->catname;
 					JPluginHelper::importPlugin('content');
-					$results = $app->triggerEvent( 'onContentPrepare', array( 'com_jem.categories', &$category, &$params, 0 ));
+					$results = $app->triggerEvent('onContentPrepare', array('com_jem.categories', &$category, &$params, 0));
 					$category->catdescription = $category->text;
 				}
-				
+
 				//create target link
-				$task 	= JRequest::getWord('task');
-				
-				$category->linktext = $task == 'archive' ? JText::_( 'COM_JEM_SHOW_ARCHIVE' ) : JText::_( 'COM_JEM_SHOW_EVENTS' );
+				$task = JRequest::getWord('task');
+
+				$category->linktext = $task == 'archive' ? JText::_('COM_JEM_SHOW_ARCHIVE') : JText::_('COM_JEM_SHOW_EVENTS');
 
 				if ($task == 'archive') {
 					$category->linktarget = JRoute::_('index.php?view=categoryevents&id='.$category->slug.'&task=archive');
 				} else {
 					$category->linktarget = JRoute::_('index.php?view=categoryevents&id='.$category->slug);
 				}
-				
-				$k = 1 - $k;
 			}
-
 		}
 
 		return $this->_categories;
@@ -192,71 +184,93 @@ class JEMModelCategories extends JModelLegacy
 	}
 
 	/**
-	 * Method get the categories query
-	 *
-	 * @access private
-	 * @return array
+	 * Method to get the subcategories query
+	 * @param string $parent_id Parent ID of the subcategories
+	 * @return string The query string
 	 */
-	function _buildQuery( $parent_id = null )
+	function _buildQuerySubCategories($parent_id = null) {
+		return $this->_buildQuery($parent_id);
+	}
+
+	/**
+	 * Method to get the parent category query
+	 * @param string $parent_id ID of the parent category
+	 * @return string The query string
+	 */
+	function _buildQueryParentCategory($parent_id = null) {
+		return $this->_buildQuery($parent_id, true);
+	}
+
+
+	/**
+	 * Method get the categories query
+	 * @param string $parent_id
+	 * @param string $parentCategory
+	 * @return string The query string
+	 */
+	function _buildQuery($parent_id = null, $parentCategory = false)
 	{
-		$app =  JFactory::getApplication();
-		
-    	// Get the paramaters of the active menu item
-    	$params   =  $app->getParams('com_jem');
-    
+		$app = JFactory::getApplication();
+
+		// Get the paramaters of the active menu item
+		$params = $app->getParams('com_jem');
+
 		if (is_null($parent_id)) {
 			$parent_id = $this->_id;
 		}
-		
-		$user 		= JFactory::getUser();
-		if (JFactory::getUser()->authorise('core.manage')) {
-           $gid = (int) 3;      //viewlevel Special
-           } else {
-               if($user->get('id')) {
-                   $gid = (int) 2;    //viewlevel Registered
-               } else {
-                   $gid = (int) 1;    //viewlevel Public
-               }
-           }
-		
-		
-		$ordering	= 'c.ordering ASC';
 
-		//build where clause
+		$user = JFactory::getUser();
+		if (JFactory::getUser()->authorise('core.manage')) {
+			$gid = (int) 3;	  //viewlevel Special
+		} else {
+			if($user->get('id')) {
+				$gid = (int) 2;	//viewlevel Registered
+			} else {
+				$gid = (int) 1;	//viewlevel Public
+			}
+		}
+
+		$ordering = 'c.ordering ASC';
+
+		// build where clause
 		$where_sub = ' WHERE cc.published = 1';
-		$where_sub .= ' AND cc.parent_id = '.(int) $parent_id;
+		if($parentCategory) {
+			$where_sub .= ' AND cc.id = '.(int) $parent_id;
+		} else {
+			$where_sub .= ' AND cc.parent_id = '.(int) $parent_id;
+		}
 		$where_sub .= ' AND cc.access <= '.$gid;
-		
-		//check archive task and ensure that only categories get selected if they contain a published/archived event
-		$task 	= JRequest::getWord('task');
+
+		// check archive task and ensure that only categories get selected
+		// if they contain a published/archived event
+		$task = JRequest::getWord('task');
 		if($task == 'archive') {
 			$where_sub .= ' AND i.published = -1';
 		} else {
 			$where_sub .= ' AND i.published = 1';
 		}
 		$where_sub .= ' AND c.id = cc.id';
-		
+
 		// show/hide empty categories
-		$empty 	= null;		
-		if (!$params->get('empty_cat'))
-		{
-			$empty 	= ' HAVING assignedevents > 0';
-		}
-		
+		$empty = $params->get('empty_cat') ? '' : ' HAVING assignedevents > 0';
+
+		// Parent category itself or its sub categories
+		$parentCategoryQuery = $parentCategory ? 'c.id='.(int)$parent_id : 'c.parent_id='.(int)$parent_id;
+
 		$query = 'SELECT c.*,'
-				. ' CASE WHEN CHAR_LENGTH( c.alias ) THEN CONCAT_WS( \':\', c.id, c.alias ) ELSE c.id END AS slug,'
+				. ' CASE WHEN CHAR_LENGTH(c.alias) THEN CONCAT_WS(\':\', c.id, c.alias) ELSE c.id END AS slug,'
 					. ' ('
-					. ' SELECT COUNT( DISTINCT i.id )'
+					. ' SELECT COUNT(DISTINCT i.id)'
 					. ' FROM #__jem_events AS i'
 					. ' LEFT JOIN #__jem_cats_event_relations AS rel ON rel.itemid = i.id'
 					. ' LEFT JOIN #__jem_categories AS cc ON cc.id = rel.catid'
 					. $where_sub
 					. ' GROUP BY cc.id'
-					. ')' 
+					. ')'
 					. ' AS assignedevents'
 				. ' FROM #__jem_categories AS c'
 				. ' WHERE c.published = 1'
-				. ' AND c.parent_id = '.(int)$parent_id
+				. ' AND '.$parentCategoryQuery
 				. ' AND c.access <= '.$gid
 				. ' GROUP BY c.id '.$empty
 				. ' ORDER BY '.$ordering
@@ -264,74 +278,73 @@ class JEMModelCategories extends JModelLegacy
 
 		return $query;
 	}
-	
-  
-  /**
-   * Method to build the Categories query without subselect
-   * That's enough to get the total value.
-   *
-   * @access private
-   * @return string
-   */
-  function _buildQueryTotal()
-  {
-    $app =  JFactory::getApplication();
-	
-    // Get the paramaters of the active menu item
-    $params   =  $app->getParams('com_jem');
-    
-    $user     = JFactory::getUser();
-    if (JFactory::getUser()->authorise('core.manage')) {
-           $gid = (int) 3;      //viewlevel Special
-           } else {
-               if($user->get('id')) {
-                   $gid = (int) 2;    //viewlevel Registered
-               } else {
-                   $gid = (int) 1;    //viewlevel Public
-               }
-           }
-    
-    $query = 'SELECT DISTINCT c.id'
-        . ' FROM #__jem_categories AS c';
-  
-    if (!$params->get('empty_cat', 1))
-    {
-      $query .= ' INNER JOIN #__jem_cats_event_relations AS rel ON rel.catid = c.id '
-              . ' INNER JOIN #__jem_events AS e ON e.id = rel.itemid ';
-    }
-    $query .= ' WHERE c.published = 1'
-        . ' AND c.parent_id = ' . (int) $this->_id
-        . ' AND c.access <= '.$gid
-        ;
-    if (!$params->get('empty_cat', 1))
-    {
-      $task   = JRequest::getWord('task');
-      if($task == 'archive') {
-        $query .= ' AND e.published = -1';
-      } else {
-        $query .= ' AND e.published = 1';
-      }
-    }
 
-    return $query;
-  }
-  
-  
- /**
-   * Method to get a pagination object for the events
-   *
-   * @access public
-   * @return integer
-   */
-  function getPagination()
-  {
-    // Lets load the content if it doesn't already exist
-    if (empty($this->_pagination))
-    {
-      jimport('joomla.html.pagination');
-      $this->_pagination = new JPagination( $this->getTotal(), $this->getState('limitstart'), $this->getState('limit') );
-    }
-    return $this->_pagination;
-  }
+
+	/**
+	 * Method to build the Categories query without subselect
+	 * That's enough to get the total value.
+	 *
+	 * @access private
+	 * @return string
+	 */
+	function _buildQueryTotal()
+	{
+		$app = JFactory::getApplication();
+
+		// Get the paramaters of the active menu item
+		$params = $app->getParams('com_jem');
+
+		$user = JFactory::getUser();
+		if (JFactory::getUser()->authorise('core.manage')) {
+			$gid = (int) 3;	  //viewlevel Special
+		} else {
+			if($user->get('id')) {
+				$gid = (int) 2;	//viewlevel Registered
+			} else {
+				$gid = (int) 1;	//viewlevel Public
+			}
+		}
+
+		$query = 'SELECT DISTINCT c.id'
+			. ' FROM #__jem_categories AS c';
+
+		if (!$params->get('empty_cat', 1))
+		{
+			$query .= ' INNER JOIN #__jem_cats_event_relations AS rel ON rel.catid = c.id '
+					. ' INNER JOIN #__jem_events AS e ON e.id = rel.itemid ';
+		}
+		$query .= ' WHERE c.published = 1'
+			. ' AND c.parent_id = ' . (int) $this->_id
+			. ' AND c.access <= '.$gid
+			;
+		if (!$params->get('empty_cat', 1))
+		{
+			$task = JRequest::getWord('task');
+			if($task == 'archive') {
+				$query .= ' AND e.published = -1';
+			} else {
+				$query .= ' AND e.published = 1';
+			}
+		}
+
+		return $query;
+	}
+
+	/**
+	 * Method to get a pagination object for the events
+	 *
+	 * @access public
+	 * @return integer
+	 */
+	function getPagination()
+	{
+		// Lets load the content if it doesn't already exist
+		if (empty($this->_pagination))
+		{
+			jimport('joomla.html.pagination');
+			$this->_pagination = new JPagination($this->getTotal(), $this->getState('limitstart'), $this->getState('limit'));
+		}
+		return $this->_pagination;
+	}
 }
 ?>
