76544089270f7b330b0a74814622a4ee2547e282
diesl <meine.anmeldungen.github@online.de>
2013-07-14 02:43:49 +0200

    Added missing views to router

--- site/router.php
+++ site/router.php
@@ -57,7 +57,6 @@ function jemParseRoute($segments)
 			if($count > 2) {
 				$vars['task'] = $segments[2];
 			}
-
 		} break;
 
 		case 'details':
@@ -65,7 +64,6 @@ function jemParseRoute($segments)
 			$id = explode(':', $segments[1]);
 			$vars['id'] = $id[0];
 			$vars['view'] = 'details';
-
 		} break;
 
 		case 'venueevents':
@@ -77,20 +75,18 @@ function jemParseRoute($segments)
 			if($count > 2) {
 				$vars['task'] = $segments[2];
 			}
-
 		} break;
 
 		case 'editevent':
 		{
 			$count = count($segments);
-			
+
 			$vars['view'] = 'editevent';
 
 			if($count == 3) {
 				$vars['id'] = $segments[1];
 				$vars['returnid'] = $segments[2];
 			}
-
 		} break;
 
 		case 'editvenue':
@@ -98,39 +94,36 @@ function jemParseRoute($segments)
 			$count = count($segments);
 
 			$vars['view'] = 'editvenue';
-			
+
 			if($count == 3) {
 				$vars['id'] = $segments[1];
 				$vars['returnid'] = $segments[2];
 			}
-
 		} break;
 
 		case 'eventslist':
 		{
 			$vars['view'] = 'eventslist';
-			
+
 			$count = count($segments);
 			if($count == 2) {
 				$vars['task'] = $segments[1];
 			}
-
 		} break;
-				
-    	case 'search':
-    	{
-      		$vars['view'] = 'search';
-    	} break;
+
+		case 'search':
+		{
+			$vars['view'] = 'search';
+		} break;
 
 		case 'categoriesdetailed':
 		{
 			$vars['view'] = 'categoriesdetailed';
-			
+
 			$count = count($segments);
 			if($count == 2) {
 				$vars['task'] = $segments[1];
 			}
-
 		} break;
 
 		case 'categories':
@@ -141,12 +134,11 @@ function jemParseRoute($segments)
 			if($count == 2) {
 				$vars['task'] = $segments[1];
 			}
-
 		} break;
-		
+
 		case 'calendar':
-    	{
-       //     $id = explode(':', $segments[1]);
+		{
+		//	$id = explode(':', $segments[1]);
 		//	$vars['id'] = $id[0];
 			$vars['view'] = 'calendar';
 
@@ -154,66 +146,65 @@ function jemParseRoute($segments)
 			if($count > 2) {
 				$vars['task'] = $segments[2];
 			}
-    	} break;
-   
-
+		} break;
 
 		case 'venues':
 		{
 			$vars['view'] = 'venues';
-			
+
 			$count = count($segments);
 			if($count == 2) {
 				$vars['task'] = $segments[1];
 			}
-
 		} break;
-		
+
 		case 'day':
 		{
 			$vars['view'] = 'day';
-			
+
 			$count = count($segments);
 			if($count == 2) {
 				$vars['id'] = $segments[1];
 			}
-
 		} break;
-		
-		case 'my':
-    	{
-      		$vars['view'] = 'my';
-    	} break;
-    	
-    	case 'myattending':
-    		{
-    			$vars['view'] = 'myattending';
-    		} break;
-    		
-    		case 'attendees':
-    			{
-    				$id = explode(':', $segments[1]);
-    				$vars['id'] = $id[0];
-    				$vars['view'] = 'attendees';
-    				$count = count($segments);
-    				if($count > 2) {
-    					$vars['task'] = $segments[2];
-    				}
-    			} break;
-    	
-    	
-    	
-		
-    // some tasks !
-		case 'getfile': 
-    	{
-      		$vars['task'] = 'getfile';
-    	} break;
 
-    	default:
-    	{
-      		$vars['view'] = $segments[0];
-    	} break;
+		case 'myattending':
+		{
+			$vars['view'] = 'myattending';
+		} break;
+
+		case 'myevents':
+		{
+			$vars['view'] = 'myevents';
+		} break;
+
+		case 'myvenues':
+		{
+			$vars['view'] = 'myvenues';
+		} break;
+
+		case 'attendees':
+		{
+			$id = explode(':', $segments[1]);
+			$vars['id'] = $id[0];
+			$vars['view'] = 'attendees';
+			$count = count($segments);
+			if($count > 2) {
+				$vars['task'] = $segments[2];
+			}
+		} break;
+
+
+		// some tasks !
+		case 'getfile':
+		{
+			$vars['task'] = 'getfile';
+		} break;
+
+		default:
+		{
+			$vars['view'] = $segments[0];
+		} break;
 	}
 
 	return $vars;
