114df53115b0e48c0e5833f5a284313f7e1edb26
Jaap Koelman <jaapkoelman@hotmail.com>
2013-08-08 12:56:48 +0200

    altered sql update-file

--- admin/sql/updates/1.9.1.sql
+++ admin/sql/updates/1.9.1.sql
@@ -1,1 +1,6 @@
-UPDATE `#__jem_settings` SET showtitle = 1-showtitle WHERE id = 1;
+ALTER TABLE `#__jem_venues` ADD publish_upx datetime NOT NULL , ADD publish_downx datetime NOT NULL;
+ALTER TABLE `#__jem_groups` ADD addvenuex int(11) NOT NULL,
+ADD addeventx int(11) NOT NULL,
+ADD publishvenuex int(11) NOT NULL,
+ADD editvenuex int(11) NOT NULL;
+ALTER TABLE `#__jem_settings` ADD fitemidx varchar(5) NOT NULL;
