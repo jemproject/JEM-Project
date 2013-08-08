0593b79102a451ba4f93d3c1417cb3632b9ae4dc
Jaap Koelman <jaapkoelman@hotmail.com>
2013-08-08 13:18:20 +0200

    replaced sql-update with correct version

--- admin/sql/updates/1.9.1.sql
+++ admin/sql/updates/1.9.1.sql
@@ -1,6 +1,6 @@
-ALTER TABLE `#__jem_venues` ADD publish_upx datetime NOT NULL , ADD publish_downx datetime NOT NULL;
-ALTER TABLE `#__jem_groups` ADD addvenuex int(11) NOT NULL,
-ADD addeventx int(11) NOT NULL,
-ADD publishvenuex int(11) NOT NULL,
-ADD editvenuex int(11) NOT NULL;
-ALTER TABLE `#__jem_settings` ADD fitemidx varchar(5) NOT NULL;
+ALTER TABLE `#__jem_venues` ADD publish_up datetime NOT NULL , ADD publish_down datetime NOT NULL;
+ALTER TABLE `#__jem_groups` ADD addvenue int(11) NOT NULL,
+ADD addevent int(11) NOT NULL,
+ADD publishvenue int(11) NOT NULL,
+ADD editvenue int(11) NOT NULL;
+ALTER TABLE `#__jem_settings` ADD fitemid varchar(5) NOT NULL;
