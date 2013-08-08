fd3a3367daa762be70aca1b4067614299366c41b
Jaap Koelman <jaapkoelman@hotmail.com>
2013-08-08 13:35:20 +0200

    removed fitemid from sql-file

--- admin/sql/updates/1.9.1.sql
+++ admin/sql/updates/1.9.1.sql
@@ -3,4 +3,3 @@ ALTER TABLE `#__jem_groups` ADD addvenue int(11) NOT NULL,
 ADD addevent int(11) NOT NULL,
 ADD publishvenue int(11) NOT NULL,
 ADD editvenue int(11) NOT NULL;
-ALTER TABLE `#__jem_settings` ADD fitemid varchar(5) NOT NULL;
