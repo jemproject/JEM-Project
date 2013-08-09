d68b094eb07c802cffa64bec3032576a6acc8442
JKoelman <jaapkoelman@hotmail.com>
2013-08-09 22:13:24 +0200

    altered substr events-view (backend), rel #264
    altered substr events-view (backend)

--- admin/views/events/tmpl/default.php
+++ admin/views/events/tmpl/default.php
@@ -172,7 +172,7 @@ window.addEvent('domready', function(){
 					<br />
 				<?php
 				if (JString::strlen($row->alias) > 25) {
-					echo JString::substr( JText::sprintf('JGLOBAL_LIST_ALIAS', $this->escape($row->alias), 0 , 25)).'...';
+					echo JString::substr( $this->escape($row->alias), 0 , 25).'...';
 				} else {
 					echo $this->escape($row->alias);
 				}
