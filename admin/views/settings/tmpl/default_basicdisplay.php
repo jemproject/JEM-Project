9769a92cabe74fdbb405cd343e166ec4afb7a0cb
JKoelman <jaapkoelman@hotmail.com>
2013-08-17 00:33:04 +0200

    alterations settings-view (backend)

--- admin/views/settings/tmpl/default_basicdisplay.php
+++ admin/views/settings/tmpl/default_basicdisplay.php
@@ -13,14 +13,40 @@ defined('_JEXEC') or die;
 <fieldset class="adminform">
 	<legend><?php echo JText::_( 'COM_JEM_DISPLAY_SETTINGS' ); ?></legend>
 	<ul class="adminformlist">
-			<?php
-			foreach ($this->form->getFieldset('display') as $field):
-			?>
-					<li><?php echo $field->label; ?>
-					<?php echo $field->input; ?></li>
-			<?php
-			endforeach;
-			?>
+
+			<li><?php echo $this->form->getLabel('showdetails'); ?> <?php echo $this->form->getInput('showdetails'); ?>
+			</li>
+
+
+			<li><?php echo $this->form->getLabel('formatShortDate'); ?> <?php echo $this->form->getInput('formatShortDate'); ?>
+			<span class="error hasTip" title="<?php echo JText::_('COM_JEM_PHP_DATE_MANUAL');?>::<?php echo JText::_('COM_JEM_PHP_DATE_MANUAL_DESC'); ?>">
+				<a href="http://php.net/manual/en/function.date.php" target="_blank"><?php echo $this->WarningIcon(); ?></a>
+			</span>
+			</li>
+
+			<li><?php echo $this->form->getLabel('formatdate'); ?> <?php echo $this->form->getInput('formatdate'); ?>
+			<span class="error hasTip" title="<?php echo JText::_('COM_JEM_PHP_DATE_MANUAL');?>::<?php echo JText::_('COM_JEM_PHP_DATE_MANUAL_DESC'); ?>">
+				<a href="http://php.net/manual/en/function.date.php" target="_blank"><?php echo $this->WarningIcon(); ?></a>
+			</span>
+			</li>
+
+			<li><?php echo $this->form->getLabel('formattime'); ?> <?php echo $this->form->getInput('formattime'); ?>
+			<span class="error hasTip" title="<?php echo JText::_('COM_JEM_TIME_STRFTIME');?>::<?php echo JText::_('COM_JEM_TIME_STRFTIME_DESC'); ?>">
+				<a href="http://www.php.net/strftime" target="_blank"><?php echo $this->WarningIcon(); ?></a>
+			</span>
+			</li>
+
+			<li><?php echo $this->form->getLabel('timename'); ?> <?php echo $this->form->getInput('timename'); ?>
+			</li>
+
+			<li><?php echo $this->form->getLabel('storeip'); ?> <?php echo $this->form->getInput('storeip'); ?>
+			</li>
+
+
 	</ul>
 </fieldset>
 </div>
+
+
+
+
