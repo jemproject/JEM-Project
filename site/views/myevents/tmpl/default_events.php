b00275715f58cd6d2a9ecc1c99d87cd52909fab7
JKoelman <jaapkoelman@hotmail.com>
2013-08-18 20:36:15 +0200

    added formtoken to myevents-view

--- site/views/myevents/tmpl/default_events.php
+++ site/views/myevents/tmpl/default_events.php
@@ -270,6 +270,7 @@ defined('_JEXEC') or die;
 		<input type="hidden" name="boxchecked" value="0" />
 		<input type="hidden" name="task" value="" />
 		<input type="hidden" name="option" value="com_jem" />
+		<?php echo JHtml::_('form.token'); ?>
 	</p>
 </form>
 <div class="pagination">
