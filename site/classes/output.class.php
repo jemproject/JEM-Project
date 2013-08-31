14546ce6a1567a6e1751e8be72b787c5254b0f8a
Jaap Koelman <jaapkoelman@hotmail.com>
2013-08-31 12:52:01 +0200

    altered maplink
    alterd maplink to replace the & into +. This as the & signs are causing
    errors.

--- site/classes/output.class.php
+++ site/classes/output.class.php
@@ -546,8 +546,11 @@ class JEMOutput {
 		if ($data->longitude == 0.000000) {
 			$data->longitude = null;
 		}
-		$url1 = 'http://maps.google.'.$jemsettings->tld.'/maps?hl='.$jemsettings->lg.'&q='.str_replace(" ", "+", $data->street).', '.$data->postalCode.' '.str_replace(" ", "+", $data->city).', '.$data->country.'+ ('.$data->venue.')&ie=UTF8&z=15&iwloc=B&output=embed" ';
+		
+		$url1 = 'http://maps.google.'.$jemsettings->tld.'/maps?hl='.$jemsettings->lg.'&q='.str_replace(" ", "+", $data->street).', '.$data->postalCode.' '.str_replace(" ", "+", $data->city).', '.$data->country.'+ ('.mb_ereg_replace("&", "+", $data->venue).')&ie=UTF8&z=15&iwloc=B&output=embed" ';
 
+		//var_dump($url1);exit;
+		
 		//google map link or include
 		switch ($jemsettings->showmapserv)
 		{
