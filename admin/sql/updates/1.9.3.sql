b82cc55b91a3adc13c62f02a70fe193bc2c73ae6
Jaap Koelman <jaapkoelman@hotmail.com>
2013-09-19 20:36:12 +0200

    added custom-venuefields to update sql, rel #323

---
+++ admin/sql/updates/1.9.3.sql
@@ -0,0 +1,11 @@
+ALTER TABLE `#__jem_venues`
+	ADD custom1 varchar(200) NOT NULL DEFAULT '',
+	ADD custom2 varchar(200) NOT NULL DEFAULT '',
+	ADD custom3 varchar(100) NOT NULL DEFAULT '',
+	ADD custom4 varchar(100) NOT NULL DEFAULT '',
+	ADD custom5 varchar(100) NOT NULL DEFAULT '',
+	ADD custom6 varchar(100) NOT NULL DEFAULT '',
+	ADD custom7 varchar(100) NOT NULL DEFAULT '',
+	ADD custom8 varchar(100) NOT NULL DEFAULT '',
+	ADD custom9 varchar(100) NOT NULL DEFAULT '',
+	ADD custom10 varchar(100) NOT NULL DEFAULT '';
