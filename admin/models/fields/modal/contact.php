ea745214cc9ae0e1d563117bca65c6f4bb164d0d
diesl <meine.anmeldungen.github@online.de>
2013-10-26 11:24:08 +0200

    Fixed indentation, added JEM header

--- admin/models/fields/modal/contact.php
+++ admin/models/fields/modal/contact.php
@@ -1,141 +1,118 @@
 <?php
- defined('_JEXEC') or die('Restricted access');
+/**
+ * @version 1.9.5
+ * @package JEM
+ * @copyright (C) 2013-2013 joomlaeventmanager.net
+ * @copyright (C) 2005-2009 Christoph Lukes
+ * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
 
- jimport('joomla.form.formfield');
+defined('_JEXEC') or die;
 
- /**
-  * Book form field class
-  */
- class JFormFieldModal_Contact extends JFormField
- {
-        /**
-         * field type
-         * @var string
-         */
-        protected $type = 'Modal_Contact';
+jimport('joomla.form.formfield');
+
+/**
+ * Contact select
+ */
+class JFormFieldModal_Contact extends JFormField
+{
+	/**
+	 * field type
+	 * @var string
+	 */
+	protected $type = 'Modal_Contact';
 
 
+	/*
+	$linkcsel = 'index.php?option=com_jem&amp;view=contactelement&amp;tmpl=component';
 
-        /*
-        $linkcsel = 'index.php?option=com_jem&amp;view=contactelement&amp;tmpl=component';
-
-        // build venue select js and load the view
-        $js = "
+	// build venue select js and load the view
+	$js = "
 		function elSelectContact(id, contactid) {
 			document.getElementById('a_id2').value = id;
 			document.getElementById('a_name2').value = contactid;
 			window.parent.SqueezeBox.close();
 		}";
 
-        $document->addScriptDeclaration($js);
+	$document->addScriptDeclaration($js);
 
-        $contactselect = "\n<div style=\"float: left;\"><input style=\"background: #ffffff;\" type=\"text\" id=\"a_name2\" value=\"$row->contactname\" disabled=\"disabled\" /></div>";
-        $contactselect .= "<div class=\"button2-left\"><div class=\"blank\"><a class=\"modal\" title=\"".JText::_('COM_JEM_SELECT')."\" href=\"$linkcsel\" rel=\"{handler: 'iframe', size: {x: 650, y: 375}}\">".JText::_('COM_JEM_SELECT')."</a></div></div>\n";
-        $contactselect .= "\n<input type=\"hidden\" id=\"a_id2\" name=\"contactid\" value=\"$row->contactid\" />";
-        $contactselect .= "\n&nbsp;<input class=\"inputbox\" type=\"button\" onclick=\"elSelectContact(0, '".JText::_('COM_JEM_NO_CONTACT')."' );\" value=\"".JText::_('COM_JEM_NO_CONTACT')."\" onblur=\"seo_switch()\" />";
-        */
-
-
+	$contactselect = "\n<div style=\"float: left;\"><input style=\"background: #ffffff;\" type=\"text\" id=\"a_name2\" value=\"$row->contactname\" disabled=\"disabled\" /></div>";
+	$contactselect .= "<div class=\"button2-left\"><div class=\"blank\"><a class=\"modal\" title=\"".JText::_('COM_JEM_SELECT')."\" href=\"$linkcsel\" rel=\"{handler: 'iframe', size: {x: 650, y: 375}}\">".JText::_('COM_JEM_SELECT')."</a></div></div>\n";
+	$contactselect .= "\n<input type=\"hidden\" id=\"a_id2\" name=\"contactid\" value=\"$row->contactid\" />";
+	$contactselect .= "\n&nbsp;<input class=\"inputbox\" type=\"button\" onclick=\"elSelectContact(0, '".JText::_('COM_JEM_NO_CONTACT')."' );\" value=\"".JText::_('COM_JEM_NO_CONTACT')."\" onblur=\"seo_switch()\" />";
+	*/
 
 
+	/**
+	 * Method to get the field input markup
+	 */
+	protected function getInput()
+	{
+		// Load modal behavior
+		JHtml::_('behavior.modal', 'a.modal');
 
- 	/**
-   * Method to get the field input markup
-   */
-  protected function getInput()
-  {
-          // Load modal behavior
-          JHtml::_('behavior.modal', 'a.modal');
+		// Build the script
+		$script = array();
+		$script[] = '    function jSelectContact_'.$this->id.'(id, name, object) {';
+		$script[] = '        document.id("'.$this->id.'_id").value = id;';
+		$script[] = '        document.id("'.$this->id.'_name").value = name;';
+		$script[] = '        SqueezeBox.close();';
+		$script[] = '    }';
 
-          // Build the script
-          $script = array();
-          $script[] = '    function jSelectContact_'.$this->id.'(id, name, object) {';
-          $script[] = '        document.id("'.$this->id.'_id").value = id;';
-          $script[] = '        document.id("'.$this->id.'_name").value = name;';
-          $script[] = '        SqueezeBox.close();';
-          $script[] = '    }';
+		// Add to document head
+		JFactory::getDocument()->addScriptDeclaration(implode("\n", $script));
 
-          // Add to document head
-          JFactory::getDocument()->addScriptDeclaration(implode("\n", $script));
+		// Setup variables for display
+		$html = array();
+		$link = 'index.php?option=com_jem&amp;view=contactelement&amp;tmpl=component&amp;function=jSelectContact_'.$this->id;
 
-          // Setup variables for display
-          $html = array();
+		$db = JFactory::getDbo();
+		$query = $db->getQuery(true);
+		$query->select('name');
+		$query->from('#__contact_details');
+		$query->where('id='.(int)$this->value);
+		$db->setQuery($query);
 
-          $link = 'index.php?option=com_jem&amp;view=contactelement&amp;tmpl=component&amp;function=jSelectContact_'.$this->id;
+		$contactid = $db->loadResult();
 
+		if ($error = $db->getErrorMsg()) {
+			JError::raiseWarning(500, $error);
+		}
 
+		if (empty($contactid)) {
+			$contactid = JText::_('COM_JEM_SELECTCONTACT');
+		}
+		$contactid = htmlspecialchars($contactid, ENT_QUOTES, 'UTF-8');
 
+		// The current contact input field
+		$html[] = '<div class="fltlft">';
+		$html[] = '  <input type="text" id="'.$this->id.'_name" value="'.$contactid.'" disabled="disabled" size="35" />';
+		$html[] = '</div>';
 
-          $db = JFactory::getDbo();
-          $query = $db->getQuery(true);
-          $query->select('name');
-          $query->from('#__contact_details');
-          $query->where('id='.(int)$this->value);
-          $db->setQuery($query);
+		// The contact select button
+		$html[] = '<div class="button2-left">';
+		$html[] = '  <div class="blank">';
+		$html[] = '    <a class="modal" title="'.JText::_('COM_JEM_SELECT').'" href="'.$link.'&amp;'.JSession::getFormToken().'=1" rel="{handler: \'iframe\', size: {x:800, y:450}}">'.
+					JText::_('COM_JEM_SELECT').'</a>';
+		$html[] = '  </div>';
+		$html[] = '</div>';
 
+		// The active contact id field
+		if (0 == (int)$this->value) {
+			$value = '';
+		} else {
+			$value = (int)$this->value;
+		}
 
-          $contactid = $db->loadResult();
+		// class='required' for client side validation
+		$class = '';
+		if ($this->required) {
+			$class = ' class="required modal-value"';
+		}
 
-          if ($error = $db->getErrorMsg()) {
-          	JError::raiseWarning(500, $error);
-          }
+		$html[] = '<input type="hidden" id="'.$this->id.'_id"'.$class.' name="'.$this->name.'" value="'.$value.'" />';
 
-
-
-          if (empty($contactid)) {
-                  $contactid = JText::_('COM_JEM_SELECTCONTACT');
-          }
-          $contactid = htmlspecialchars($contactid, ENT_QUOTES, 'UTF-8');
-
-          // The current book input field
-          $html[] = '<div class="fltlft">';
-          $html[] = '  <input type="text" id="'.$this->id.'_name" value="'.$contactid.'" disabled="disabled" size="35" />';
-          $html[] = '</div>';
-
-          // The book select button
-          $html[] = '<div class="button2-left">';
-          $html[] = '  <div class="blank">';
-          $html[] = '    <a class="modal" title="'.JText::_('COM_JEM_SELECT').'" href="'.$link.'&amp;'.JSession::getFormToken().'=1" rel="{handler: \'iframe\', size: {x:800, y:450}}">'.
-                         JText::_('COM_JEM_SELECT').'</a>';
-          $html[] = '  </div>';
-          $html[] = '</div>';
-
-         // The active book id field
-          if (0 == (int)$this->value) {
-                  $value = '';
-          } else {
-                  $value = (int)$this->value;
-          }
-
-          // class='required' for client side validation
-          $class = '';
-          if ($this->required) {
-                  $class = ' class="required modal-value"';
-          }
-
-          $html[] = '<input type="hidden" id="'.$this->id.'_id"'.$class.' name="'.$this->name.'" value="'.$value.'" />';
-
-         return implode("\n", $html);
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- }
- ?>
+		return implode("\n", $html);
+	}
+}
+?>
--- admin/models/fields/modal/venue.php
+++ admin/models/fields/modal/venue.php
@@ -1,117 +1,98 @@
 <?php
 /**
- * @version 1.9.1
+ * @version 1.9.5
  * @package JEM
  * @copyright (C) 2013-2013 joomlaeventmanager.net
  * @copyright (C) 2005-2009 Christoph Lukes
  * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  */
- defined('_JEXEC') or die;
 
- jimport('joomla.form.formfield');
+defined('_JEXEC') or die;
 
- /**
-  * Venue Select
-  */
- class JFormFieldModal_Venue extends JFormField
- {
-        /**
-         * field type
-         * @var string
-         */
-        protected $type = 'Modal_Venue';
+jimport('joomla.form.formfield');
 
-
- 	/**
-   * Method to get the field input markup
-   */
-  protected function getInput()
-  {
-          // Load modal behavior
-          JHtml::_('behavior.modal', 'a.modal');
-
-          // Build the script
-          $script = array();
-          $script[] = '    function jSelectVenue_'.$this->id.'(id, venue, object) {';
-          $script[] = '        document.id("'.$this->id.'_id").value = id;';
-          $script[] = '        document.id("'.$this->id.'_name").value = venue;';
-          $script[] = '        SqueezeBox.close();';
-          $script[] = '    }';
-
-          // Add to document head
-          JFactory::getDocument()->addScriptDeclaration(implode("\n", $script));
-
-          // Setup variables for display
-          $html = array();
-          $link = 'index.php?option=com_jem&amp;view=venueelement&amp;tmpl=component&amp;function=jSelectVenue_'.$this->id;
-
-          $db = JFactory::getDbo();
-          $query = $db->getQuery(true);
-          $query->select('venue');
-          $query->from('#__jem_venues');
-          $query->where('id='.(int)$this->value);
-          $db->setQuery($query);
-
-          $venue = $db->loadResult();
-
-          if ($error = $db->getErrorMsg()) {
-          	JError::raiseWarning(500, $error);
-          }
-
-          if (empty($venue)) {
-                  $venue = JText::_('COM_JEM_SELECTVENUE');
-          }
-          $venue = htmlspecialchars($venue, ENT_QUOTES, 'UTF-8');
-
-          // The current venue input field
-          $html[] = '<div class="fltlft">';
-          $html[] = '  <input type="text" id="'.$this->id.'_name" value="'.$venue.'" disabled="disabled" size="35" />';
-          $html[] = '</div>';
-
-          // The venue select button
-          $html[] = '<div class="button2-left">';
-          $html[] = '  <div class="blank">';
-          $html[] = '    <a class="modal" title="'.JText::_('COM_JEM_SELECT').'" href="'.$link.'&amp;'.JSession::getFormToken().'=1" rel="{handler: \'iframe\', size: {x:800, y:450}}">'.
-                         JText::_('COM_JEM_SELECT').'</a>';
-          $html[] = '  </div>';
-          $html[] = '</div>';
-
-         // The active venue id field
-          if (0 == (int)$this->value) {
-                  $value = '';
-          } else {
-                  $value = (int)$this->value;
-          }
-
-          // class='required' for client side validation
-          $class = '';
-          if ($this->required) {
-                  $class = ' class="required modal-value"';
-          }
-
-          $html[] = '<input type="hidden" id="'.$this->id.'_id"'.$class.' name="'.$this->name.'" value="'.$value.'" />';
-
-         return implode("\n", $html);
-  }
+/**
+ * Venue Select
+ */
+class JFormFieldModal_Venue extends JFormField
+{
+	/**
+	 * field type
+	 * @var string
+	 */
+	protected $type = 'Modal_Venue';
 
 
+	/**
+	 * Method to get the field input markup
+	 */
+	protected function getInput()
+	{
+		// Load modal behavior
+		JHtml::_('behavior.modal', 'a.modal');
 
+		// Build the script
+		$script = array();
+		$script[] = '    function jSelectVenue_'.$this->id.'(id, venue, object) {';
+		$script[] = '        document.id("'.$this->id.'_id").value = id;';
+		$script[] = '        document.id("'.$this->id.'_name").value = venue;';
+		$script[] = '        SqueezeBox.close();';
+		$script[] = '    }';
 
+		// Add to document head
+		JFactory::getDocument()->addScriptDeclaration(implode("\n", $script));
 
+		// Setup variables for display
+		$html = array();
+		$link = 'index.php?option=com_jem&amp;view=venueelement&amp;tmpl=component&amp;function=jSelectVenue_'.$this->id;
 
+		$db = JFactory::getDbo();
+		$query = $db->getQuery(true);
+		$query->select('venue');
+		$query->from('#__jem_venues');
+		$query->where('id='.(int)$this->value);
+		$db->setQuery($query);
 
+		$venue = $db->loadResult();
 
+		if ($error = $db->getErrorMsg()) {
+			JError::raiseWarning(500, $error);
+		}
 
+		if (empty($venue)) {
+			$venue = JText::_('COM_JEM_SELECTVENUE');
+		}
+		$venue = htmlspecialchars($venue, ENT_QUOTES, 'UTF-8');
 
+		// The current venue input field
+		$html[] = '<div class="fltlft">';
+		$html[] = '  <input type="text" id="'.$this->id.'_name" value="'.$venue.'" disabled="disabled" size="35" />';
+		$html[] = '</div>';
 
+		// The venue select button
+		$html[] = '<div class="button2-left">';
+		$html[] = '  <div class="blank">';
+		$html[] = '    <a class="modal" title="'.JText::_('COM_JEM_SELECT').'" href="'.$link.'&amp;'.JSession::getFormToken().'=1" rel="{handler: \'iframe\', size: {x:800, y:450}}">'.
+					JText::_('COM_JEM_SELECT').'</a>';
+		$html[] = '  </div>';
+		$html[] = '</div>';
 
+		// The active venue id field
+		if (0 == (int)$this->value) {
+			$value = '';
+		} else {
+			$value = (int)$this->value;
+		}
 
+		// class='required' for client side validation
+		$class = '';
+		if ($this->required) {
+			$class = ' class="required modal-value"';
+		}
 
+		$html[] = '<input type="hidden" id="'.$this->id.'_id"'.$class.' name="'.$this->name.'" value="'.$value.'" />';
 
-
-
-
-
-
- }
- ?>
+		return implode("\n", $html);
+	}
+}
+?>
