fb1d4b88345a85edbc0f2eeb17d880b4bb139ebb
diesl <meine.anmeldungen.github@online.de>
2013-11-07 02:16:15 +0100

    Fixed code style

--- admin/sql/updates/1.9.5.sql
+++ admin/sql/updates/1.9.5.sql
@@ -8,25 +8,24 @@ ALTER TABLE `#__jem_settings`
 DELETE FROM `#__menu`
 	WHERE `type`='component' AND `title`='COM_JEM_MENU_ARCHIVE';
 
-	ALTER TABLE `#__jem_events`
+ALTER TABLE `#__jem_events`
 	MODIFY `title` VARCHAR(255),
-	MODIFY `alias` VARCHAR(255)
-	;
+	MODIFY `alias` VARCHAR(255);
 	
 ALTER TABLE `#__jem_categories`
 	ADD `title` varchar(255) NOT NULL,
-  	ADD `note` varchar(255) NOT NULL,
- 	ADD `lft` int(11) NOT NULL DEFAULT '0',
+	ADD `note` varchar(255) NOT NULL,
+	ADD `lft` int(11) NOT NULL DEFAULT '0',
 	ADD `rgt` int(11) NOT NULL DEFAULT '0',
- 	ADD `level` int(10) unsigned NOT NULL DEFAULT '1',
-  	ADD `language` varchar(7) NOT NULL,
-  	ADD `created_user_id` int(10) unsigned NOT NULL DEFAULT '0',
-  	ADD `created_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  	ADD `path` varchar(255) NOT NULL,
-  	ADD `metadata` varchar(2048) NOT NULL,
- 	ADD `modified_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  	ADD `modified_user_id` int(10) unsigned NOT NULL DEFAULT '0'
-	;	
+	ADD `level` int(10) unsigned NOT NULL DEFAULT '1',
+	ADD `language` varchar(7) NOT NULL,
+	ADD `created_user_id` int(10) unsigned NOT NULL DEFAULT '0',
+	ADD `created_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+	ADD `path` varchar(255) NOT NULL,
+	ADD `metadata` varchar(2048) NOT NULL,
+	ADD `modified_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+	ADD `modified_user_id` int(10) unsigned NOT NULL DEFAULT '0';
 	
-INSERT IGNORE INTO `#__jem_categories` (`parent_id`, `lft`, `rgt`, `level`, `catname`,`alias`,`access`,`path`) VALUES
-('0', '0', '1', '0', 'root','root','1','');
+INSERT IGNORE INTO `#__jem_categories`
+	(`parent_id`, `lft`, `rgt`, `level`, `catname`, `alias`, `access`, `path`)
+	VALUES ('0', '0', '1', '0', 'root','root','1','');
