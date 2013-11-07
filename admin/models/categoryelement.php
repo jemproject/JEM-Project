cb9bc8db545c52ad087fd6c85cc8b966041516a2
Jaap Koelman <jaapkoelman@hotmail.com>
2013-11-07 08:23:21 +0100

    altered categoryelement model

--- admin/models/categoryelement.php
+++ admin/models/categoryelement.php
@@ -1,13 +1,12 @@
 <?php
 /**
- * @version 1.9.1
+ * @version 1.9.5
  * @package JEM
  * @copyright (C) 2013-2013 joomlaeventmanager.net
  * @copyright (C) 2005-2009 Christoph Lukes
  * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  */
-
-defined('_JEXEC') or die;
+defined('_JEXEC') or die();
 
 jimport('joomla.application.component.model');
 
@@ -18,167 +17,175 @@ jimport('joomla.application.component.model');
  *
  */
 class JEMModelCategoryelement extends JModelLegacy
-{	/**
-	 * Pagination object
-	 *
-	 * @var object
-	 */
-	var $_pagination = null;
+{
 
-	/**
-	 * Categorie id
-	 *
-	 * @var int
-	 */
-	var $_id = null;
+    /**
+     * Pagination object
+     *
+     * @var object
+     */
+    var $_pagination = null;
 
-	/**
-	 * Constructor
-	 *
-	 */
-	function __construct()
-	{
-		parent::__construct();
+    /**
+     * Categorie id
+     *
+     * @var int
+     */
+    var $_id = null;
 
-		$jinput = JFactory::getApplication()->input;
-		$array = $jinput->get('cid',  0, 'array');
+    /**
+     * Constructor
+     */
+    function __construct()
+    {
+        parent::__construct();
 
-		$this->setId((int)$array[0]);
+        $jinput = JFactory::getApplication()->input;
+        $array = $jinput->get('cid', 0, 'array');
 
-	}
+        $this->setId((int) $array[0]);
+    }
 
-	/**
-	 * Method to set the category identifier
-	 *
-	 * @access	public
-	 * @param	int Category identifier
-	 */
-	function setId($id)
-	{
-		// Set id
-		$this->_id	 = $id;
-	}
+    /**
+     * Method to set the category identifier
+     *
+     * @access public
+     * @param
+     *            int Category identifier
+     */
+    function setId($id)
+    {
+        // Set id
+        $this->_id = $id;
+    }
 
-	/**
-	 * Method to get categories item data
-	 *
-	 * @access public
-	 * @return array
-	 */
-	function getData()
-	{
-		$app =  JFactory::getApplication();
+    /**
+     * Method to get categories item data
+     *
+     * @access public
+     * @return array
+     */
+    function getData()
+    {
+        $app = JFactory::getApplication();
+        $db = JFactory::getDBO();
 
-		static $items;
+        static $items;
 
-		if (isset($items)) {
-			return $items;
-		}
+        if (isset($items)) {
+            return $items;
+        }
 
-		$limit				= $app->getUserStateFromRequest( 'com_jem.limit', 'limit', $app->getCfg('list_limit'), 'int');
-		$limitstart 		= $app->getUserStateFromRequest( 'com_jem.limitstart', 'limitstart', 0, 'int' );
-		$filter_order		= $app->getUserStateFromRequest( 'com_jem.categoryelement.filter_order', 		'filter_order', 	'c.ordering', 'cmd' );
-		$filter_order_Dir	= $app->getUserStateFromRequest( 'com_jem.categoryelement.filter_order_Dir',	'filter_order_Dir',	'', 'word' );
-		$filter_state 		= $app->getUserStateFromRequest( 'com_jem.categoryelement.filter_state', 'filter_state', '', 'word' );
-		$search 			= $app->getUserStateFromRequest( 'com_jem.categoryelement.filter_search', 'filter_search', '', 'string' );
-		$search 			= $this->_db->escape( trim(JString::strtolower( $search ) ) );
+        $limit = $app->getUserStateFromRequest('com_jem.limit', 'limit', $app->getCfg('list_limit'), 'int');
+        $limitstart = $app->getUserStateFromRequest('com_jem.limitstart', 'limitstart', 0, 'int');
+        $filter_order = $app->getUserStateFromRequest('com_jem.categoryelement.filter_order', 'filter_order', 'c.ordering', 'cmd');
+        $filter_order_Dir = $app->getUserStateFromRequest('com_jem.categoryelement.filter_order_Dir', 'filter_order_Dir', '', 'word');
+        $filter_state = $app->getUserStateFromRequest('com_jem.categoryelement.filter_state', 'filter_state', '', 'word');
+        $search = $app->getUserStateFromRequest('com_jem.categoryelement.filter_search', 'filter_search', '', 'string');
+        $search = $db->escape(trim(JString::strtolower($search)));
 
-		$filter_order		= JFilterInput::getinstance()->clean($filter_order, 'cmd');
-		$filter_order_Dir	= JFilterInput::getinstance()->clean($filter_order_Dir, 'word');
+        $filter_order = JFilterInput::getinstance()->clean($filter_order, 'cmd');
+        $filter_order_Dir = JFilterInput::getinstance()->clean($filter_order_Dir, 'word');
 
-		$orderby 	= ' ORDER BY '.$filter_order.' '.$filter_order_Dir.', c.ordering';
+        $orderby = ' ORDER BY ' . $filter_order . ' ' . $filter_order_Dir . ', c.ordering';
+        $state = array(0,1);
 
-		$where = array();
-		if ( $filter_state ) {
-			if ( $filter_state == 'P' ) {
-				$where[] = 'c.published = 1';
-			} else if ($filter_state == 'U' ) {
-				$where[] = 'c.published = 0';
-			}
-		}
+        if ($filter_state) {
+            if ($filter_state == 'P') {
+                $where = ' WHERE c.published = 1';
+            } else
+                if ($filter_state == 'U') {
+                    $where = ' WHERE c.published = 0';
+                    $where .= ' AND c.catname NOT LIKE "root"';
+                }
+        } else {
+            $where = ' WHERE c.published IN (' . implode(',', $state) . ')';
+            $where .= ' AND c.catname NOT LIKE "root"';
+        }
 
-		$where 		= ( count( $where ) ? ' WHERE ' . implode( ' AND ', $where ) : '' );
+        $where2 = ' AND c.published IN (' . implode(',', $state) . ')';
+        $where2 .= ' AND c.catname NOT LIKE "root"';
 
-		//select the records
-		//note, since this is a tree we have to do the limits code-side
-		if ($search) {
+        // select the records
+        // note, since this is a tree we have to do the limits code-side
+        if ($search) {
 
+            $query = 'SELECT c.id' . ' FROM #__jem_categories AS c' . ' WHERE LOWER(c.catname) LIKE ' . $db->Quote('%' . $this->_db->escape($search, true) . '%', false) . $where2;
+            $db->setQuery($query);
+            echo $query;
+            $search_rows = $db->loadColumn();
+        }
 
-			$query = 'SELECT c.id'
-					. ' FROM #__jem_categories AS c'
-					. ' WHERE LOWER(c.catname) LIKE '.$this->_db->Quote( '%'.$this->_db->escape( $search, true ).'%', false )
-					. $where
-					;
-			$this->_db->setQuery( $query );
-			$search_rows = $this->_db->loadColumn();
-		}
+        $query = 'SELECT c.*, u.name AS editor, g.title AS groupname, gr.name AS catgroup'
+             . ' FROM #__jem_categories AS c' . ' LEFT JOIN #__viewlevels AS g ON g.id = c.access'
+             . ' LEFT JOIN #__users AS u ON u.id = c.checked_out'
+             . ' LEFT JOIN #__jem_groups AS gr ON gr.id = c.groupid'
+             . $where
+             . ' ORDER BY c.parent_id, c.ordering';
+        $db->setQuery($query);
+        $mitems = $db->loadObjectList();
 
-		$query = 'SELECT c.*, u.name AS editor, g.title AS groupname, gr.name AS catgroup'
-					. ' FROM #__jem_categories AS c'
-				//	. ' LEFT JOIN #__groups AS g ON g.id = c.access'
-				    . ' LEFT JOIN #__viewlevels AS g ON g.id = c.access'
-					. ' LEFT JOIN #__users AS u ON u.id = c.checked_out'
-					. ' LEFT JOIN #__jem_groups AS gr ON gr.id = c.groupid'
-					. $where
-					. $orderby
-					;
-		$this->_db->setQuery( $query );
-		$rows = $this->_db->loadObjectList();
+        // Check for a database error.
+        if ($db->getErrorNum()) {
+            JError::raiseNotice(500, $db->getErrorMsg());
+        }
 
-	// for debugging
-	//	print_r($query);
+        if (! $mitems) {
+            $mitems = array();
+            $children = array();
 
-		//establish the hierarchy of the categories
-		$children = array();
+            $parentid = $mitems;
+        } else {
 
-		//first pass - collect children
-		//set depth limit
-		$levellimit = 10;
+            $mitems_temp = $mitems;
 
-    	foreach ($rows as $child) {
-        	$parent = $child->parent_id;
-       		$list 	= @$children[$parent] ? $children[$parent] : array();
-        	array_push($list, $child);
-        	$children[$parent] = $list;
-    	}
+            $children = array();
+            // First pass - collect children
+            foreach ($mitems as $v) {
+                $pt = $v->parent_id;
+                $list = @$children[$pt] ? $children[$pt] : array();
+                array_push($list, $v);
+                $children[$pt] = $list;
+            }
 
-    	//second pass - get an indent list of the items
-    	$list = JEMCategories::treerecurse(0, '', array(), $children, false, max(0, $levellimit-1));
+            $parentid = intval($mitems[0]->parent_id);
+        }
 
-    	//eventually only pick out the searched items.
-		if ($search) {
-			$list1 = array();
+        // get list of the items
+        $list = JEMCategories::treerecurse($parentid, '', array(), $children, 9999, 0, 0);
 
-			foreach ($search_rows as $sid )
-			{
-				foreach ($list as $item)
-				{
-					if ($item->id == $sid) {
-						$list1[] = $item;
-					}
-				}
-			}
-			// replace full list with found items
-			$list = $list1;
-		}
+        // eventually only pick out the searched items.
+        if ($search) {
+            $list1 = array();
 
-    	$total = count( $list );
+            foreach ($search_rows as $sid) {
+                foreach ($list as $item) {
+                    if ($item->id == $sid) {
+                        $list1[] = $item;
+                    }
+                }
+            }
+            // replace full list with found items
+            $list = $list1;
+        }
 
-		jimport('joomla.html.pagination');
-		$this->_pagination = new JPagination( $total, $limitstart, $limit );
+        $total = count($list);
 
-		// slice out elements based on limits
-		$list = array_slice( $list, $this->_pagination->limitstart, $this->_pagination->limit );
+        jimport('joomla.html.pagination');
+        $this->_pagination = new JPagination($total, $limitstart, $limit);
 
-		return $list;
-	}
+        // slice out elements based on limits
+        $list = array_slice($list, $this->_pagination->limitstart, $this->_pagination->limit);
 
-	function &getPagination()
-	{
-		if ($this->_pagination == null) {
-			$this->getItems();
-		}
-		return $this->_pagination;
-	}
+        return $list;
+    }
+
+    function &getPagination()
+    {
+        if ($this->_pagination == null) {
+            $this->getItems();
+        }
+        return $this->_pagination;
+    }
 }
 ?>
