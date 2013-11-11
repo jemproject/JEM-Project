451984bd3d7a74d6bc8176159a7efa18300c6386
diesl <meine.anmeldungen.github@online.de>
2013-11-11 13:54:29 +0100

    Fixed indentation, added javadoc, cleanup

--- admin/models/categoryelement.php
+++ admin/models/categoryelement.php
@@ -18,174 +18,171 @@ jimport('joomla.application.component.model');
  */
 class JEMModelCategoryelement extends JModelLegacy
 {
+	/**
+	 * Pagination object
+	 *
+	 * @var object
+	 */
+	var $_pagination = null;
 
-    /**
-     * Pagination object
-     *
-     * @var object
-     */
-    var $_pagination = null;
+	/**
+	 * Category id
+	 *
+	 * @var int
+	 */
+	var $_id = null;
 
-    /**
-     * Categorie id
-     *
-     * @var int
-     */
-    var $_id = null;
+	/**
+	 * Constructor
+	 */
+	function __construct()
+	{
+		parent::__construct();
 
-    /**
-     * Constructor
-     */
-    function __construct()
-    {
-        parent::__construct();
+		$jinput = JFactory::getApplication()->input;
+		$array = $jinput->get('cid', 0, 'array');
 
-        $jinput = JFactory::getApplication()->input;
-        $array = $jinput->get('cid', 0, 'array');
+		$this->setId((int) $array[0]);
+	}
 
-        $this->setId((int) $array[0]);
-    }
+	/**
+	 * Method to set the category identifier
+	 *
+	 * @access public
+	 * @param int Category identifier
+	 */
+	function setId($id)
+	{
+		// Set id
+		$this->_id = $id;
+	}
 
-    /**
-     * Method to set the category identifier
-     *
-     * @access public
-     * @param
-     *            int Category identifier
-     */
-    function setId($id)
-    {
-        // Set id
-        $this->_id = $id;
-    }
+	/**
+	 * Method to get categories item data
+	 *
+	 * @access public
+	 * @return array
+	 */
+	function getData()
+	{
+		$app = JFactory::getApplication();
+		$db = JFactory::getDBO();
 
-    /**
-     * Method to get categories item data
-     *
-     * @access public
-     * @return array
-     */
-    function getData()
-    {
-        $app = JFactory::getApplication();
-        $db = JFactory::getDBO();
+		static $items;
 
-        static $items;
+		if (isset($items)) {
+			return $items;
+		}
 
-        if (isset($items)) {
-            return $items;
-        }
+		$limit = $app->getUserStateFromRequest('com_jem.limit', 'limit', $app->getCfg('list_limit'), 'int');
+		$limitstart = $app->getUserStateFromRequest('com_jem.limitstart', 'limitstart', 0, 'int');
+		$filter_order = $app->getUserStateFromRequest('com_jem.categoryelement.filter_order', 'filter_order', 'c.ordering', 'cmd');
+		$filter_order_Dir = $app->getUserStateFromRequest('com_jem.categoryelement.filter_order_Dir', 'filter_order_Dir', '', 'word');
+		$filter_state = $app->getUserStateFromRequest('com_jem.categoryelement.filter_state', 'filter_state', '', 'word');
+		$search = $app->getUserStateFromRequest('com_jem.categoryelement.filter_search', 'filter_search', '', 'string');
+		$search = $db->escape(trim(JString::strtolower($search)));
 
-        $limit = $app->getUserStateFromRequest('com_jem.limit', 'limit', $app->getCfg('list_limit'), 'int');
-        $limitstart = $app->getUserStateFromRequest('com_jem.limitstart', 'limitstart', 0, 'int');
-        $filter_order = $app->getUserStateFromRequest('com_jem.categoryelement.filter_order', 'filter_order', 'c.ordering', 'cmd');
-        $filter_order_Dir = $app->getUserStateFromRequest('com_jem.categoryelement.filter_order_Dir', 'filter_order_Dir', '', 'word');
-        $filter_state = $app->getUserStateFromRequest('com_jem.categoryelement.filter_state', 'filter_state', '', 'word');
-        $search = $app->getUserStateFromRequest('com_jem.categoryelement.filter_search', 'filter_search', '', 'string');
-        $search = $db->escape(trim(JString::strtolower($search)));
+		$filter_order = JFilterInput::getinstance()->clean($filter_order, 'cmd');
+		$filter_order_Dir = JFilterInput::getinstance()->clean($filter_order_Dir, 'word');
 
-        $filter_order = JFilterInput::getinstance()->clean($filter_order, 'cmd');
-        $filter_order_Dir = JFilterInput::getinstance()->clean($filter_order_Dir, 'word');
+		$orderby = ' ORDER BY ' . $filter_order . ' ' . $filter_order_Dir . ', c.ordering';
+		$state = array(0,1);
 
-        $orderby = ' ORDER BY ' . $filter_order . ' ' . $filter_order_Dir . ', c.ordering';
-        $state = array(0,1);
+		if ($filter_state) {
+			if ($filter_state == 'P') {
+				$where = ' WHERE c.published = 1';
+			} else {
+				if ($filter_state == 'U') {
+					$where = ' WHERE c.published = 0';
+					$where .= ' AND c.catname NOT LIKE "root"';
+				}
+			}
+		} else {
+			$where = ' WHERE c.published IN (' . implode(',', $state) . ')';
+			$where .= ' AND c.catname NOT LIKE "root"';
+		}
 
-        if ($filter_state) {
-            if ($filter_state == 'P') {
-                $where = ' WHERE c.published = 1';
-            } else
-                if ($filter_state == 'U') {
-                    $where = ' WHERE c.published = 0';
-                    $where .= ' AND c.catname NOT LIKE "root"';
-                }
-        } else {
-            $where = ' WHERE c.published IN (' . implode(',', $state) . ')';
-            $where .= ' AND c.catname NOT LIKE "root"';
-        }
+		$where2 = ' AND c.published IN (' . implode(',', $state) . ')';
+		$where2 .= ' AND c.catname NOT LIKE "root"';
 
-        $where2 = ' AND c.published IN (' . implode(',', $state) . ')';
-        $where2 .= ' AND c.catname NOT LIKE "root"';
+		// select the records
+		// note, since this is a tree we have to do the limits code-side
+		if ($search) {
+			$query = 'SELECT c.id FROM #__jem_categories AS c' . ' WHERE LOWER(c.catname) LIKE ' . $db->Quote('%' . $this->_db->escape($search, true) . '%', false) . $where2;
+			$db->setQuery($query);
+			echo $query;
+			$search_rows = $db->loadColumn();
+		}
 
-        // select the records
-        // note, since this is a tree we have to do the limits code-side
-        if ($search) {
+		$query = 'SELECT c.*, u.name AS editor, g.title AS groupname, gr.name AS catgroup'
+			 . ' FROM #__jem_categories AS c' . ' LEFT JOIN #__viewlevels AS g ON g.id = c.access'
+			 . ' LEFT JOIN #__users AS u ON u.id = c.checked_out'
+			 . ' LEFT JOIN #__jem_groups AS gr ON gr.id = c.groupid'
+			 . $where
+			 . ' ORDER BY c.parent_id, c.ordering';
+		$db->setQuery($query);
+		$mitems = $db->loadObjectList();
 
-            $query = 'SELECT c.id' . ' FROM #__jem_categories AS c' . ' WHERE LOWER(c.catname) LIKE ' . $db->Quote('%' . $this->_db->escape($search, true) . '%', false) . $where2;
-            $db->setQuery($query);
-            echo $query;
-            $search_rows = $db->loadColumn();
-        }
+		// Check for a database error.
+		if ($db->getErrorNum()) {
+			JError::raiseNotice(500, $db->getErrorMsg());
+		}
 
-        $query = 'SELECT c.*, u.name AS editor, g.title AS groupname, gr.name AS catgroup'
-             . ' FROM #__jem_categories AS c' . ' LEFT JOIN #__viewlevels AS g ON g.id = c.access'
-             . ' LEFT JOIN #__users AS u ON u.id = c.checked_out'
-             . ' LEFT JOIN #__jem_groups AS gr ON gr.id = c.groupid'
-             . $where
-             . ' ORDER BY c.parent_id, c.ordering';
-        $db->setQuery($query);
-        $mitems = $db->loadObjectList();
+		if (! $mitems) {
+			$mitems = array();
+			$children = array();
 
-        // Check for a database error.
-        if ($db->getErrorNum()) {
-            JError::raiseNotice(500, $db->getErrorMsg());
-        }
+			$parentid = $mitems;
+		} else {
+			$mitems_temp = $mitems;
 
-        if (! $mitems) {
-            $mitems = array();
-            $children = array();
+			$children = array();
+			// First pass - collect children
+			foreach ($mitems as $v) {
+				$pt = $v->parent_id;
+				$list = @$children[$pt] ? $children[$pt] : array();
+				array_push($list, $v);
+				$children[$pt] = $list;
+			}
 
-            $parentid = $mitems;
-        } else {
+			$parentid = intval($mitems[0]->parent_id);
+		}
 
-            $mitems_temp = $mitems;
+		// get list of the items
+		$list = JEMCategories::treerecurse($parentid, '', array(), $children, 9999, 0, 0);
 
-            $children = array();
-            // First pass - collect children
-            foreach ($mitems as $v) {
-                $pt = $v->parent_id;
-                $list = @$children[$pt] ? $children[$pt] : array();
-                array_push($list, $v);
-                $children[$pt] = $list;
-            }
+		// eventually only pick out the searched items.
+		if ($search) {
+			$list1 = array();
 
-            $parentid = intval($mitems[0]->parent_id);
-        }
+			foreach ($search_rows as $sid) {
+				foreach ($list as $item) {
+					if ($item->id == $sid) {
+						$list1[] = $item;
+					}
+				}
+			}
+			// replace full list with found items
+			$list = $list1;
+		}
 
-        // get list of the items
-        $list = JEMCategories::treerecurse($parentid, '', array(), $children, 9999, 0, 0);
+		$total = count($list);
 
-        // eventually only pick out the searched items.
-        if ($search) {
-            $list1 = array();
+		jimport('joomla.html.pagination');
+		$this->_pagination = new JPagination($total, $limitstart, $limit);
 
-            foreach ($search_rows as $sid) {
-                foreach ($list as $item) {
-                    if ($item->id == $sid) {
-                        $list1[] = $item;
-                    }
-                }
-            }
-            // replace full list with found items
-            $list = $list1;
-        }
+		// slice out elements based on limits
+		$list = array_slice($list, $this->_pagination->limitstart, $this->_pagination->limit);
 
-        $total = count($list);
+		return $list;
+	}
 
-        jimport('joomla.html.pagination');
-        $this->_pagination = new JPagination($total, $limitstart, $limit);
-
-        // slice out elements based on limits
-        $list = array_slice($list, $this->_pagination->limitstart, $this->_pagination->limit);
-
-        return $list;
-    }
-
-    function &getPagination()
-    {
-        if ($this->_pagination == null) {
-            $this->getItems();
-        }
-        return $this->_pagination;
-    }
+	function &getPagination()
+	{
+		if ($this->_pagination == null) {
+			$this->getItems();
+		}
+		return $this->_pagination;
+	}
 }
 ?>
--- admin/models/export.php
+++ admin/models/export.php
@@ -17,304 +17,298 @@ jimport('joomla.application.component.modellist');
  */
 class JEMModelExport extends JModelList
 {
+	/**
+	 * Constructor.
+	 *
+	 * @param array An optional associative array of configuration settings.
+	 * @see JController
+	 *
+	 */
+	public function __construct($config = array())
+	{
+		if (empty($config['filter_fields'])) {
+			$config['filter_fields'] = array(
+				'id',
+				'a.id'
+			);
+		}
 
-    /**
-     * Constructor.
-     *
-     * @param array An optional associative array of configuration settings.
-     * @see JController
-     *
-     */
-    public function __construct($config = array())
-    {
-        if (empty($config['filter_fields'])) {
-            $config['filter_fields'] = array(
-                'id',
-                'a.id'
-            );
-        }
+		parent::__construct($config);
+	}
 
-        parent::__construct($config);
-    }
+	/**
+	 * Method to auto-populate the model state.
+	 *
+	 * Note. Calling getState in this method will result in recursion.
+	 */
+	protected function populateState($ordering = null, $direction = null)
+	{
+		// Initialise variables.
+		$app = JApplication::getInstance('administrator');
 
-    /**
-     * Method to auto-populate the model state.
-     *
-     * Note. Calling getState in this method will result in recursion.
-     */
-    protected function populateState($ordering = null, $direction = null)
-    {
-        // Initialise variables.
-        $app = JApplication::getInstance('administrator');
+		// Load the filter state.
+		$filter_form_type = $app->getUserStateFromRequest($this->context . '.filter.form_type', 'filter_form_type');
+		$this->setState('filter.form_type', $filter_form_type);
 
-        // Load the filter state.
-        $filter_form_type = $app->getUserStateFromRequest($this->context . '.filter.form_type', 'filter_form_type');
-        $this->setState('filter.form_type', $filter_form_type);
+		$filter_start_date = $app->getUserStateFromRequest($this->context . '.filter.start_date', 'filter_start_date');
+		$this->setState('filter.start_date', $filter_start_date);
 
-        $filter_start_date = $app->getUserStateFromRequest($this->context . '.filter.start_date', 'filter_start_date');
-        $this->setState('filter.start_date', $filter_start_date);
+		$filter_end_date = $app->getUserStateFromRequest($this->context . '.filter.end_date', 'filter_end_date');
+		$this->setState('filter.end_date', $filter_end_date);
 
-        $filter_end_date = $app->getUserStateFromRequest($this->context . '.filter.end_date', 'filter_end_date');
-        $this->setState('filter.end_date', $filter_end_date);
+		// Load the parameters.
+		$params = JComponentHelper::getParams('com_jem');
+		$this->setState('params', $params);
 
-        // Load the parameters.
-        $params = JComponentHelper::getParams('com_jem');
-        $this->setState('params', $params);
+		// List state information.
+		parent::populateState('a.first_name', 'asc');
+	}
 
-        // List state information.
-        parent::populateState('a.first_name', 'asc');
-    }
 
-    /**
-     * ******
-     * Events
-     * ******
-     */
+	/**
+	 * Build an SQL query to load the Events data.
+	 *
+	 * @return JDatabaseQuery
+	 */
+	protected function getListQuery()
+	{
+		// Retrieve variables
+		$jinput = JFactory::getApplication()->input;
+		$startdate = $jinput->get('dates', '', 'string');
+		$enddate = $jinput->get('enddates', '', 'string');
+		$cats = $jinput->get('cid', array(), 'post', 'array');
 
-    /**
-     * Build an SQL query to load the list data.
-     *
-     * @return JDatabaseQuery
-     */
-    protected function getListQuery()
-    {
-        // Retrieve variables
-        $jinput = JFactory::getApplication()->input;
-        $startdate = $jinput->get('dates', '', 'string');
-        $enddate = $jinput->get('enddates', '', 'string');
-        $cats = $jinput->get('cid', array(), 'post', 'array');
+		// Create a new query object.
+		$db = $this->getDbo();
+		$query = $db->getQuery(true);
 
-        // Create a new query object.
-        $db = $this->getDbo();
-        $query = $db->getQuery(true);
+		// Select the required fields from the table.
+		$query->select('a.*');
+		$query->from('`#__jem_events` AS a');
+		$query->join('LEFT', '#__jem_cats_event_relations AS rel ON rel.itemid = a.id');
+		$query->join('LEFT', '#__jem_categories AS c ON c.id = rel.catid');
 
-        // Select the required fields from the table.
-        $query->select('a.*');
-        $query->from('`#__jem_events` AS a');
-        $query->join('LEFT', '#__jem_cats_event_relations AS rel ON rel.itemid = a.id');
-        $query->join('LEFT', '#__jem_categories AS c ON c.id = rel.catid');
+		// check if startdate + enddate are set.
+		if (! empty($startdate) && ! empty($enddate)) {
+			$query->where('DATEDIFF(IF (a.enddates IS NOT NULL AND a.enddates <> ' . $db->Quote('0000-00-00') . ', a.enddates, a.dates), "' . $startdate . '") >= 0');
+			$query->where('DATEDIFF(a.dates, "' . $enddate . '") <= 0');
+		}
 
-        // check if startdate + enddate are set.
-        if (! empty($startdate) && ! empty($enddate)) {
-            $query->where('DATEDIFF(IF (a.enddates IS NOT NULL AND a.enddates <> ' . $db->Quote('0000-00-00') . ', a.enddates, a.dates), "' . $startdate . '") >= 0');
-            $query->where('DATEDIFF(a.dates, "' . $enddate . '") <= 0');
-        }
+		// check if specific category's have been selected
+		if (! empty($cats)) {
+			$query->where('  (c.id=' . implode(' OR c.id=', $cats) . ')');
+		}
 
-        // check if specific category's have been selected
-        if (! empty($cats)) {
-            $query->where('  (c.id=' . implode(' OR c.id=', $cats) . ')');
-        }
+		// Group the query
+		$query->group('a.id');
 
-        // Group the query
-        $query->group('a.id');
+		return $query;
+	}
 
-        return $query;
-    }
+	/**
+	 * Returns a CSV file with Events data
+	 * @return boolean
+	 */
+	public function getCsv()
+	{
+		$this->populateState();
 
-    public function getCsv()
-    {
-        $this->populateState();
+		$jinput = JFactory::getApplication()->input;
+		$includecategories = $jinput->get('categorycolumn', 0, 'int');
 
-        $jinput = JFactory::getApplication()->input;
-        $includecategories = $jinput->get('categorycolumn', 0, 'int');
+		$csv = fopen('php://output', 'w');
+		$db = $this->getDbo();
 
-        $csv = fopen('php://output', 'w');
-        $db = $this->getDbo();
+		if ($includecategories == 1) {
+			$header = array();
+			$events = array_keys($db->getTableColumns('#__jem_events'));
+			$categories = array();
+			$categories[] = "categories";
+			$header = array_merge($events, $categories);
 
-        if ($includecategories == 1) {
-            $header = array();
-            $events = array_keys($db->getTableColumns('#__jem_events'));
-            $categories = array();
-            $categories[] = "categories";
-            $header = array_merge($events, $categories);
+			fputcsv($csv, $header, ';');
 
-            fputcsv($csv, $header, ';');
+			$query = $this->getListQuery();
+			$items = $this->_getList($query);
 
-            $query = $this->getListQuery();
-            $items = $this->_getList($query);
+			foreach ($items as $item) {
+				$item->categories = $this->getCatEvent($item->id);
+			}
+		} else {
+			$header = array_keys($db->getTableColumns('#__jem_events'));
+			fputcsv($csv, $header, ';');
+			$query = $this->getListQuery();
+			$items = $this->_getList($query);
+		}
 
-            foreach ($items as $item) {
-                $item->categories = $this->getCatEvent($item->id);
-            }
-        } else {
-            $header = array_keys($db->getTableColumns('#__jem_events'));
-            fputcsv($csv, $header, ';');
-            $query = $this->getListQuery();
-            $items = $this->_getList($query);
-        }
+		foreach ($items as $lines) {
+			foreach ($lines as &$line) {
+				$line = mb_convert_encoding($line, 'Windows-1252', 'auto');
+			}
+			fputcsv($csv, (array) $lines, ';', '"');
+		}
 
-        foreach ($items as $lines) {
-            foreach ($lines as &$line) {
-                $line = mb_convert_encoding($line, 'Windows-1252', 'auto');
-            }
-            fputcsv($csv, (array) $lines, ';', '"');
-        }
+		return fclose($csv);
+	}
 
-        return fclose($csv);
-    }
 
-    /**
-     * ******
-     * Categories
-     * ******
-     */
+	/**
+	 * Build an SQL query to load the Categories data.
+	 *
+	 * @return JDatabaseQuery
+	 */
+	protected function getListQuerycats()
+	{
+		// Create a new query object.
+		$db = $this->getDbo();
+		$query = $db->getQuery(true);
 
-    /**
-     * Build an SQL query to load the list data.
-     *
-     * @return JDatabaseQuery
-     */
-    protected function getListQuerycats()
-    {
-        // Create a new query object.
-        $db = $this->getDbo();
-        $query = $db->getQuery(true);
+		// Select the required fields from the table.
+		$query->select('a.*');
+		$query->from('#__jem_categories AS a');
 
-        // Select the required fields from the table.
-        $query->select('a.*');
-        $query->from('#__jem_categories AS a');
+		return $query;
+	}
 
-        return $query;
-    }
+	/**
+	 * Returns a CSV file with Categories data
+	 * @return boolean
+	 */
+	public function getCsvcats()
+	{
+		$this->populateState();
 
-    public function getCsvcats()
-    {
-        $this->populateState();
+		$csv = fopen('php://output', 'w');
+		$db = $this->getDbo();
+		$header = array();
+		$header = array_keys($db->getTableColumns('#__jem_categories'));
+		fputcsv($csv, $header, ';');
 
-        $csv = fopen('php://output', 'w');
-        $db = $this->getDbo();
-        $header = array();
-        $header = array_keys($db->getTableColumns('#__jem_categories'));
-        fputcsv($csv, $header, ';');
+		$items = $db->setQuery($this->getListQuerycats())
+			->loadObjectList();
 
-        $items = $db->setQuery($this->getListQuerycats())
-            ->loadObjectList();
+		foreach ($items as $lines) {
+			foreach ($lines as &$line) {
+				$line = mb_convert_encoding($line, 'Windows-1252', 'auto');
+			}
+			fputcsv($csv, (array) $lines, ';', '"');
+		}
 
-        foreach ($items as $lines) {
-            foreach ($lines as &$line) {
-                $line = mb_convert_encoding($line, 'Windows-1252', 'auto');
-            }
-            fputcsv($csv, (array) $lines, ';', '"');
-        }
+		return fclose($csv);
+	}
 
-        return fclose($csv);
-    }
 
-    /**
-     * ******
-     * Venues
-     * ******
-     */
+	/**
+	 * Build an SQL query to load the Venues data.
+	 *
+	 * @return JDatabaseQuery
+	 */
+	protected function getListQueryvenues()
+	{
+		// Create a new query object.
+		$db = $this->getDbo();
+		$query = $db->getQuery(true);
 
-    /**
-     * Build an SQL query to load the list data.
-     *
-     * @return JDatabaseQuery
-     */
-    protected function getListQueryvenues()
-    {
-        // Create a new query object.
-        $db = $this->getDbo();
-        $query = $db->getQuery(true);
+		// Select the required fields from the table.
+		$query->select('a.*');
+		$query->from('#__jem_venues AS a');
 
-        // Select the required fields from the table.
-        $query->select('a.*');
-        $query->from('#__jem_venues AS a');
+		return $query;
+	}
 
-        return $query;
-    }
+	/**
+	 * Returns a CSV file with Venues data
+	 * @return boolean
+	 */
+	public function getCsvvenues()
+	{
+		$this->populateState();
 
-    public function getCsvvenues()
-    {
-        $this->populateState();
+		$csv = fopen('php://output', 'w');
+		$db = $this->getDbo();
+		$header = array();
+		$header = array_keys($db->getTableColumns('#__jem_venues'));
+		fputcsv($csv, $header, ';');
 
-        $csv = fopen('php://output', 'w');
-        $db = $this->getDbo();
-        $header = array();
-        $header = array_keys($db->getTableColumns('#__jem_venues'));
-        fputcsv($csv, $header, ';');
+		$items = $db->setQuery($this->getListQueryvenues())
+			->loadObjectList();
 
-        $items = $db->setQuery($this->getListQueryvenues())
-            ->loadObjectList();
+		foreach ($items as $lines) {
+			foreach ($lines as &$line) {
+				$line = mb_convert_encoding($line, 'Windows-1252', 'auto');
+			}
+			fputcsv($csv, (array) $lines, ';', '"');
+		}
 
-        foreach ($items as $lines) {
-            foreach ($lines as &$line) {
-                $line = mb_convert_encoding($line, 'Windows-1252', 'auto');
-            }
-            fputcsv($csv, (array) $lines, ';', '"');
-        }
+		return fclose($csv);
+	}
 
-        return fclose($csv);
-    }
 
-    /**
-     * ******
-     * CATS_EVENTS
-     * ******
-     */
+	/**
+	 * Build an SQL query to load the Cats/Events data.
+	 *
+	 * @return JDatabaseQuery
+	 */
+	protected function getListQuerycatsevents()
+	{
+		// Create a new query object.
+		$db = $this->getDbo();
+		$query = $db->getQuery(true);
 
-    /**
-     * Build an SQL query to load the list data.
-     *
-     * @return JDatabaseQuery
-     */
-    protected function getListQuerycatsevents()
-    {
-        // Create a new query object.
-        $db = $this->getDbo();
-        $query = $db->getQuery(true);
+		// Select the required fields from the table.
+		$query->select('a.*');
+		$query->from('#__jem_cats_event_relations AS a');
 
-        // Select the required fields from the table.
-        $query->select('a.*');
-        $query->from('#__jem_cats_event_relations AS a');
+		return $query;
+	}
 
-        return $query;
-    }
+	/**
+	 * Returns a CSV file with Cats/Events data
+	 * @return boolean
+	 */
+	public function getCsvcatsevents()
+	{
+		$this->populateState();
 
-    public function getCsvcatsevents()
-    {
-        $this->populateState();
+		$csv = fopen('php://output', 'w');
+		$db = $this->getDbo();
+		$header = array();
+		$header = array_keys($db->getTableColumns('#__jem_cats_event_relations'));
+		fputcsv($csv, $header, ';');
 
-        $csv = fopen('php://output', 'w');
-        $db = $this->getDbo();
-        $header = array();
-        $header = array_keys($db->getTableColumns('#__jem_cats_event_relations'));
-        fputcsv($csv, $header, ';');
+		$items = $db->setQuery($this->getListQuerycatsevents())
+			->loadObjectList();
 
-        $items = $db->setQuery($this->getListQuerycatsevents())
-            ->loadObjectList();
+		foreach ($items as $lines) {
+			foreach ($lines as &$line) {
+				$line = mb_convert_encoding($line, 'Windows-1252', 'auto');
+			}
+			fputcsv($csv, (array) $lines, ';', '"');
+		}
 
-        foreach ($items as $lines) {
-            foreach ($lines as &$line) {
-                $line = mb_convert_encoding($line, 'Windows-1252', 'auto');
-            }
-            fputcsv($csv, (array) $lines, ';', '"');
-        }
+		return fclose($csv);
+	}
 
-        return fclose($csv);
-    }
+	/**
+	 * logic to get the categories
+	 *
+	 * @return void
+	 */
+	public function getCategories()
+	{
+		// @todo alter function
+		$user = JFactory::getUser();
+		$jemsettings = JEMHelper::config();
+		$userid = (int) $user->get('id');
+		$superuser = JEMUser::superuser();
+		$db = JFactory::getDBO();
 
-    /**
-     * logic to get the categories
-     *
-     * @access public
-     * @return void
-     */
-    function getCategories()
-    {
-        // @todo alter function
-        $user = JFactory::getUser();
-        $jemsettings = JEMHelper::config();
-        $userid = (int) $user->get('id');
-        $superuser = JEMUser::superuser();
-        $db = JFactory::getDBO();
+		$where = ' WHERE c.published = 1';
 
-        $where = ' WHERE c.published = 1';
+		$query = 'SELECT c.* FROM #__jem_categories AS c' . $where . ' ORDER BY parent_id, c.ordering';
+		$db->setQuery($query);
 
-        $query = 'SELECT c.*' . ' FROM #__jem_categories AS c' . $where . ' ORDER BY parent_id, c.ordering';
-        $db->setQuery($query);
-
-        $mitems = $db->loadObjectList();
+		$mitems = $db->loadObjectList();
 
 		// Check for a database error.
 		if ($db->getErrorNum()){
@@ -345,36 +339,39 @@ class JEMModelExport extends JModelList
 		//get list of the items
 		$list = JEMCategories::treerecurse($parentid, '', array(), $children, 9999, 0, 0);
 
-        return $list;
-    }
+		return $list;
+	}
 
-    function getCatEvent($catid)
-    {
+	/**
+	 * Get Cat ID for a specific event
+	 * @param unknown $id event id
+	 * @return Ambigous <boolean, string>
+	 */
+	function getCatEvent($id)
+	{
+		// Create a new query object.
+		$db = $this->getDbo();
+		$query = $db->getQuery(true);
 
-        // Create a new query object.
-        $db = $this->getDbo();
-        $query = $db->getQuery(true);
+		// Select the required fields from the table.
+		$query->select('catid');
+		$query->from('#__jem_cats_event_relations');
+		$query->where('itemid = ' . $id);
 
-        // Select the required fields from the table.
-        $query->select('catid');
-        $query->from('#__jem_cats_event_relations');
-        $query->where('itemid = ' . $catid);
+		$db->setQuery($query);
+		$catidlist = $db->loadObjectList();
 
-        $db->setQuery($query);
-        $catidlist = $db->loadObjectList();
+		if (count($catidlist)) {
+			$catidarray;
+			foreach ($catidlist as $obj) {
+				$catidarray[] = $obj->catid;
+			}
 
-        if (count($catidlist)) {
+			$catids = implode(',', $catidarray);
+		} else {
+			$catids = false;
+		}
 
-            $catidarray;
-            foreach ($catidlist as $obj) {
-                $catidarray[] = $obj->catid;
-            }
-
-            $catids = implode(',', $catidarray);
-        } else {
-            $catids = false;
-        }
-
-        return $catids;
-    }
+		return $catids;
+	}
 }
