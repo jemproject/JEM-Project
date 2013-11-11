9c468532052ee6acb23f85f9a144bfaa3ea6d2ff
Jaap Koelman <jaapkoelman@hotmail.com>
2013-11-11 23:54:41 +0100

    modified export-view (backend), ref #442

--- admin/views/export/tmpl/default.php
+++ admin/views/export/tmpl/default.php
@@ -10,185 +10,79 @@
 defined('_JEXEC') or die;
 // JEMHelper::headerDeclarations();
 ?>
-
 <script type="text/javascript">
     function selectAll()
     {
         selectBox = document.getElementById("cid");
 
-        for (var i = 0; i < selectBox.options.length; i++)
-        {
+        for (var i = 0; i < selectBox.options.length; i++){
              selectBox.options[i].selected = true;
         }
     }
-
 
     function unselectAll()
     {
         selectBox = document.getElementById("cid");
 
-        for (var i = 0; i < selectBox.options.length; i++)
-        {
+        for (var i = 0; i < selectBox.options.length; i++){
              selectBox.options[i].selected = false;
         }
     }
-
 </script>
-
 
 <div id="jem" class="jem_jem">
 <form action="index.php" method="post" name="adminForm" enctype="multipart/form-data" id="adminForm">
-	<fieldset class="adminform">
-		<legend><?php echo JText::_('COM_JEM_EXPORT_EVENTS'); ?></legend>
-		<table>
-			<tr>
-				<td>
-				</td>
-				<td></td>
-			</tr>
-			<tr>
-				<td>
-				<span class="editlinktip hasTip" title="<?php echo JText::_('COM_JEM_EXPORT_ADD_CATEGORYCOLUMN'); ?>::<?php echo JText::_('COM_JEM_EXPORT_ADD_CATEGORYCOLUMN'); ?>">
-						<?php echo JText::_('COM_JEM_EXPORT_ADD_CATEGORYCOLUMN'); ?>
-					</span>
-				</td>
-				<td>
-				<?php
+	
+	<div class="width-50 fltlft">
+		<fieldset class="adminform">
+			<legend><?php echo JText::_('COM_JEM_EXPORT_EVENTS_LEGEND');?></legend>
+			
+		<ul class="adminformlist">
+		<li><label class="editlinktip hasTip" title="<?php echo JText::_('COM_JEM_EXPORT_ADD_CATEGORYCOLUMN'); ?>::<?php echo JText::_('COM_JEM_EXPORT_ADD_CATEGORYCOLUMN'); ?>">
+		<?php echo JText::_('COM_JEM_EXPORT_ADD_CATEGORYCOLUMN'); ?></label>
+		<?php
 				$categorycolumn = array();
 				$categorycolumn[] = JHTML::_('select.option', '0', JText::_( 'JNO' ) );
 				$categorycolumn[] = JHTML::_('select.option', '1', JText::_( 'JYES' ) );
 				$categorycolumn = JHTML::_('select.genericlist', $categorycolumn, 'categorycolumn', 'size="1" class="inputbox"', 'value', 'text', '1');
-				echo $categorycolumn;
-				?>
-				</td>
-			</tr>
-			<tr>
-				<td>
-				<label for="dates">
-					<?php echo JText::_( 'COM_JEM_DATE' ).':'; ?>
-				</label>
-				</td>
-				<td>
-				<?php echo JHTML::_('calendar', date("Y-m-d"), 'dates', 'dates', '%Y-%m-%d', array('class' => 'inputbox validate-date')); ?>
-			</td>
-			</tr>
-			<tr>
-				<td>
-				<label for="enddates">
-					<?php echo JText::_( 'COM_JEM_ENDDATE' ).':'; ?>
-				</label>
-				</td>
-				<td>
-				<?php echo JHTML::_('calendar', date("Y-m-d"), 'enddates', 'enddates', '%Y-%m-%d', array('class' => 'inputbox validate-date')); ?>
-			</td>
-			</tr>
-			<tr>
-				<td>
-				<label for="cid">
-					<?php echo JText::_( 'COM_JEM_CATEGORY' ).':'; ?>
-				</label>
-				</td>
-				<td>
-				<?php echo $this->categories; ?>
-				<input class="button" name="selectall" value="<?php echo JText::_('COM_JEM_EXPORT_SELECT_ALL_CATEGORIES'); ?>" onclick="selectAll();">
-				<br>
-				<input class="button" name="unselectall" value="<?php echo JText::_('COM_JEM_EXPORT_UNSELECT_ALL_CATEGORIES'); ?>" onclick="unselectAll();">
-			</td>
-			</tr>
-			<tr>
-				<td>
-				</td>
-				<td>
-				<input class="button" type="submit" value="<?php echo JText::_('COM_JEM_CSV_EXPORT'); ?>" onclick="document.getElementsByName('task')[0].value='export.export';return true;" />
-				</td>
-			</tr>
-		</table>
-	</fieldset>
+				echo $categorycolumn;?></li>
+		<li><label for="dates"><?php echo JText::_( 'COM_JEM_DATE' ).':'; ?></label>
+		<?php echo JHTML::_('calendar', date("Y-m-d"), 'dates', 'dates', '%Y-%m-%d', array('class' => 'inputbox validate-date')); ?></li>
+		<li><label for="enddates"><?php echo JText::_( 'COM_JEM_ENDDATE' ).':'; ?></label>
+		<?php echo JHTML::_('calendar', date("Y-m-d"), 'enddates', 'enddates', '%Y-%m-%d', array('class' => 'inputbox validate-date')); ?></li>
+		
+		<li><label for="cid"><?php echo JText::_( 'COM_JEM_CATEGORY' ).':'; ?></label>
+		<?php echo $this->categories; ?>
+		<input class="button" name="selectall" value=<?php echo JText::_('COM_JEM_EXPORT_SELECT_ALL_CATEGORIES'); ?> onclick="selectAll();"><br />
+		<input class="button" name="unselectall" value=<?php echo JText::_('COM_JEM_EXPORT_UNSELECT_ALL_CATEGORIES'); ?> onclick="unselectAll();"></li>
 
-	<fieldset class="adminform">
-		<legend><?php echo JText::_('COM_JEM_EXPORT_CATEGORIES'); ?></legend>
-		<table>
-			<tr>
-				<td>
-					<label for="file">
-						<?php echo JText::_('').':'; ?>
-					</label>
-				</td>
-				<td></td>
-			</tr>
-			<tr>
-				<td>
-					<label for="replace_cats">
-						<?php echo JText::_('').':'; ?>
-					</label>
-				</td>
-				<td>
-					<div class="button2-left">
-						<div class="blank">
-							<a title="<?php echo JText::_('COM_JEM_CSV_EXPORT'); ?>" onclick="window.open('index.php?option=com_jem&task=export.exportcats')"><?php echo JText::_('COM_JEM_EXPORT_BUTTON'); ?></a>
-						</div>
-					</div>
-				</td>
-			</tr>
-		</table>
-	</fieldset>
+		<li><label></label>
+		<input type="submit" id="csvexport" value="<?php echo JText::_('COM_JEM_EXPORT_FILE'); ?>" onclick="document.getElementsByName('task')[0].value='export.export';return true;"></input></li>
+		</ul>
+		</fieldset>
+		
+		<div class="clr"></div>
+	</div>
+	
+	<div class="width-50 fltrt">
+		<fieldset class="adminform">
+			<legend><?php echo JText::_('COM_JEM_EXPORT_OTHER_LEGEND');?></legend>
 
-	<fieldset class="adminform">
-		<legend><?php echo JText::_('COM_JEM_EXPORT_VENUES'); ?></legend>
-		<table>
-			<tr>
-				<td>
-					<label for="file">
-						<?php echo JText::_('').':'; ?>
-					</label>
-				</td>
-				<td></td>
-			</tr>
-			<tr>
-				<td>
-					<label for="replace_venues">
-						<?php echo JText::_('').':'; ?>
-					</label>
-				</td>
-				<td>
-					<div class="button2-left">
-						<div class="blank">
-							<a title="<?php echo JText::_('COM_JEM_CSV_EXPORT'); ?>" onclick="window.open('index.php?option=com_jem&task=export.exportvenues')"><?php echo JText::_('COM_JEM_EXPORT_BUTTON'); ?></a>
-						</div>
-					</div>
-				</td>
-			</tr>
-		</table>
-	</fieldset>
+		<ul class="adminformlist">
+		<li><label><?php echo JText::_('COM_JEM_EXPORT_CATEGORIES'); ?></label>
+		<input type="submit" id="csvexport" value="<?php echo JText::_('COM_JEM_EXPORT_FILE'); ?>" onclick="document.getElementsByName('task')[0].value='export.exportcats';return true;"></input></li>
+		
+		<li><label><?php echo JText::_('COM_JEM_EXPORT_VENUES'); ?></label>
+		<input type="submit" id="csvexport" value="<?php echo JText::_('COM_JEM_EXPORT_FILE'); ?>" onclick="document.getElementsByName('task')[0].value='export.exportvenues';return true;"></input></li>
+		
+		<li><label><?php echo JText::_('COM_JEM_EXPORT_CAT_EVENTS'); ?></label>
+		<input type="submit" id="csvexport" value="<?php echo JText::_('COM_JEM_EXPORT_FILE'); ?>" onclick="document.getElementsByName('task')[0].value='export.exportcatevents';return true;"></input></li>
 
-	<fieldset class="adminform">
-		<legend><?php echo JText::_('COM_JEM_EXPORT_CAT_EVENTS'); ?></legend>
-		<table>
-			<tr>
-				<td>
-					<label for="file">
-						<?php echo JText::_('').':'; ?>
-					</label>
-				</td>
-				<td></td>
-			</tr>
-			<tr>
-				<td>
-					<label for="replace_catevents">
-						<?php echo JText::_('').':'; ?>
-					</label>
-				</td>
-				<td>
-					<div class="button2-left">
-						<div class="blank">
-							<a title="<?php echo JText::_('COM_JEM_CSV_EXPORT'); ?>" onclick="window.open('index.php?option=com_jem&task=export.exportcatevents')"><?php echo JText::_('COM_JEM_EXPORT_BUTTON'); ?></a>
-						</div>
-					</div>
-				</td>
-			</tr>
-		</table>
-	</fieldset>
-
+		</ul>	
+		</fieldset>
+		<div class="clr"></div>
+	</div>
+	
 	<input type="hidden" name="option" value="com_jem" />
 	<input type="hidden" name="view" value="export" />
 	<input type="hidden" name="controller" value="export" />
