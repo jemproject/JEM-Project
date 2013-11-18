4f72bcfd9149d635ac2b7cb4b26848da400d782b
Jaap Koelman <jaapkoelman@hotmail.com>
2013-11-18 23:49:57 +0100

    little bit of cleanup (helper file, backend)

--- admin/helpers/helper.php
+++ admin/helpers/helper.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version 1.9.1
+ * @version 1.9.5
  * @package JEM
  * @copyright (C) 2013-2013 joomlaeventmanager.net
  * @copyright (C) 2005-2009 Christoph Lukes
@@ -74,8 +74,6 @@ class JEMHelperBackend
 					);
 		}
 
-
-
 	}
 
 	/**
@@ -122,12 +120,5 @@ class JEMHelperBackend
 
 		return $options;
 	}
-
-
-
-
-
-
-
 
 }
