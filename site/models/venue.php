79b20ddd37a2dd1878548d7679a68908f6e2ba53
jojo12 <jojo.murer@bluewin.ch>
2013-12-05 10:57:09 +0100

    Update venue.php

--- site/models/venue.php
+++ site/models/venue.php
@@ -228,7 +228,7 @@ class JEMModelVenue extends JModelLegacy
 	{
 		$app = JFactory::getApplication();
 
-		$filter_order		= $app->getUserStateFromRequest('com_jem.venue.filter_order', 'filter_order', 'a.dates', 'cmd');
+		$filter_order		= $app->getUserStateFromRequest('com_jem.venue.filter_order', 'filter_order', '', 'cmd');
 		$filter_order_Dir	= $app->getUserStateFromRequest('com_jem.venue.filter_order_Dir', 'filter_order_Dir', '', 'word');
 
 		$filter_order		= JFilterInput::getInstance()->clean($filter_order, 'cmd');
