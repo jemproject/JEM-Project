e5a3666beda216fb6e55b95455e0e8d260c08615
Jaap Koelman <jaapkoelman@hotmail.com>
2013-12-13 17:47:41 +0100

    altered buildQuery (venue-model)

--- site/models/venues.php
+++ site/models/venues.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version 1.9.1
+ * @version 1.9.5
  * @package JEM
  * @copyright (C) 2013-2013 joomlaeventmanager.net
  * @copyright (C) 2005-2009 Christoph Lukes
@@ -166,25 +166,41 @@ class JEMModelVenues extends JModelLegacy
 	 */
 	function _buildQuery()
 	{
+		$user = JFactory::getUser();
+		$levels = $user->getAuthorisedViewLevels();
+		
 		//check archive task
 		$task 	= JRequest::getVar('task', '', '', 'string');
+		
+		
+		$where = array();
+		
+		
 		if($task == 'archive') {
-			$eventstate = ' AND a.published = 2';
+			$where[] = ' a.published = 2';
 		} else {
-			$eventstate = ' AND a.published = 1';
+			$where[] = ' a.published = 1';
 		}
+		
+		$where[] = ' c.access IN (' . implode(',', $levels) . ')';
+		$where[] = ' c.published = 1';
+		$where[] = ' l.published = 1';
+		
+		$where = (count($where) ? ' WHERE ' . implode(' AND ', $where) : '');
 
 		//get categories
-		$query = 'SELECT v.*, COUNT(a.id) AS assignedevents,'
-				. ' CASE WHEN CHAR_LENGTH(v.alias) THEN CONCAT_WS(\':\', v.id, v.alias) ELSE v.id END as slug'
-				. ' FROM #__jem_venues as v'
-				. ' LEFT JOIN #__jem_events AS a ON a.locid = v.id'
-				. ' WHERE v.published = 1'
-				. $eventstate
-				. ' GROUP BY v.id'
-				. ' ORDER BY v.venue'
+		$query = 'SELECT'
+				. ' l.locimage,l.locdescription,l.url,l.venue,l.street,l.city,l.country,l.postalCode,l.state,l.map,l.latitude,l.longitude,'
+				. ' CASE WHEN a.id IS NULL THEN 0 ELSE COUNT(a.id) END AS assignedevents,'
+				. ' CASE WHEN CHAR_LENGTH(l.alias) THEN CONCAT_WS(\':\', l.id, l.alias) ELSE l.id END as slug'
+				. ' FROM #__jem_events as a'
+				. ' LEFT JOIN #__jem_venues AS l ON l.id = a.locid'
+				. ' LEFT JOIN #__jem_cats_event_relations AS rel ON rel.itemid = a.id'
+				. ' LEFT JOIN #__jem_categories AS c ON c.id = rel.catid'					
+				. $where
+				. ' GROUP BY l.id'
+				. ' ORDER BY l.venue'
 				;
-
 		return $query;
 	}
 }
