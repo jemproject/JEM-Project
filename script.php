df3877bdfec730e98908a0855d79664f0e883cab
diesl <meine.anmeldungen.github@online.de>
2014-01-26 01:57:56 +0100

    Added install check requirement for magic quotes to be disabled

--- script.php
+++ script.php
@@ -197,6 +197,12 @@ class com_jemInstallerScript
 			return false;
 		}
 
+		// Abort if Magic Quotes are enabled
+		if(get_magic_quotes_gpc()) {
+			Jerror::raiseWarning(100, JText::_('COM_JEM_PREFLIGHT_MAGIC_QUOTES_ENABLED'));
+			return false;
+		}
+
 		// Minimum Joomla version as per Manifest file
 		$minJoomlaVersion = $parent->get('manifest')->attributes()->version;
 
