e2200deb89570dde2f9a9b6c46e35df9d5a43e55
JKoelman <jaapkoelman@hotmail.com>
2014-01-29 23:31:12 +0100

    cleanup edit_attachments (editvenue)

--- site/views/editvenue/tmpl/edit_attachments.php
+++ site/views/editvenue/tmpl/edit_attachments.php
@@ -1,13 +1,11 @@
 <?php
 /**
- * @version 1.9.5
+ * @version 1.9.6
  * @package JEM
  * @copyright (C) 2013-2013 joomlaeventmanager.net
  * @copyright (C) 2005-2009 Christoph Lukes
  * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  */
-
-
 defined ( '_JEXEC' ) or die;
 ?>
 
@@ -48,4 +46,3 @@ defined ( '_JEXEC' ) or die;
 		</tr>
 	</tbody>
 </table>
-
