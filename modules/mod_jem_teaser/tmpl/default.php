b53e8784d09b199dfc5cb4d4c55601095acee5ff
JKoelman <jaapkoelman@hotmail.com>
2014-01-30 12:23:23 +0100

    added flyermodal to teaser-module, FIX #525

--- modules/mod_jem_teaser/tmpl/default.php
+++ modules/mod_jem_teaser/tmpl/default.php
@@ -1,15 +1,20 @@
 <?php
 /**
- * @version 1.9.5
+ * @version 1.9.6
  * @package JEM
  * @subpackage JEM Teaser Module
  * @copyright (C) 2013-2013 joomlaeventmanager.net
  * @copyright (C) 2005-2009 Christoph Lukes
  * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  */
-
 defined('_JEXEC') or die;
-JHtml::_('behavior.modal', 'a.flyermodal');
+
+if ($params->get('use_modal', 0)) {
+	JHtml::_('behavior.modal', 'a.flyermodal');
+	$modal = 'flyermodal';
+} else {
+	$modal = 'notmodal';
+}
 ?>
 
 <div id="jemmoduleteaser">
@@ -52,23 +57,23 @@ JHtml::_('behavior.modal', 'a.flyermodal');
 					<?php if(($item->eventimage)!=str_replace("jpg","",($item->eventimage)) OR
 							 ($item->eventimage)!=str_replace("gif","",($item->eventimage)) OR
 							 ($item->eventimage)!=str_replace("png","",($item->eventimage))) : ?>
-						<a href="<?php echo $item->eventimageorig; ?>" class="modal-jem" title="<?php echo $item->title; ?> ">
+						<a href="<?php echo $item->eventimageorig; ?>" class="<?php echo $modal;?>" title="<?php echo $item->title; ?> ">
 						<img class="float_right image-preview" src="<?php echo $item->eventimage; ?>" alt="<?php echo $item->title; ?>" /></a>
 					<?php else : ?>
 					<?php endif; ?>
 						<?php if(($item->venueimage)!=str_replace("jpg","",($item->venueimage)) OR
 								 ($item->venueimage)!=str_replace("gif","",($item->venueimage)) OR
 								 ($item->venueimage)!=str_replace("png","",($item->venueimage))) : ?>
-							<a href="<?php echo $item->venueimageorig; ?>" class="modal-jem" title="<?php echo $item->venue; ?> ">
+							<a href="<?php echo $item->venueimageorig; ?>" class="<?php echo $modal;?>" title="<?php echo $item->venue; ?> ">
 							<img src="<?php echo $item->venueimage; ?>" alt="<?php echo $item->venue; ?>" class="float_right image-preview" /></a>
-						
+
 					<?php endif; ?>
 				</div>
 				<div>
 					<?php echo $item->eventdescription; ?>
 					<?php if (isset($item->link) && $item->readmore != 0 && $params->get('readmore')) :
 						echo '<a class="readmore" href="'.$item->link.'">'.$item->linkText.'</a>';
-						endif; 
+						endif;
 					?>
 				</div>
 			</div>
