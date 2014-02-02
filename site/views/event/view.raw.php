86f7d9ea475b31ac42e67e511ed03005394d3e0e
JKoelman <jaapkoelman@hotmail.com>
2014-02-02 07:50:15 +0100

    altered view.raw, REF #586

--- site/views/event/view.raw.php
+++ site/views/event/view.raw.php
@@ -6,7 +6,6 @@
  * @copyright (C) 2005-2009 Christoph Lukes
  * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  */
-
 defined('_JEXEC') or die;
 
 jimport( 'joomla.application.component.view');
@@ -25,21 +24,34 @@ class JemViewEvent extends JViewLegacy
 	 */
 	function display($tpl = null)
 	{
-		// Get data from the model
-		$row 				= $this->get('Item');
-		$row->categories 	= $this->get('Categories');
-		$row->id 			= $row->did;
-		$row->slug			= $row->alias ? ($row->id.':'.$row->alias) : $row->id;
+		$settings = JemHelper::globalattribs();
 
-		// initiate new CALENDAR
-		$vcal = JemHelper::getCalendarTool();
-		$vcal->setConfig( "filename", "event".$row->did.".ics" );
+		// check iCal global setting
+		if ($settings->get('global_show_ical_icon','0')==1) {
 
-		JemHelper::icalAddEvent($vcal, $row);
+			// Get data from the model
+			$row 				= $this->get('Item');
+			$row->categories 	= $this->get('Categories');
+			$row->id 			= $row->did;
+			$row->slug			= $row->alias ? ($row->id.':'.$row->alias) : $row->id;
+			$params				= $row->params;
 
-		// generate and redirect output to user browser
-		$vcal->returnCalendar();
-		echo $vcal->createCalendar(); // debug
+			// check individual iCal Event setting
+			if ($params->get('event_show_ical_icon',1)) {
+				// initiate new CALENDAR
+				$vcal = JemHelper::getCalendarTool();
+				$vcal->setConfig( "filename", "event".$row->did.".ics" );
+				JemHelper::icalAddEvent($vcal, $row);
+
+				// generate and redirect output to user browser
+				$vcal->returnCalendar();
+				echo $vcal->createCalendar(); // debug
+			} else {
+				return;
+			}
+		} else {
+			return;
+		}
 	}
 }
 ?>
