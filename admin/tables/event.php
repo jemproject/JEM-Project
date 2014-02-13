51b1777378867247f3475c7682169f6363b3d41a
Hoffi1 <Hoffi1@users.noreply.github.com>
2014-02-13 20:48:25 +0100

    backend: no import of time (issue #594)

--- admin/tables/event.php
+++ admin/tables/event.php
@@ -90,36 +90,48 @@ class JEMTableEvent extends JTable
 
 		$this->alias = JApplication::stringURLSafe($this->alias);
 
-		//get values from time selectlist and concatenate them accordingly
-		$starthours		= $jinput->get('starthours','','cmd');
-		$startminutes	= $jinput->get('startminutes','','cmd');
-		$endhours		= $jinput->get('endhours','','cmd');
-		$endminutes		= $jinput->get('endminutes','','cmd');
-
-		// StartTime
-		if ($starthours != '' && $startminutes != '') {
-			$this->times = $starthours.':'.$startminutes;
-		} else if ($starthours != '' && $startminutes == '') {
-			$startminutes = "00";
-			$this->times = $starthours.':'.$startminutes;
-		} else if ($starthours == '' && $startminutes != '') {
-			$starthours = "00";
-			$this->times = $starthours.':'.$startminutes;
+		if (($app->isAdmin()) && ($jinput->get('option','','cmd') == 'com_jem')
+		                      && ($jinput->get('view','','cmd') == 'import')
+		                      && ($jinput->get('controller','','cmd') == 'import')) {
+			// we are on Import page in backend so no data is given on url
+			if (empty($this->times)) {
+				$this->times = NULL;
+			}
+			if (empty($this->endtimes)) {
+				$this->endtimes = NULL;
+			}
 		} else {
-			$this->times = NULL;
-		}
+			//get values from time selectlist and concatenate them accordingly
+			$starthours		= $jinput->get('starthours','','cmd');
+			$startminutes	= $jinput->get('startminutes','','cmd');
+			$endhours		= $jinput->get('endhours','','cmd');
+			$endminutes		= $jinput->get('endminutes','','cmd');
 
-		// EndTime
-		if ($endhours != '' && $endminutes != '') {
-			$this->endtimes = $endhours.':'.$endminutes;
-		} else if ($endhours != '' && $endminutes == '') {
-			$endminutes = "00";
-			$this->endtimes = $endhours.':'.$endminutes;
-		} else if ($endhours == '' && $endminutes != '') {
-			$endhours = "00";
-			$this->endtimes = $endhours.':'.$endminutes;
-		} else {
-			$this->endtimes = NULL;
+			// StartTime
+			if ($starthours != '' && $startminutes != '') {
+				$this->times = $starthours.':'.$startminutes;
+			} else if ($starthours != '' && $startminutes == '') {
+				$startminutes = "00";
+				$this->times = $starthours.':'.$startminutes;
+			} else if ($starthours == '' && $startminutes != '') {
+				$starthours = "00";
+				$this->times = $starthours.':'.$startminutes;
+			} else {
+				$this->times = NULL;
+			}
+
+			// EndTime
+			if ($endhours != '' && $endminutes != '') {
+				$this->endtimes = $endhours.':'.$endminutes;
+			} else if ($endhours != '' && $endminutes == '') {
+				$endminutes = "00";
+				$this->endtimes = $endhours.':'.$endminutes;
+			} else if ($endhours == '' && $endminutes != '') {
+				$endhours = "00";
+				$this->endtimes = $endhours.':'.$endminutes;
+			} else {
+				$this->endtimes = NULL;
+			}
 		}
 
 		// Dates
