069bce4da4d92473d2823a7d287342eabed6c9ff
Jaap Koelman <jaapkoelman@hotmail.com>
2014-02-22 21:16:29 +0100

    increased copyright date component

--- media/js/recurrence.js
+++ media/js/recurrence.js
@@ -1,7 +1,7 @@
 /**
- * @version 1.9.5
+ * @version 1.9.6
  * @package JEM
- * @copyright (C) 2013-2013 joomlaeventmanager.net
+ * @copyright (C) 2013-2014 joomlaeventmanager.net
  * @copyright (C) 2005-2009 Christoph Lukes
  * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  * @author Sascha Karnatz
