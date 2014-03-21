f2686efd59b8a72fd20701404a2e4f0b5937aaf8
Hoffi1 <Hoffi1@users.noreply.github.com>
2014-03-21 22:11:41 +0100

    fix undefined property (issue #636)

--- site/classes/output.class.php
+++ site/classes/output.class.php
@@ -619,7 +619,7 @@ class JEMOutput {
 		$settings = JemHelper::globalattribs();
 
 		//stop if disabled
-		if (!$event->recurrence_number) {
+		if (empty($event->recurrence_number)) {
 			return;
 		}
 
