6fd2c244352a36ebfb9b915c00f87f1643f625d5
Jaap Koelman <jaapkoelman@hotmail.com>
2014-05-15 02:29:05 +0200

    merged functions+added more fields in query (model-eventslist)
    this one is related to #666

--- site/models/eventslist.php
+++ site/models/eventslist.php
@@ -12,12 +12,9 @@ defined('_JEXEC') or die;
 jimport('joomla.application.component.model');
 
 /**
- * JEM Component JEM Model
- *
- * @package JEM
- *
-*/
-class JEMModelEventslist extends JModelLegacy
+ * Model-Eventslist
+ **/
+class JemModelEventslist extends JModelLegacy
 {
 	/**
 	 * Events data array
@@ -79,26 +76,20 @@ class JEMModelEventslist extends JModelLegacy
 	 */
 	function &getData()
 	{
-		$pop	= JRequest::getBool('pop');
-
 		// Lets load the content if it doesn't already exist
 		if (empty($this->_data))
 		{
-			$query 		= $this->_buildQuery();
-			$pagination = $this->getPagination();
-
-			if ($pop) {
-				$this->_data = $this->_getList($query);
-			} else {
-				$pagination = $this->getPagination();
-				$this->_data = $this->_getList($query, $pagination->limitstart, $pagination->limit);
-			}
+			$query 			= $this->_buildQuery();
+			$pagination		= $this->getPagination();
+			$this->_data	= $this->_getList($query, $pagination->limitstart, $pagination->limit);
 		}
 
 		if($this->_data)
 		{
-			$this->_data = JEMHelper::getAttendeesNumbers($this->_data);
-
+			# attendee
+			$this->_data = JemHelper::getAttendeesNumbers($this->_data);
+			
+			# category
 			$count = count($this->_data);
 			for($i = 0; $i < $count; $i++)
 			{
@@ -116,25 +107,6 @@ class JEMModelEventslist extends JModelLegacy
 	}
 
 	/**
-	 * Total nr of events
-	 *
-	 * @access public
-	 * @return integer
-	 */
-	function getTotal()
-	{
-		// Lets load the total nr if it doesn't already exist
-		if (empty($this->_total))
-		{
-			$query = $this->_buildQuery();
-			$this->_total = $this->_getListCount($query);
-		}
-
-		return $this->_total;
-
-	}
-
-	/**
 	 * Method to get a pagination object for the events
 	 *
 	 * @access public
@@ -142,14 +114,22 @@ class JEMModelEventslist extends JModelLegacy
 	 */
 	function getPagination()
 	{
-		// Lets load the content if it doesn't already exist
-		if (empty($this->_pagination))
-		{
-			jimport('joomla.html.pagination');
-			$this->_pagination = new JPagination($this->getTotal(), $this->getState('limitstart'), $this->getState('limit'));
-		}
+		$jemsettings 		= JemHelper::config();
+		$app 				= JFactory::getApplication();
+		$itemid 			= JRequest::getInt('id', 0) . ':' . JRequest::getInt('Itemid', 0);
 
-		return $this->_pagination;
+		//get the number of events from database
+		$limit				= $app->getUserStateFromRequest('com_jem.eventslist.'.$itemid.'.limit', 'limit', $jemsettings->display_num, 'int');
+		$limitstart 		= $app->getUserStateFromRequest('com_jem.eventslist.'.$itemid.'.limitstart', 'limitstart', 0, 'int');
+		
+		$query 				= $this->_buildQuery();
+		$total				= $this->_getListCount($query);
+		
+		// Create the pagination object
+		jimport('joomla.html.pagination');
+		$pagination = new JPagination($total, $limitstart, $limit);
+		
+		return $pagination;	
 	}
 
 	/**
@@ -160,44 +140,23 @@ class JEMModelEventslist extends JModelLegacy
 	 */
 	protected function _buildQuery()
 	{
-		// Get the WHERE and ORDER BY clauses for the query
-		$where		= $this->_buildWhere();
-		$orderby	= $this->_buildOrderBy();
-
-		// Get Events from Database ...
-		$query = ' SELECT a.id, a.dates, a.registra, a.featured, a.datimage, a.enddates, a.times, a.endtimes, a.title, a.created, a.locid, a.maxplaces, a.waitinglist, a.fulltext, '
-				. ' a.recurrence_number, a.recurrence_first_id, a.recurrence_type, '
-				. ' l.venue, l.city, l.state, l.url, l.street, l.postalCode, ct.name AS countryname, '
-				. ' c.catname, c.id AS catid,'
-				. ' CASE WHEN CHAR_LENGTH(a.alias) THEN CONCAT_WS(\':\', a.id, a.alias) ELSE a.id END as slug,'
-				. ' CASE WHEN CHAR_LENGTH(l.alias) THEN CONCAT_WS(\':\', a.locid, l.alias) ELSE a.locid END as venueslug'
-				. ' FROM #__jem_events AS a'
-				. ' LEFT JOIN #__jem_venues AS l ON l.id = a.locid'
-				. ' LEFT JOIN #__jem_countries AS ct ON ct.iso2 = l.country '
-				. ' LEFT JOIN #__jem_cats_event_relations AS rel ON rel.itemid = a.id'
-				. ' LEFT JOIN #__jem_categories AS c ON c.id = rel.catid'
-				. $where
-				. ' GROUP BY a.id'
-				. $orderby
-				;
-
-				return $query;
-	}
-
-	/**
-	 * Build the order clause
-	 *
-	 * @access private
-	 * @return string
-	 */
-
-	protected function _buildOrderBy()
-	{
 		$app 			= JFactory::getApplication();
 		$jinput 		= JFactory::getApplication()->input;
 		$task 			= $jinput->get('task','','cmd');
 		$itemid 		= JRequest::getInt('id', 0) . ':' . JRequest::getInt('Itemid', 0);
-
+		
+		$params 		= $app->getParams();
+		$settings 		= JemHelper::globalattribs();
+		$user 			= JFactory::getUser();
+		// Support Joomla access levels instead of single group id
+		$levels 		= $user->getAuthorisedViewLevels();
+		$catswitch 		= $params->get('categoryswitch', '0');
+		
+		// Userstate variables
+		$filter 		= $app->getUserStateFromRequest('com_jem.eventslist.'.$itemid.'.filter', 'filter', '', 'int');
+		$search 		= $app->getUserStateFromRequest('com_jem.eventslist.'.$itemid.'.filter_search', 'filter_search', '', 'string');
+		$search 		= $this->_db->escape(trim(JString::strtolower($search)));
+		
 		$filter_order		= $app->getUserStateFromRequest('com_jem.eventslist.'.$itemid.'.filter_order', 'filter_order', 'a.dates', 'cmd');
 		$filter_order_DirDefault = 'ASC';
 		// Reverse default order for dates in archive mode
@@ -205,46 +164,52 @@ class JEMModelEventslist extends JModelLegacy
 			$filter_order_DirDefault = 'DESC';
 		}
 		$filter_order_Dir	= $app->getUserStateFromRequest('com_jem.eventslist.'.$itemid.'.filter_order_Dir', 'filter_order_Dir', $filter_order_DirDefault, 'word');
-
 		$filter_order		= JFilterInput::getInstance()->clean($filter_order, 'cmd');
 		$filter_order_Dir	= JFilterInput::getInstance()->clean($filter_order_Dir, 'word');
-
-		if ($filter_order == 'a.dates') {
-			$orderby = ' ORDER BY a.dates ' . $filter_order_Dir .', a.times ' . $filter_order_Dir;
-		} else {
-			$orderby = ' ORDER BY ' . $filter_order . ' ' . $filter_order_Dir;
-		}
-
-		return $orderby;
-	}
-
-	/**
-	 * Build the where clause
-	 *
-	 * @access private
-	 * @return string
-	 */
-	protected function _buildWhere()
-	{
-		$app 			= JFactory::getApplication();
-		$jinput 		= JFactory::getApplication()->input;
-		$task 			= $jinput->get('task','','cmd');
-		$itemid 		= JRequest::getInt('id', 0) . ':' . JRequest::getInt('Itemid', 0);
-
-		$params 		= $app->getParams();
-		$settings 		= JEMHelper::globalattribs();
-		$user 			= JFactory::getUser();
-		// Support Joomla access levels instead of single group id
-		$levels = $user->getAuthorisedViewLevels();
-
-		$catswitch 		= $params->get('categoryswitch', '0');
-
-		$filter 		= $app->getUserStateFromRequest('com_jem.eventslist.'.$itemid.'.filter', 'filter', '', 'int');
-		$search 		= $app->getUserStateFromRequest('com_jem.eventslist.'.$itemid.'.filter_search', 'filter_search', '', 'string');
-		$search 		= $this->_db->escape(trim(JString::strtolower($search)));
-
+			
+		// Query
+		$db 	= JFactory::getDBO();
+		$query = $db->getQuery(true);
+		
+		$case_when_e = ' CASE WHEN ';
+		$case_when_e .= $query->charLength('a.alias');
+		$case_when_e .= ' THEN ';
+		$id_e = $query->castAsChar('a.id');
+		$case_when_e .= $query->concatenate(array($id_e, 'a.alias'), ':');
+		$case_when_e .= ' ELSE ';
+		$case_when_e .= $id_e.' END as slug';
+		
+		$case_when_l = ' CASE WHEN ';
+		$case_when_l .= $query->charLength('l.alias');
+		$case_when_l .= ' THEN ';
+		$id_l = $query->castAsChar('a.locid');
+		$case_when_l .= $query->concatenate(array($id_l, 'l.alias'), ':');
+		$case_when_l .= ' ELSE ';
+		$case_when_l .= $id_l.' END as venueslug';
+		
+		# event
+		$query->select(array('a.access','a.alias','a.author_ip','a.checked_out','a.checked_out_time','a.contactid','a.created','a.created_by','a.created_by_alias','a.custom1','a.custom2','a.custom3','a.custom4','a.custom5','a.custom6','a.custom7','a.custom8','a.custom9','a.custom10','a.dates','a.datimage','a.enddates','a.endtimes','a.featured'));
+		$query->select(array('a.fulltext','a.hits','a.id','a.introtext','a.language','a.locid','a.maxplaces','a.metadata','a.meta_keywords','a.meta_description','a.modified','a.modified_by','a.published','a.registra','a.times','a.title','a.unregistra','a.waitinglist'));
+		$query->select(array('a.recurrence_byday','a.recurrence_counter','a.recurrence_first_id','a.recurrence_limit','a.recurrence_limit_date','a.recurrence_number', 'a.recurrence_type','a.version'));
+		# venue
+		$query->select(array('l.alias AS l_alias','l.author_ip AS l_authorip','l.checked_out AS l_checked_out','l.checked_out_time AS l_checked_out_time','l.city','l.country','l.created AS l_created','l.created_by AS l_createdby'));
+		$query->select(array('l.custom1 AS l_custom1','l.custom2 AS l_custom2','l.custom3 AS l_custom3','l.custom4 AS l_custom4','l.custom5 AS l_custom5','l.custom6 AS l_custom6','l.custom7 AS l_custom7','l.custom8 AS l_custom8','l.custom9 AS l_custom9','l.custom10 AS l_custom10'));
+		$query->select(array('l.id AS l_id','l.latitude','l.locdescription','l.locimage','l.longitude','l.map','l.meta_description','l.meta_keywords','l.modified AS l_modified','l.modified_by AS l_modified_by','l.ordering','l.postalCode','l.publish_up','l.publish_down','l.published AS l_published','l.state','l.street','l.url','l.venue','l.version AS l_version'));
+		# country
+		$query->select(array('ct.name AS countryname'));
+		# category
+		$query->select(array('c.access AS c_access','c.alias AS c_alias','c.catname','c.color','c.description AS c_description','c.id AS catid','c.image','c.published AS c_published'));
+		# the rest
+		$query->select(array($case_when_e, $case_when_l));
+		$query->from('#__jem_events as a');
+		$query->join('LEFT', '#__jem_venues AS l ON l.id = a.locid');
+		$query->join('LEFT', '#__jem_countries AS ct ON ct.iso2 = l.country');
+		$query->join('LEFT', '#__jem_cats_event_relations AS rel ON rel.itemid = a.id');
+		$query->join('LEFT', '#__jem_categories AS c ON c.id = rel.catid');
+				
+		// where
 		$where = array();
-
+		
 		// First thing we need to do is to select only needed events
 		if ($task == 'archive') {
 			$where[] = ' a.published = 2';
@@ -253,28 +218,28 @@ class JEMModelEventslist extends JModelLegacy
 		}
 		$where[] = ' c.published = 1';
 		$where[] = ' c.access IN (' . implode(',', $levels) . ')';
-
+		
 		// get included categories
 		if ($catswitch == 1) {
 			$included_cats = trim($params->get('categoryswitchcats', ''));
-
+		
 			if ($included_cats != '') {
 				$cats_included = explode(',', $included_cats);
 				$where [] = '  (c.id=' . implode(' OR c.id=', $cats_included) . ')';
 			}
 		}
-
+		
 		// get excluded categories
 		if ($catswitch == 0) {
 			$excluded_cats = trim($params->get('categoryswitchcats', ''));
-
+		
 			if ($excluded_cats != '') {
 				$cats_excluded = explode(',', $excluded_cats);
 				$where [] = '  (c.id!=' . implode(' AND c.id!=', $cats_excluded) . ')';
 			}
 		}
 		// === END Excluded categories add === //
-
+		
 		if ($settings->get('global_show_filter') && $search) {
 			switch($filter) {
 				case 1:
@@ -294,68 +259,83 @@ class JEMModelEventslist extends JModelLegacy
 					$where[] = ' LOWER(l.state) LIKE \'%'.$search.'%\' ';
 			}
 		}
-
-		$where = (count($where) ? ' WHERE ' . implode(' AND ', $where) : '');
-
-		return $where;
+		$query->where($where);
+		
+		// Group
+		$query->group('a.id');
+		
+		// ordering
+		if ($filter_order == 'a.dates') {
+			$orderby = array('a.dates '.$filter_order_Dir,'a.times '.$filter_order_Dir);
+		} else {
+			$orderby = $filter_order . ' ' . $filter_order_Dir;
+		}
+		$query->order($orderby);
+				
+		return $query;
 	}
+
+
 
 	function getCategories($id)
 	{
-		$user = JFactory::getUser();
+		$user 			= JFactory::getUser();
 		// Support Joomla access levels instead of single group id
-		$levels = $user->getAuthorisedViewLevels();
-
-		$where		= $this->_buildWhere2();
-
-		$query = 'SELECT DISTINCT c.id, c.catname, c.access, c.checked_out AS cchecked_out,'
-				. ' CASE WHEN CHAR_LENGTH(c.alias) THEN CONCAT_WS(\':\', c.id, c.alias) ELSE c.id END as catslug'
-				. ' FROM #__jem_categories AS c'
-				. ' LEFT JOIN #__jem_cats_event_relations AS rel ON rel.catid = c.id'
-				. ' WHERE rel.itemid = '.(int)$id
-				. ' AND c.published = 1'
-				. ' AND c.access IN (' . implode(',', $levels) . ')'
-				. $where
-				;
-
-		$this->_db->setQuery($query);
-		$this->_cats = $this->_db->loadObjectList();
-
-		return $this->_cats;
-	}
-
-	protected function _buildWhere2()
-	{
-		$app = JFactory::getApplication();
-
+		$levels 		= $user->getAuthorisedViewLevels();
+		$app 			= JFactory::getApplication();
+		
 		// Get the paramaters of the active menu item
-		$params 	= $app->getParams();
-		$catswitch = $params->get('categoryswitch', '0');
-
+		$params 		= $app->getParams();
+		$catswitch 		= $params->get('categoryswitch', '0');
+		
+		// Query
+		$db 	= JFactory::getDBO();
+		$query = $db->getQuery(true);
+		
+		$case_when_c = ' CASE WHEN ';
+		$case_when_c .= $query->charLength('c.alias');
+		$case_when_c .= ' THEN ';
+		$id_c = $query->castAsChar('c.id');
+		$case_when_c .= $query->concatenate(array($id_c, 'c.alias'), ':');
+		$case_when_c .= ' ELSE ';
+		$case_when_c .= $id_c.' END as catslug';
+		
+		$query->select(array('DISTINCT c.id','c.catname','c.access','c.checked_out AS cchecked_out',$case_when_c));
+		$query->from('#__jem_categories as c');
+		$query->join('LEFT', '#__jem_cats_event_relations AS rel ON rel.catid = c.id');
+		
+		// where
+		$where = array();
+		
+		$where[] = 'rel.itemid ='.(int)$id;
+		$where[] = 'c.published = 1';
+		$where[] = 'c.access IN ('.implode(',',$levels).')';
+		
 		// get included categories
 		if ($catswitch == 1) {
 			$included_cats = trim($params->get('categoryswitchcats', ''));
 			if ($included_cats != '') {
 				$cats_included = explode(',', $included_cats);
-				$where = ' AND (c.id=' . implode(' OR c.id=', $cats_included) . ')';
-			} else {		// === END Exlucded categories add === //
-				$where = '';
+				$where[] = ' (c.id=' . implode(' OR c.id=', $cats_included) . ')';
 			}
 		}
-
+		
 		// get excluded categories
 		if ($catswitch == 0) {
 			$excluded_cats = trim($params->get('categoryswitchcats', ''));
-
+		
 			if ($excluded_cats != '') {
 				$cats_excluded = explode(',', $excluded_cats);
-				$where = ' AND (c.id!=' . implode(' AND c.id!=', $cats_excluded) . ')';
-			} else {		// === END Exlucded categories add === //
-				$where = '';
+				$where[] = ' (c.id!=' . implode(' AND c.id!=', $cats_excluded) . ')';
 			}
 		}
+		
+		$query->where($where);
+		
+		$db->setQuery($query);
+		$cats = $db->loadObjectList();
 
-		return $where;
+		return $cats;
 	}
 }
 ?>
