c19bf1b8001caf742849434ca432b25afb6cb3fe
JKoelman <jaapkoelman@hotmail.com>
2014-05-17 19:04:11 +0200

    updated model-venue code

--- site/models/venue.php
+++ site/models/venue.php
@@ -20,7 +20,7 @@ class JemModelVenue extends JemModelEventslist
 		$app 			= JFactory::getApplication();
 		$jinput			= $app->input;
 		$params			= $app->getParams();
-		
+
 		# determing the id to load
 		if ($jinput->get('id',null,'int')) {
 			$id = $jinput->get('id',null,'int');
@@ -28,7 +28,7 @@ class JemModelVenue extends JemModelEventslist
 			$id = $params->get('id');
 		}
 		$this->setId((int)$id);
-		
+
 		parent::__construct();
 	}
 
@@ -38,52 +38,50 @@ class JemModelVenue extends JemModelEventslist
 	protected function populateState($ordering = null, $direction = null)
 	{
 		// parent::populateState($ordering, $direction);
-	
+
 		$app 			= JFactory::getApplication();
 		$jemsettings	= JemHelper::config();
 		$jinput			= JFactory::getApplication()->input;
 		$itemid 		= JRequest::getInt('id', 0) . ':' . JRequest::getInt('Itemid', 0);
 		$params 		= $app->getParams();
 		$task           = $jinput->get('task','','cmd');
-		
+
 		// List state information
 		$limitstart = $app->getUserStateFromRequest('com_jem.venue.'.$itemid.'.limitstart', 'limitstart', 0, 'int');
 		$this->setState('list.start', $limitstart);
-	
+
 		$limit		= $app->getUserStateFromRequest('com_jem.venue.'.$itemid.'.limit', 'limit', $jemsettings->display_num, 'int');
 		$this->setState('list.limit', $limit);
 
 		# Search
 		$search = $app->getUserStateFromRequest('com_jem.venue.'.$itemid.'.filter_search', 'filter_search', '', 'string');
 		$this->setState('filter.filter_search', $search);
-		
+
 		# FilterType
 		$filtertype = $app->getUserStateFromRequest('com_jem.venue.'.$itemid.'.filter_type', 'filter_type', '', 'int');
 		$this->setState('filter.filter_type', $filtertype);
-		
+
 		# filter_order
 		$orderCol = $app->getUserStateFromRequest('com_jem.venue.'.$itemid.'.filter_order', 'filter_order', 'a.dates', 'cmd');
 		$this->setState('filter.filter_ordering', $orderCol);
-		
+
 		# filter_direction
 		$listOrder = $app->getUserStateFromRequest('com_jem.venue.'.$itemid.'.filter_order_Dir', 'filter_order_Dir', 'ASC', 'word');
 		$this->setState('filter.filter_direction', $listOrder);
-		
+
 		# params
 		$this->setState('params', $params);
-	
+
 		if ($task == 'archive') {
-			$archived = array('a.published = 2','a.locid = '.$this->_id);
-			$this->setState('filter.archived',$archived);
+			$this->setState('filter.published',2);
 		} else {
-			$published = array('a.published = 1', 'a.locid = '.$this->_id);
-			$this->setState('filter.published',$published);
+			$this->setState('filter.published',1);
 		}
-		
+
 		$this->setState('filter.groupby',array('a.id'));
 	}
-	
-	
+
+
 	/**
 	 * Method to get a list of events.
 	 */
@@ -91,20 +89,18 @@ class JemModelVenue extends JemModelEventslist
 	{
 		$params = clone $this->getState('params');
 		$items	= parent::getItems();
-		
+
 		if ($items) {
 			foreach ($items as &$item)
 			{
-			# adding categories
-			$item->categories = $this->getCategories($item->id);
+
 			}
-			$items	= JemHelper::getAttendeesNumbers($items);
 		}
-		
+
 		return $items;
 	}
-	
-	
+
+
 	/**
 	 * @return	JDatabaseQuery
 	 */
@@ -113,18 +109,20 @@ class JemModelVenue extends JemModelEventslist
 		$params  = $this->state->params;
 		$jinput  = JFactory::getApplication()->input;
 		$task    = $jinput->get('task','','cmd');
-		
+
 		// Create a new query object.
 		$query = parent::getListQuery();
-		
-		// here we can extend the query of the Eventslist model 
+
+		$query->where('a.locid = '.$this->_id);
+
+		// here we can extend the query of the Eventslist model
 		return $query;
 	}
-	
-	
+
+
 	/**
 	 * Method to set the venue id
-	 * 
+	 *
 	 * The venue-id can be set by a menu-parameter
 	 */
 	function setId($id)
@@ -175,41 +173,14 @@ class JemModelVenue extends JemModelEventslist
 
 		$db->setQuery($query);
 		$_venue = $db->loadObject();
-		
+
 		if (empty($_venue)) {
 			return JError::raiseError(404, JText::_('COM_JEM_VENUE_NOTFOUND'));
 		}
-		
+
 		$_venue->attachments = JEMAttachment::getAttachments('venue'.$_venue->id);
-				
+
 		return $_venue;
-	}
-
-	
-	/**
-	 * multi-cat 
-	 */
-	function getCategories($id)
-	{
-		$user = JFactory::getUser();
-		$levels = $user->getAuthorisedViewLevels();
-
-		$db = JFactory::getDbo();
-		$query = $db->getQuery(true);
-
-		$query->select('DISTINCT c.id, c.catname, c.access, c.color, c.checked_out AS cchecked_out,'
-				. ' CASE WHEN CHAR_LENGTH(c.alias) THEN CONCAT_WS(\':\', c.id, c.alias) ELSE c.id END as catslug');
-		$query->from('#__jem_categories AS c');
-		$query->join('LEFT', '#__jem_cats_event_relations AS rel ON rel.catid = c.id');
-		$query->where('rel.itemid = ' . (int) $id);
-		$query->where('c.published = 1');
-		$query->where('c.access IN (' . implode(',', $levels) . ')');
-		$query->group('c.id');
-
-		$db->setQuery($query);
-		$cats = $db->loadObjectList();
-
-		return $cats;
 	}
 }
 ?>
