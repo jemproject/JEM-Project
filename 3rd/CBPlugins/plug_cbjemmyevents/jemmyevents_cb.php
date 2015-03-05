0b46f0c9d18c1d09ce5c836e63aed81ef4f535cc
jojo12 <jojo.murer@bluewin.ch>
2015-03-05 14:18:19 +0100

    Update jemmyevents_cb.php

--- 3rd/CBPlugins/plug_cbjemmyevents/jemmyevents_cb.php
+++ 3rd/CBPlugins/plug_cbjemmyevents/jemmyevents_cb.php
@@ -7,10 +7,7 @@
  * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU/GPL version 2
  */
  
-if (! (defined('_VALID_CB') || defined('_JEXEC') || defined('_VALID_MOS')))
-{
-	die();
-}
+if ( ! ( defined( '_VALID_CB' ) || defined( '_JEXEC' ) || defined( '_VALID_MOS' ) ) ) { die( 'Direct Access to this location is not allowed.' ); }
 
 @include_once (JPATH_SITE.'/components/com_jem/classes/image.class.php');
 @include_once (JPATH_SITE.'/components/com_jem/classes/Zebra_Image.php');
