38467b95401abc89fde412e3a0497e2570810ce4
Hoffi1 <Hoffi1@users.noreply.github.com>
2015-05-22 00:36:53 +0200

    backend Edit attendee: add notice that changing user will unregister old one (hotfix #932)

--- admin/views/attendee/tmpl/default.php
+++ admin/views/attendee/tmpl/default.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @version 2.0.0
+ * @version 2.1.4
  * @package JEM
- * @copyright (C) 2013-2014 joomlaeventmanager.net
+ * @copyright (C) 2013-2015 joomlaeventmanager.net
  * @copyright (C) 2005-2009 Christoph Lukes
  * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  */
@@ -47,42 +47,45 @@ $selectuser_link = JRoute::_('index.php?option=com_jem&task=attendee.selectuser&
 
 <form action="<?php echo JRoute::_('index.php?option=com_jem&view=attendee'); ?>" method="post" name="adminForm" id="adminForm" class="form-validate">
 	<fieldset><legend><?php echo JText::_('COM_JEM_DETAILS'); ?></legend>
-	<table  class="admintable">
-		<tr>
-			<td class="key" width="150">
-				<label for="uid">
-					<?php echo JText::_('COM_JEM_USER').':'; ?>
-				</label>
-			</td>
-			<td>
-				<input type="text" name="username" id="username" readonly="readonly" value="<?php echo $this->row->username; ?>" />
-				<input type="hidden" name="uid" id="uid" value="<?php echo $this->row->uid; ?>" />
-				<a class="usermodal" title="<?php echo JText::_('COM_JEM_SELECT_USER'); ?>" href="<?php echo $selectuser_link; ?>" rel="{handler: 'iframe', size: {x: 800, y: 500}}">
-					<span><?php echo JText::_('COM_JEM_SELECT_USER')?></span>
-		</a>
-			</td>
-		</tr>
-		<?php if (1/*!$this->row->id*/): ?>
-		<tr>
-			<td class="key" width="150">
-				<label for="sendemail">
-					<?php echo JText::_('COM_JEM_SEND_REGISTRATION_NOTIFICATION_EMAIL').':'; ?>
-				</label>
-			</td>
-			<td>
-				<input type="checkbox" name="sendemail" value="1" checked="checked"/>
-			</td>
-		</tr>
+		<?php if (!empty($this->row->id)) : ?>
+		<p>
+			<?php echo JText::_('COM_JEM_EDITATTENDEE_NOTICE'); ?>
+		</p>
 		<?php endif; ?>
-	</table>
+		<table  class="admintable">
+			<tr>
+				<td class="key" width="150">
+					<label for="uid">
+						<?php echo JText::_('COM_JEM_USER').':'; ?>
+					</label>
+				</td>
+				<td>
+					<input type="text" name="username" id="username" readonly="readonly" value="<?php echo $this->row->username; ?>" />
+					<input type="hidden" name="uid" id="uid" value="<?php echo $this->row->uid; ?>" />
+					<a class="usermodal" title="<?php echo JText::_('COM_JEM_SELECT_USER'); ?>" href="<?php echo $selectuser_link; ?>" rel="{handler: 'iframe', size: {x: 800, y: 500}}">
+						<span><?php echo JText::_('COM_JEM_SELECT_USER')?></span>
+					</a>
+				</td>
+			</tr>
+			<?php if (1/*!$this->row->id*/): ?>
+			<tr>
+				<td class="key" width="150">
+					<label for="sendemail">
+						<?php echo JText::_('COM_JEM_SEND_REGISTRATION_NOTIFICATION_EMAIL').':'; ?>
+					</label>
+				</td>
+				<td>
+					<input type="checkbox" name="sendemail" value="1" checked="checked"/>
+				</td>
+			</tr>
+			<?php endif; ?>
+		</table>
 	</fieldset>
 
-<?php
-echo JHtml::_('form.token');
-?>
-<input type="hidden" name="id" value="<?php echo $this->row->id; ?>" />
-<input type="hidden" name="event" value="<?php echo ($this->row->event ? $this->row->event : $this->event); ?>" />
-<input type="hidden" name="task" value="" />
+	<?php echo JHtml::_('form.token'); ?>
+	<input type="hidden" name="id" value="<?php echo $this->row->id; ?>" />
+	<input type="hidden" name="event" value="<?php echo ($this->row->event ? $this->row->event : $this->event); ?>" />
+	<input type="hidden" name="task" value="" />
 </form>
 
 
