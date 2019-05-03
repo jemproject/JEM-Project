0c2e95fd3cab502961bc8edd66c305fd43a38cd5
Egnarts94 <eggiegnarts@gmx.de>
2019-05-03 13:49:49 +0200

    Update admin to dev23

--- admin/views/settings/tmpl/default_evregistration.php
+++ admin/views/settings/tmpl/default_evregistration.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @version 2.0.0
+ * @version 2.3.0-dev3
  * @package JEM
- * @copyright (C) 2013-2014 joomlaeventmanager.net
+ * @copyright (C) 2013-2019 joomlaeventmanager.net
  * @copyright (C) 2005-2009 Christoph Lukes
  * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  */
@@ -15,8 +15,8 @@ $group = 'globalattribs';
 	<fieldset class="adminform">
 		<legend><?php echo JText::_('COM_JEM_REGISTRATION'); ?></legend>
 		<ul class="adminformlist">
-			<li><?php echo $this->form->getLabel('event_show_more_attendeedetails',$group); ?> <?php echo $this->form->getInput('event_show_more_attendeedetails',$group); ?></li>
 			<li><?php echo $this->form->getLabel('event_show_attendeenames',$group); ?> <?php echo $this->form->getInput('event_show_attendeenames',$group); ?></li>
+			<li><?php echo $this->form->getLabel('event_show_more_attendeedetails',$group); ?> <?php echo $this->form->getInput('event_show_more_attendeedetails',$group); ?></li>
 			<li><?php echo $this->form->getLabel('event_comunsolution',$group); ?> <?php echo $this->form->getInput('event_comunsolution',$group); ?></li>
 			<li id="comm1" style="display:none"><?php echo $this->form->getLabel('event_comunoption',$group); ?> <?php echo $this->form->getInput('event_comunoption',$group); ?></li>
 		</ul>
