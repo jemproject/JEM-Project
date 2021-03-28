ab05d737f2e344538436ed664ff566906f505375
jojo12 <jojo.murer@bluewin.ch>
2021-03-28 17:12:38 +0200

    Add files via upload
    corrected some layout errors

--- site/views/venueslist/tmpl/default_venues.php
+++ site/views/venueslist/tmpl/default_venues.php
@@ -1,85 +1,123 @@
 <?php
 /**
- * @version 2.3.0
+ * @version 2.1.5
  * @package JEM
- * @copyright (C) 2013-2020 joomlaeventmanager.net
+ * @copyright (C) 2013-2015 joomlaeventmanager.net
  * @copyright (C) 2005-2009 Christoph Lukes
  * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  */
 defined('_JEXEC') or die;
+JHtml::_('behavior.tooltip');							 
 ?>
-
+	<?php /*?>	
+<style>
+div#jem_filter select {
+    width: auto;
+    margin-right:10px;
+    border: 1px solid #808080;
+	background-color: #C6CCBE;
+	cursor: pointer;
+}
+</style>
+	<?php */?>	
 
 <form action="<?php echo htmlspecialchars($this->action); ?>" method="post" id="adminForm" name="adminForm">
-<?php if ($this->settings->get('global_show_filter',1) || $this->settings->get('global_display',1)) : ?>
-	
-<div id="jem_filter" class="floattext">	
-	<?php if ($this->settings->get('global_show_filter',1)) : ?>
-	<div class="pull-left">
-			<?php
-			echo '<label for="filter">'.JText::_('COM_JEM_FILTER').'</label>&nbsp;';
-			echo $this->lists['filter'].'&nbsp;';
-			?>
-			<input type="text" name="filter_search" id="filter_search" value="<?php echo $this->lists['search'];?>" class="inputbox" onchange="document.adminForm.submit();" />
-			<button class="buttonfilter" type="submit"><?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?></button>
-			<button class="buttonfilter" type="button" onclick="document.id('filter_search').value='';this.form.submit();"><?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?></button>
-		</div>
-		<?php endif; ?>	
-	
 
-
-	<?php if ($this->settings->get('global_display',1)) : ?>
-	<div class="pull-right">
-			<?php
-
-			echo $this->pagination->getLimitBox();
-			?>
+<?php
+function jem_common_show_filter(&$obj) {
+  if ($obj->settings->get('global_show_filter',1) && !JemHelper::jemStringContains($obj->params->get('pageclass_sfx'), 'jem-hidefilter')) {
+    return true;
+  }
+  if (JemHelper::jemStringContains($obj->params->get('pageclass_sfx'), 'jem-showfilter')) {
+    return true;
+  }
+  return false;
+}
+?>
+<?php if (jem_common_show_filter($this) && !JemHelper::jemStringContains($this->params->get('pageclass_sfx'), 'jem-filterbelow')): ?>
+  <div id="jem_filter" class="floattext jem-form jem-row jem-justify-start">
+    <div>
+      <?php echo '<label for="filter">'.JText::_('COM_JEM_FILTER').'</label>'; ?>
+    </div>
+    <div class="jem-row jem-justify-start jem-nowrap">
+      <?php echo $this->lists['filter']; ?>
+      <input type="text" name="filter_search" id="filter_search" value="<?php echo $this->lists['search'];?>" class="inputbox" onchange="document.adminForm.submit();" />
+    </div>
+    <div class="jem-row jem-justify-start jem-nowrap">
+      <button class="buttonfilter btn btn-primary" type="submit"><?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?></button>
+      <button class="buttonfilter btn btn-secondary" type="button" onclick="document.id('filter_search').value='';this.form.submit();"><?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?></button> 
+    </div>
+	  	<?php if ($this->settings->get('global_display',1)) : ?>
+	<div class="jem_fright">
+		   
+		<label for="limit"><?php echo JText::_('COM_JEM_DISPLAY_NUM'); ?></label>
+		<?php echo $this->pagination->getLimitBox(); ?>
+		
 	</div>
 	<?php endif; ?>
-</div>	
-	
+  </div>
+
 <?php endif; ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	<div class="table-responsive">
-	<table class="eventtable table" style="width:<?php echo $this->jemsettings->tablewidth; ?>;" summary="Venues">
+	<table class="eventtable" style="width:<?php echo $this->jemsettings->tablewidth; ?>;" summary="Venues">
 		<colgroup>
-			<col width="5%" class="jem_col_city" />
-			<col width="1%" class="jem_col_state" />
-
-
-			
-			<?php if ($this->jemsettings->showlocate == 1) :	?>
+			<col width="20%" class="jem_col_city" />
+	<?php if ($this->params->get('showstate')) : ?>			
+			<col width="20%" class="jem_col_state" />
+	<?php endif; ?>	
 			<col width="<?php echo $this->jemsettings->locationwidth; ?>" class="jem_col_venue" />
-			<?php endif; ?>
+
 		</colgroup>
 		<thead>
 			<tr>
 				<th id="jem_city" class="sectiontableheader" align="left"><?php echo JHtml::_('grid.sort', 'COM_JEM_TABLE_CITY', 'a.city', $this->lists['order_Dir'], $this->lists['order']); ?></th>
+	<?php if ($this->params->get('showstate')) : ?>
 				<th id="jem_state" class="sectiontableheader" align="left"><?php echo JHtml::_('grid.sort', 'COM_JEM_TABLE_STATE', 'a.state', $this->lists['order_Dir'], $this->lists['order']); ?></th>
+	<?php endif; ?>	
 			
 			
-				<?php if ($this->jemsettings->showlocate == 1) : ?>
+
 				<th id="jem_location" class="sectiontableheader" align="left"><?php echo JHtml::_('grid.sort', 'COM_JEM_TABLE_LOCATION', 'a.venue', $this->lists['order_Dir'], $this->lists['order']); ?></th>
-				<?php endif; ?>
+
 
 
 			</tr>
 		</thead>
 
 		<tbody>
-		<?php if (count((array)$this->items) == 0) : ?>
-			<tr align="center"><td colspan="0"><?php echo JText::_('COM_JEM_NO_VENUES'); ?></td></tr>
-		<?php else :?>
-			<?php foreach ($this->items as $i => $row) : ?>
-				<tr class="row<?php echo $i % 2; ?>">
-				
-		
-				
+			<?php if (empty($this->rows)) : ?>
+				<tr class="no_events"><td colspan="20"><?php echo JText::_('COM_JEM_NO_VENUES'); ?></td></tr>
+			<?php else : ?>
+				<?php $odd = 0; ?>
+				<?php foreach ($this->rows as $row) : ?>
+					<?php $odd = 1 - $odd; ?>
 					<td headers="jem_city" align="left" valign="top"><?php echo $row->city ? $this->escape($row->city) : '-'; ?></td>
+				<?php if ($this->params->get('showstate')) : ?>
 					<td headers="jem_state" align="left" valign="top">
 						<?php echo !empty($row->state) ? $this->escape($row->state) : '-'; ?>
 					</td>	
+				<?php endif; ?>		
 				
-					<?php if ($this->jemsettings->showlocate == 1) : ?>
 					<td headers="jem_location" align="left" valign="top">
 						<?php
 						if ($this->jemsettings->showlinkvenue == 1) :
@@ -89,19 +127,19 @@ defined('_JEXEC') or die;
 						endif;
 						?>
 					</td>
-					<?php endif; ?>
+
 
 
 
 
 				</tr>
 
-				<?php $i = 1 - $i; ?>
+				<?php// $i = 1 - $i; ?>
 			<?php endforeach; ?>
 		<?php endif; ?>
 		</tbody>
 	</table>
-
+	</div>
 	<input type="hidden" name="filter_order" value="<?php echo $this->lists['order']; ?>" />
 	<input type="hidden" name="filter_order_Dir" value="<?php echo $this->lists['order_Dir']; ?>" />
 	<input type="hidden" name="boxchecked" value="0" />
