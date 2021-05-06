98bff4d4ae8df3ef33377d28b1468f0212fb36f3
jojo12 <jojo.murer@bluewin.ch>
2021-05-06 10:24:15 +0200

    Add files via upload

--- site/views/category/tmpl/responsive/calendar.php
+++ site/views/category/tmpl/responsive/calendar.php
@@ -296,10 +296,10 @@ defined('_JEXEC') or die;
 
 	<!-- Calendar buttons -->
 		<div class="calendarButtons jem-row jem-justify-start">
-				<button id="buttonshowall" class="calendarButton btn btn-secondary">
+				<button id="buttonshowall" class="calendarButton btn btn-outline-dark">
 					<?php echo JText::_('COM_JEM_SHOWALL'); ?>
 				</button>
-				<button id="buttonhideall" class="calendarButton btn btn-secondary">
+				<button id="buttonhideall" class="calendarButton btn btn-outline-dark">
 					<?php echo JText::_('COM_JEM_HIDEALL'); ?>
 				</button>
 		</div>
@@ -330,7 +330,7 @@ defined('_JEXEC') or die;
 							# build legend
 							if (array_key_exists($cat->id, $countcatevents)) {
 							?>
-								<button class="eventCat btn btn-outline-secondary" id="cat<?php echo $cat->id; ?>">
+								<button class="eventCat btn btn-outline-dark" id="cat<?php echo $cat->id; ?>">
 									<?php
 									if (isset($cat->color) && $cat->color) {
 										echo '<span class="colorpic" style="background-color: '.$cat->color.';"></span>';
@@ -360,10 +360,10 @@ defined('_JEXEC') or die;
 
 	<!-- Calendar buttons -->
 		<div class="calendarButtons jem-row jem-justify-start">
-				<button id="buttonshowall" class="calendarButton btn btn-secondary">
+				<button id="buttonshowall" class="calendarButton btn btn-outline-dark">
 					<?php echo JText::_('COM_JEM_SHOWALL'); ?>
 				</button>
-				<button id="buttonhideall" class="calendarButton btn btn-secondary">
+				<button id="buttonhideall" class="calendarButton btn btn-outline-dark">
 					<?php echo JText::_('COM_JEM_HIDEALL'); ?>
 				</button>
 		</div>
@@ -393,7 +393,7 @@ defined('_JEXEC') or die;
 						# build legend
 						if (array_key_exists($cat->id, $countcatevents)) {
 						?>
-							<button class="eventCat btn btn-outline-secondary" id="cat<?php echo $cat->id; ?>">
+							<button class="eventCat btn btn-outline-dark" id="cat<?php echo $cat->id; ?>">
 								<?php
 								if (isset($cat->color) && $cat->color) {
 									echo '<span class="colorpic" style="background-color: '.$cat->color.';"></span>';
