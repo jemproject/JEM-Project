a0a1e7ef971d4eab73441a243e9455029d2d10ce
jojo12 <jojo.murer@bluewin.ch>
2021-08-23 14:41:36 +0200

    Update default_events_table.php
    better view

--- site/common/views/tmpl/default_events_table.php
+++ site/common/views/tmpl/default_events_table.php
@@ -71,7 +71,8 @@ JHtml::_('behavior.tooltip');
 			<?php endif; ?>
 		</colgroup>
 
-		<thead>
+		<thead>		
+		<th>
 			<tr>
 				<?php if ($this->jemsettings->showeventimage == 1) : ?>
 				<th id="jem_eventimage" class="sectiontableheader" align="left"><?php echo JText::_('COM_JEM_TABLE_EVENTIMAGE'); ?></th>
