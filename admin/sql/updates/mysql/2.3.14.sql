00b5357d57c412b6746f8de5d97f7c427ebb8337
McKillo <mckillo@hotmail.com>
2023-04-03 07:30:47 +0200

    Fix There are tables not up to date #180
    Fix error after install last version 2.3.15, attribute COLUMN isn't required in Joomla.

--- admin/sql/updates/mysql/2.3.14.sql
+++ admin/sql/updates/mysql/2.3.14.sql
@@ -1,44 +1,44 @@
 -- update all JEM table from v.2.3.13 to v.2.3.14 to new JEM version with support Joomla 4
 ALTER TABLE #__jem_attachments
-  MODIFY COLUMN added datetime NULL DEFAULT null;
+  MODIFY added datetime NULL DEFAULT null;
 
 ALTER TABLE #__jem_categories
-  MODIFY COLUMN description mediumtext NULL DEFAULT null
-  , MODIFY COLUMN meta_keywords text NULL DEFAULT null
-  , MODIFY COLUMN meta_description text NULL DEFAULT null
-  , MODIFY COLUMN checked_out_time datetime NULL DEFAULT null
-  , MODIFY COLUMN note varchar(255) NULL DEFAULT null
-  , MODIFY COLUMN language varchar(7) NULL DEFAULT null
-  , MODIFY COLUMN created_time datetime NOT NULL DEFAULT current_timestamp()
-  , MODIFY COLUMN path varchar(255) NULL DEFAULT null
-  , MODIFY COLUMN metadata varchar(2048) NULL DEFAULT null
-  , MODIFY COLUMN modified_time datetime NULL DEFAULT null
-  , MODIFY COLUMN email varchar(200) NULL DEFAULT null;
+  MODIFY description mediumtext NULL DEFAULT null
+  , MODIFY meta_keywords text NULL DEFAULT null
+  , MODIFY meta_description text NULL DEFAULT null
+  , MODIFY checked_out_time datetime NULL DEFAULT null
+  , MODIFY note varchar(255) NULL DEFAULT null
+  , MODIFY language varchar(7) NULL DEFAULT null
+  , MODIFY created_time datetime NOT NULL DEFAULT current_timestamp()
+  , MODIFY path varchar(255) NULL DEFAULT null
+  , MODIFY metadata varchar(2048) NULL DEFAULT null
+  , MODIFY modified_time datetime NULL DEFAULT null
+  , MODIFY email varchar(200) NULL DEFAULT null;
 
 ALTER TABLE #__jem_cats_event_relations
-  MODIFY COLUMN ordering tinyint(11) NOT NULL DEFAULT 0;
+  MODIFY ordering tinyint(11) NOT NULL DEFAULT 0;
 
 ALTER TABLE #__jem_events
-  MODIFY COLUMN modified datetime NULL DEFAULT null
-  , MODIFY COLUMN created datetime NOT NULL DEFAULT current_timestamp()
-  , MODIFY COLUMN meta_keywords varchar(200) NULL DEFAULT null
-  , MODIFY COLUMN meta_description varchar(255) NULL DEFAULT null
-  , MODIFY COLUMN checked_out_time datetime NULL DEFAULT null
-  , MODIFY COLUMN attribs varchar(5120) NOT NULL DEFAULT ''''
-  , MODIFY COLUMN language char(7) NOT NULL DEFAULT '''';
+  MODIFY modified datetime NULL DEFAULT null
+  , MODIFY created datetime NOT NULL DEFAULT current_timestamp()
+  , MODIFY meta_keywords varchar(200) NULL DEFAULT null
+  , MODIFY meta_description varchar(255) NULL DEFAULT null
+  , MODIFY checked_out_time datetime NULL DEFAULT null
+  , MODIFY attribs varchar(5120) NOT NULL DEFAULT ''''
+  , MODIFY language char(7) NOT NULL DEFAULT '''';
 
 ALTER TABLE #__jem_groups
-  MODIFY COLUMN description mediumtext NULL DEFAULT null
-  , MODIFY COLUMN checked_out_time datetime NULL DEFAULT null;
+  MODIFY description mediumtext NULL DEFAULT null
+  , MODIFY checked_out_time datetime NULL DEFAULT null;
 
 ALTER TABLE #__jem_venues
-  MODIFY COLUMN locdescription mediumtext NULL DEFAULT null
-  , MODIFY COLUMN meta_keywords text NULL DEFAULT null
-  , MODIFY COLUMN meta_description text NULL DEFAULT null
-  , MODIFY COLUMN created datetime NOT NULL DEFAULT current_timestamp()
-  , MODIFY COLUMN modified datetime NULL DEFAULT null
-  , MODIFY COLUMN checked_out_time datetime NULL DEFAULT null
-  , MODIFY COLUMN publish_up datetime NULL DEFAULT null
-  , MODIFY COLUMN publish_down datetime NULL DEFAULT null
-  , MODIFY COLUMN attribs varchar(5120) NULL DEFAULT null
-  , MODIFY COLUMN language char(7) NULL DEFAULT null;
+  MODIFY locdescription mediumtext NULL DEFAULT null
+  , MODIFY meta_keywords text NULL DEFAULT null
+  , MODIFY meta_description text NULL DEFAULT null
+  , MODIFY created datetime NOT NULL DEFAULT current_timestamp()
+  , MODIFY modified datetime NULL DEFAULT null
+  , MODIFY checked_out_time datetime NULL DEFAULT null
+  , MODIFY publish_up datetime NULL DEFAULT null
+  , MODIFY publish_down datetime NULL DEFAULT null
+  , MODIFY attribs varchar(5120) NULL DEFAULT null
+  , MODIFY language char(7) NULL DEFAULT null;
