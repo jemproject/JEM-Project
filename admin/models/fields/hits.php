ea7ba6c09021dad409bd8106565b77a77f95879b
Michael Anderau <webstuff@sternenaugen.net>
2023-04-05 10:33:56 +0200

    cosmentics in edit event: Hit count field styling

--- admin/models/fields/hits.php
+++ admin/models/fields/hits.php
@@ -29,6 +29,6 @@ class JFormFieldHits extends JFormField
 	{
 		$onclick	= ' onclick="document.getElementById(\''.$this->id.'\').value=\'0\';"';
 
-		return '<input style="border:0;" type="text" name="'.$this->name.'" id="'.$this->id.'" value="'.htmlspecialchars($this->value, ENT_COMPAT, 'UTF-8').'" readonly="readonly" /><input type="button"'.$onclick.' value="'.JText::_('COM_JEM_RESET_HITS').'" class="button"/>';
+		return '<input type="text" name="'.$this->name.'" id="'.$this->id.'" class="form-control field-user-input-name valid form-control-success w-20" value="'.htmlspecialchars($this->value, ENT_COMPAT, 'UTF-8').'" readonly="readonly" style="display:inline-block;" /><input type="button"'.$onclick.' value="'.JText::_('COM_JEM_RESET_HITS').'" class="btn btn-primary selectcat" />';
 	}
 }
