9b716bde564b48801cf9fe133e6336dd6d2ecc77
McKillo <mckillo@hotmail.com>
2023-06-07 17:04:01 +0200

    add/move ical button in eventslist from footer to the top buttons #1585
    Apply change in the default and legacy style (frontend).

--- site/classes/output.class.php
+++ site/classes/output.class.php
@@ -116,7 +116,7 @@ class JemOutput
 		if (in_array('print', $btns_show) || (!in_array('print', $btns_hide) && in_array($view, array('attendees', 'calendar', 'categories', 'category', 'category-cal', 'day', 'event', 'eventslist', 'myattendances', 'myevents', 'myvenues', 'venue', 'venue-cal', 'venues', 'venueslist', 'weekcal')))) {
 			$buttons[$idx][] = JemOutput::printbutton($print_link, null);
 		}
-		if (in_array('ical', $btns_show) || (!in_array('ical', $btns_hide) && in_array($view, array('event')))) {
+		if (in_array('ical', $btns_show) || (!in_array('ical', $btns_hide) && in_array($view, array('event', 'eventslist')))) {
 			$buttons[$idx][] = JemOutput::icalbutton($slug, $view); // slug: for '&id='
 		}
 		if (in_array('export', $btns_show) || (!in_array('export', $btns_hide) && in_array($view, array('attendees')))) {
