b6e3512583a96cfda41c63f1e17f01a8b5e6a2d1
Michael Anderau <webstuff@sternenaugen.net>
2023-07-10 21:34:31 +0200

    JEM quickicon: add icon / add "add event" #1606

--- plugins/plg_quickicon_jem/jemquickicon.php
+++ plugins/plg_quickicon_jem/jemquickicon.php
@@ -12,6 +12,9 @@
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Router\Route;
+
 
 /**
  * JEM Quickicon Plugin
@@ -27,7 +30,7 @@ class plgQuickiconJEMquickicon extends JPlugin
     public function onGetIcons($context)
     {
         if ($context != $this->params->get('context', 'mod_quickicon') ||
-            !JFactory::getUser()->authorise('core.manage', 'com_jem') ||
+            !Factory::getUser()->authorise('core.manage', 'com_jem') ||
             !file_exists(JPATH_ADMINISTRATOR . '/components/com_jem/helpers/helper.php')) {
             return;
         }
@@ -36,17 +39,19 @@ class plgQuickiconJEMquickicon extends JPlugin
         $icon     = 'com_jem/icon-48-home.png'; // which means '/media/com_jem/images/icon-48-home.png'
         $text     = $this->params->get('displayedtext');
         if (empty($text)) {
-            $text = Text::_('JEM-Events');
+            $text = Text::_('COM_JEM_JEM_FULLNAME');
         }
 
         return array(
             array(
-                'link'  => 'index.php?option=com_jem',
-                'image' => 'calendar',
-                'icon'  => '',
-                'text'  => $text,
-                'id'    => 'plg_quickicon_jemquickicon',
-                'group' => 'JEM'
+            	'access'  => array('core.manage', 'com_jem'),	
+                'link'    => Route::_('index.php?option=com_jem'),
+                'linkadd' => Route::_('index.php?option=com_jem&task=event.add'),
+                'image'   => 'icon-calendar',
+                'icon'    => '',
+                'text'    => $text,
+                'id'      => 'plg_quickicon_jemquickicon',
+                'group'   => 'JEM'
             )
         );
     }
