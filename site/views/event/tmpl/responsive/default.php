306b633e04f6fcd5c72f0d81f33a38dc5aebd02d
Michael Anderau <webstuff@sternenaugen.net>
2023-07-26 09:11:23 +0200

    correct wrong child element

--- site/views/event/tmpl/responsive/default.php
+++ site/views/event/tmpl/responsive/default.php
@@ -74,12 +74,12 @@ if ($jemsettings->oldevent > 0) {
         ?>
         <dt class="jem-when hasTooltip" data-original-title="<?php echo Text::_('COM_JEM_WHEN'); ?>"><?php echo Text::_('COM_JEM_WHEN'); ?>:</dt>
         <dd class="jem-when">
-            <div style="white-space: nowrap;">
+            <span style="white-space: nowrap;">
               <?php
               echo JemOutput::formatLongDateTime($this->item->dates, $this->item->times,$this->item->enddates, $this->item->endtimes);
               echo JemOutput::formatSchemaOrgDateTime($this->item->dates, $this->item->times,$this->item->enddates, $this->item->endtimes);
               ?>
-            </div>
+            </span>
         </dd>
         <?php if ($this->item->locid != 0) : ?>
         <dt class="jem-where hasTooltip" data-original-title="<?php echo Text::_('COM_JEM_WHERE'); ?>"><?php echo Text::_('COM_JEM_WHERE'); ?>:</dt>
