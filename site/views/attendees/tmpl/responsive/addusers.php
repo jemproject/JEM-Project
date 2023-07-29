7308910711d09d70ade77610d84f0a01bdeb8a2e
Michael Anderau <webstuff@sternenaugen.net>
2023-07-29 18:35:29 +0200

    frontend attendee reg.: set min places to 1 #1625

--- site/views/attendees/tmpl/responsive/addusers.php
+++ site/views/attendees/tmpl/responsive/addusers.php
@@ -146,23 +146,11 @@ if (empty($form)) {
 		?>
 
         <div class="jem-row jem-justify-start valign-baseline">
-            <div style="padding-right:5px;">
-				<?php echo Text::_('COM_JEM_SELECT');?>
+            <div class="choose-status">
+				<?php echo Text::_('COM_JEM_SELECT');?> <?php echo $form->getLabel('status'); ?> <?php echo $form->getInput('status'); ?>
             </div>
-            <div style="padding-right:10px;">
-				<?php echo $form->getLabel('status'); ?>
-            </div>
-            <div style="padding-right:10px;">
-				<?php echo $form->getInput('status'); ?>
-            </div>
-            <div style="padding-right:5px;">
-				<?php echo Text::_('COM_JEM_SELECT');?>
-            </div>
-            <div style="padding-right:10px;">
-				<?php echo Text::_('COM_JEM_PLACES'); ?>
-            </div>
-            <div style="padding-right:10px;">
-                <input id="places" name="places" type="number" style="text-align: center; width:auto;"  value="0" max="<?php echo ($placesavailableuser>0? $placesavailableuser:($placesavailableus?? '')); ?>" min="0">
+            <div class="choose-places">
+				<?php echo Text::_('COM_JEM_SELECT');?> <?php echo Text::_('COM_JEM_PLACES'); ?> <input id="places" name="places" type="number" style="text-align: center; width:auto;" value="1" max="<?php echo ($placesavailableuser>0? $placesavailableuser:($placesavailableus?? '')); ?>" min="1">
             </div>
         </div>
 
