c9c535dcd73b020d96aed2abdb6b840f12f61087
McKillo <mckillo@hotmail.com>
2023-08-02 17:36:08 +0200

    Fix available filed as number #1630
    Fix the available field as a number, with a default value 0 when placesmax =0 (add the meaning to the help tag)

--- admin/views/event/tmpl/edit.php
+++ admin/views/event/tmpl/edit.php
@@ -189,19 +189,16 @@ Joomla.submitbutton = function(task)
             var maxBookedUserValue = parseInt($maxBookedUserInput.val());
             var maxPlacesValue = parseInt($maxPlacesInput.val());
             var reservedPlacesValue = parseInt($reservedPlacesInput.val());
-            if (minBookedUserValue > maxPlacesValue) {
+            if (minBookedUserValue > maxPlacesValue && maxPlacesValue != 0) {
                 $minBookedUserInput.val(maxPlacesValue);
             }
-            if (maxBookedUserValue > maxPlacesValue) {
+            if (maxBookedUserValue > maxPlacesValue && maxPlacesValue != 0) {
                 $maxBookedUserInput.val(maxPlacesValue);
             }
             if (minBookedUserValue > maxBookedUserValue) {
                 $minBookedUserInput.val(maxBookedUserValue);
             }
-            if (maxBookedUserValue < minBookedUserValue) {
-                $maxBookedUserInput.val(minBookedUserValue);
-            }
-            if (reservedPlacesValue > maxPlacesValue) {
+            if (reservedPlacesValue > maxPlacesValue && maxPlacesValue != 0)) {
                 $reservedPlacesInput.val(maxPlacesValue);
             }
         });
@@ -364,7 +361,7 @@ Joomla.submitbutton = function(task)
                                             <label id="availableplaces-lbl"><?php echo Text::_ ('COM_JEM_AVAILABLE_PLACES') . ':';?></label>
                                         </div>
                                         <div class="controls">
-                                            <input type="text" name="availableplaces" id="availableplaces" value=<?php echo  ($this->item->maxplaces? ($this->item->maxplaces-$this->item->booked-$this->item->reservedplaces):'---'); ?> class="form-control inputbox" size="4" aria-describedby="jform_reservedplaces-desc" readonly>
+                                            <input type="number" name="availableplaces" id="availableplaces" value=<?php echo  ($this->item->maxplaces? ($this->item->maxplaces-$this->item->booked-$this->item->reservedplaces):'0'); ?> class="form-control inputbox" size="4" aria-describedby="jform_reservedplaces-desc" readonly>
                                             <div id="availableplaces-desc" class="hide-aware-inline-help d-none">
                                                 <small class="form-text">
                                                     <?php echo Text::_ ('COM_JEM_AVAILABLE_PLACES_DESC') ;?></small>
