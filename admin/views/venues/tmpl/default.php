374b5e0c98d008f11c44edd1434828d109e12749
McKillo <mckillo@hotmail.com>
2024-04-17 18:34:19 +0200

    Fix Backend Categories: change category order not working #1551
    Now, the admin can up or down to change order in categories and venues views.
    Improvement style and format of these views too.

--- admin/views/venues/tmpl/default.php
+++ admin/views/venues/tmpl/default.php
@@ -13,6 +13,7 @@ use Joomla\CMS\Router\Route;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Factory;
+use Joomla\CMS\Object\CMSObject;
 
 $user		= JemFactory::getUser();
 $userId		= $user->get('id');
@@ -22,33 +23,10 @@ $canOrder	= $user->authorise('core.edit.state', 'com_jem.category');
 $saveOrder	= $listOrder=='a.ordering';
 $wa = Factory::getApplication()->getDocument()->getWebAssetManager();
 $wa->useScript('table.columns');
-
-$params		= (isset($this->state->params)) ? $this->state->params : new JObject();
-$settings	= $this->settings;
 ?>
-<script>
-$(document).ready(function() {
-	var h = <?php echo $settings->get('highlight','0'); ?>;
-
-	switch(h)
-	{
-	case 0:
-		break;
-	case 1:
-		highlightvenues();
-		break;
-	}
-});
-</script>
 
 <form action="<?php echo Route::_('index.php?option=com_jem&view=venues'); ?>" method="post" name="adminForm" id="adminForm">
-	<?php if (isset($this->sidebar)) : ?>
-	<!-- <div id="j-sidebar-container" class="span2">
-		<?php //echo $this->sidebar; ?>
-	</div> -->
-	<?php endif; ?>
 	<div id="j-main-container" class="j-main-container">
-
 		<fieldset id="filter-bar" class=" mb-3">
 			<div class="row mb-3">
 				<div class="col-md-4">
@@ -75,30 +53,52 @@ $(document).ready(function() {
 				<button type="submit"><?php echo Text::_('JSEARCH_FILTER_SUBMIT'); ?></button>
 				<button type="button" onclick="document.getElementById('filter_search').value='';this.form.submit();"><?php echo Text::_('JSEARCH_FILTER_CLEAR'); ?></button>
 			</div> -->
-			
+
 		</fieldset>
 		<div class="clr"> </div>
 
 		<table class="table table-striped" id="articleList">
 			<thead>
 				<tr>
-					<th width="1%" class="center"><input type="checkbox" name="checkall-toggle" value="" title="<?php echo Text::_('JGLOBAL_CHECK_ALL'); ?>" onclick="Joomla.checkAll(this)" /></th>
-					<th class="title"><?php echo HTMLHelper::_('grid.sort', 'COM_JEM_VENUE', 'a.venue', $listDirn, $listOrder ); ?></th>
-					<th width="20%"><?php echo HTMLHelper::_('grid.sort', 'COM_JEM_ALIAS', 'a.alias', $listDirn, $listOrder ); ?></th>
-					<th><?php echo Text::_('COM_JEM_WEBSITE'); ?></th>
-					<th><?php echo HTMLHelper::_('grid.sort', 'COM_JEM_CITY', 'a.city', $listDirn, $listOrder ); ?></th>
-					<th><?php echo HTMLHelper::_('grid.sort', 'COM_JEM_STATE', 'a.state', $listDirn, $listOrder ); ?></th>
-					<th width="1%"><?php echo HTMLHelper::_('grid.sort', 'COM_JEM_COUNTRY', 'a.country', $listDirn, $listOrder ); ?></th>
-					<th width="1%" class="center" nowrap="nowrap"><?php echo Text::_('JSTATUS'); ?></th>
-					<th><?php echo Text::_('COM_JEM_CREATION'); ?></th>
-					<th width="1%" class="center" nowrap="nowrap"><?php echo HTMLHelper::_('grid.sort', 'COM_JEM_EVENTS', 'assignedevents', $listDirn, $listOrder ); ?></th>
-					<th width="10%">
+					<th style="width:1%" class="center">
+						<input type="checkbox" name="checkall-toggle" value="" title="<?php echo Text::_('JGLOBAL_CHECK_ALL'); ?>" onclick="Joomla.checkAll(this)" />
+					</th>
+					<th class="title">
+						<?php echo HTMLHelper::_('grid.sort', 'COM_JEM_VENUE', 'a.venue', $listDirn, $listOrder ); ?>
+					</th>
+					<th style="width:20%">
+						<?php echo HTMLHelper::_('grid.sort', 'COM_JEM_ALIAS', 'a.alias', $listDirn, $listOrder ); ?>
+					</th>
+					<th>
+						<?php echo Text::_('COM_JEM_WEBSITE'); ?>
+					</th>
+					<th style="width:10%">
+						<?php echo HTMLHelper::_('grid.sort', 'COM_JEM_CITY', 'a.city', $listDirn, $listOrder ); ?>
+					</th>
+					<th style="width:5%" class="center">
+						<?php echo HTMLHelper::_('grid.sort', 'COM_JEM_STATE', 'a.state', $listDirn, $listOrder ); ?>
+						</th>
+					<th style="width:5%" class="center">
+						<?php echo HTMLHelper::_('grid.sort', 'COM_JEM_COUNTRY', 'a.country', $listDirn, $listOrder ); ?>
+					</th>
+					<th style="width:5%" class="center" nowrap="nowrap">
+						<?php echo Text::_('JSTATUS'); ?>
+					</th>
+					<th>
+						<?php echo Text::_('COM_JEM_CREATION'); ?>
+					</th>
+					<th style="width:5%" class="center" nowrap="nowrap">
+						<?php echo HTMLHelper::_('grid.sort', 'COM_JEM_EVENTS', 'assignedevents', $listDirn, $listOrder ); ?>
+						</th>
+					<th style="width:5%" class="center">
 						<?php echo HTMLHelper::_('grid.sort', 'JGRID_HEADING_ORDERING', 'a.ordering', $listDirn, $listOrder ); ?>
 						<?php if ($saveOrder) :?>
-							<?php echo HTMLHelper::_('grid.order',  $this->items, 'filesave.png', 'venues.saveorder'); ?>
+							<?php //echo HTMLHelper::_('grid.order',  $this->items, 'filesave.png', 'venues.saveorder'); ?>
 						<?php endif; ?>
 					</th>
-					<th width="1%" class="center" nowrap="nowrap"><?php echo HTMLHelper::_('grid.sort', 'COM_JEM_ID', 'a.id', $listDirn, $listOrder ); ?></th>
+					<th style="width:1%" class="center" nowrap="nowrap">
+						<?php echo HTMLHelper::_('grid.sort', 'COM_JEM_ID', 'a.id', $listDirn, $listOrder ); ?>
+					</th>
 				</tr>
 			</thead>
 
@@ -107,123 +107,147 @@ $(document).ready(function() {
 					<td colspan="20">
 						<?php //echo (method_exists($this->pagination, 'getPaginationLinks') ? $this->pagination->getPaginationLinks(null, array('showLimitBox' => true)) : $this->pagination->getListFooter()); ?>
 						<div class="row align-items-center">
-							<div class="col-md-9">
-								<?php
-									echo  (method_exists($this->pagination, 'getPaginationLinks') ? $this->pagination->getPaginationLinks(null) : $this->pagination->getListFooter()); 
-								?>
-							</div>
-                            <div class="col-md-3">
-                                <div class="limit float-end">
-                                    <?php
-                                    echo $this->pagination->getLimitBox();
-                                    ?>
-                                </div>
+                            <div class="col-md-9">
+                                <?php
+                                echo  (method_exists($this->pagination, 'getPaginationLinks') ? $this->pagination->getPaginationLinks(null) : $this->pagination->getListFooter());
+                                ?>
                             </div>
+							<div class="col-md-3">
+								<div class="limit float-end">
+									<?php 
+										echo $this->pagination->getLimitBox();	
+									?>
+								</div>
+							</div>
 						</div>
 					</td>
 				</tr>
 			</tfoot>
 
-			<tbody id="search_in_here">
-				<?php foreach ($this->items as $i => $row) : ?>
+			<tbody>
+				<?php
+                $countItems = count($this->items);
+				foreach ($this->items as $i => $item) : ?>
 					<?php
 					$ordering	= ($listOrder == 'a.ordering');
 					$canCreate	= $user->authorise('core.create');
 					$canEdit	= $user->authorise('core.edit');
-					$canCheckin	= $user->authorise('core.manage',		'com_checkin') || $row->checked_out == $userId || $row->checked_out == 0;
+					$canCheckin	= $user->authorise('core.manage', 'com_checkin') || $item->checked_out == $userId || $item->checked_out == 0;
+					$canEditOwn	= $user->authorise('core.edit.own') && $item->created_by == $userId;
 					$canChange	= $user->authorise('core.edit.state') && $canCheckin;
-
-					$link 		= 'index.php?option=com_jem&amp;task=venue.edit&amp;id='. $row->id;
-					$published 	= HTMLHelper::_('jgrid.published', $row->published, $i, 'venues.', $canChange, 'cb', $row->publish_up, $row->publish_down);
-					?>
+					$link 		= 'index.php?option=com_jem&amp;task=venue.edit&amp;id='. $item->id;
+					$published 	= HTMLHelper::_('jgrid.published', $item->published, $i, 'venues.', $canChange, 'cb', $item->publish_up, $item->publish_down);
+				?>
 					<tr class="row<?php echo $i % 2; ?>">
-						<td class="center"><?php echo HTMLHelper::_('grid.id', $i, $row->id); ?></td>
-						<td align="left" class="venue">
-							<?php if ($row->checked_out) : ?>
-								<?php echo HTMLHelper::_('jgrid.checkedout', $i, $row->editor, $row->checked_out_time, 'venues.', $canCheckin); ?>
+						<td class="center">
+							<?php echo HTMLHelper::_('grid.id', $i, $item->id); ?></td>
+						<td style="text-align:left" class="venue">
+							<?php if ($item->checked_out) : ?>
+								<?php echo HTMLHelper::_('jgrid.checkedout', $i, $item->editor, $item->checked_out_time, 'venues.', $canCheckin); ?>
 							<?php endif; ?>
-							<?php if ($canEdit) : ?>
-								<a href="<?php echo Route::_('index.php?option=com_jem&task=venue.edit&id='.(int) $row->id); ?>">
-									<?php echo $this->escape($row->venue); ?>
+							<?php if ($canEdit || $canEditOwn) : ?>
+								<a href="<?php echo Route::_('index.php?option=com_jem&task=venue.edit&id='.(int) $item->id); ?>">
+									<?php echo $this->escape($item->venue); ?>
 								</a>
 							<?php else : ?>
-								<?php echo $this->escape($row->venue); ?>
+								<?php echo $this->escape($item->venue); ?>
 							<?php endif; ?>
 						</td>
 						<td>
-							<?php if (\Joomla\String\StringHelper::strlen($row->alias) > 25) : ?>
-								<?php echo $this->escape(\Joomla\String\StringHelper::substr($row->alias, 0 , 25)).'...'; ?>
+							<?php if (\Joomla\String\StringHelper::strlen($item->alias) > 25) : ?>
+								<?php echo $this->escape(\Joomla\String\StringHelper::substr($item->alias, 0 , 25)).'...'; ?>
 							<?php else : ?>
-								<?php echo $this->escape($row->alias); ?>
+								<?php echo $this->escape($item->alias); ?>
 							<?php endif; ?>
 						</td>
-						<td align="left">
-							<?php if ($row->url) : ?>
-								<a href="<?php echo $this->escape($row->url); ?>" target="_blank">
-									<?php if (\Joomla\String\StringHelper::strlen($row->url) > 25) : ?>
-										<?php echo $this->escape(\Joomla\String\StringHelper::substr($row->url, 0 , 25)).'...'; ?>
+						<td style="text-align:left">
+							<?php if ($item->url) : ?>
+								<a href="<?php echo $this->escape($item->url); ?>" target="_blank">
+									<?php if (\Joomla\String\StringHelper::strlen($item->url) > 25) : ?>
+										<?php echo $this->escape(\Joomla\String\StringHelper::substr($item->url, 0 , 25)).'...'; ?>
 									<?php else : ?>
-										<?php echo $this->escape($row->url); ?>
+										<?php echo $this->escape($item->url); ?>
 									<?php endif; ?>
 								</a>
 							<?php else : ?>
 								-
 							<?php endif; ?>
 						</td>
-						<td align="left" class="city"><?php echo $row->city ? $this->escape($row->city) : '-'; ?></td>
-						<td align="left" class="state"><?php echo $row->state ? $this->escape($row->state) : '-'; ?></td>
-						<td class="country"><?php echo $row->country ? $this->escape($row->country) : '-'; ?></td>
+						<td style="text-align:left" class="city"><?php echo $item->city ? $this->escape($item->city) : '-'; ?></td>
+						<td class="center state"><?php echo $item->state ? $this->escape($item->state) : '-'; ?></td>
+						<td class="center country"><?php echo $item->country ? $this->escape($item->country) : '-'; ?></td>
 						<td class="center"><?php echo $published; ?></td>
 						<td>
 							<?php
-							$created	 	= HTMLHelper::_('date',$row->created,Text::_('DATE_FORMAT_LC5'));
+							$created	 	= HTMLHelper::_('date',$item->created,Text::_('DATE_FORMAT_LC5'));
 							$image 			= HTMLHelper::_('image','com_jem/icon-16-info.png', NULL,NULL,true);
 							$overlib 		= Text::_('COM_JEM_CREATED_AT').': '.$created.'<br />';
-							$overlib 		.= Text::_('COM_JEM_AUTHOR').'</strong>: ' . $row->author.'<br />';
-							$overlib 		.= Text::_('COM_JEM_EMAIL').'</strong>: ' . $row->email.'<br />';
-							if ($row->author_ip != '') {
-								$overlib		.= Text::_('COM_JEM_WITH_IP').': '.$row->author_ip.'<br />';
+							$overlib 		.= Text::_('COM_JEM_AUTHOR').'</strong>: ' . $item->author.'<br />';
+							$overlib 		.= Text::_('COM_JEM_EMAIL').'</strong>: ' . $item->email.'<br />';
+							if ($item->author_ip != '') {
+								$overlib		.= Text::_('COM_JEM_WITH_IP').': '.$item->author_ip.'<br />';
 							}
-							if (!empty($row->modified)) {
-								$overlib 	.= '<br />'.Text::_('COM_JEM_EDITED_AT').': '. HTMLHelper::_('date',$row->modified,Text::_('DATE_FORMAT_LC5') ) .'<br />'. Text::_('COM_JEM_GLOBAL_MODIFIEDBY').': '.$row->modified_by;
+							if (!empty($item->modified)) {
+								$overlib 	.= '<br />'.Text::_('COM_JEM_EDITED_AT').': '. HTMLHelper::_('date',$item->modified,Text::_('DATE_FORMAT_LC5') ) .'<br />'. Text::_('COM_JEM_GLOBAL_MODIFIEDBY').': '.$item->modified_by;
 							}
 							?>
 							<span <?php echo JEMOutput::tooltip(Text::_('COM_JEM_EVENTS_STATS'), $overlib, 'editlinktip'); ?>
 							
-							<a href="<?php echo 'index.php?option=com_users&amp;task=edit&amp;hidemainmenu=1&amp;cid[]='.$row->created_by; ?>"><?php echo $row->author; ?></a></span>
+							<a href="<?php echo 'index.php?option=com_users&amp;task=edit&amp;hidemainmenu=1&amp;cid[]='.$item->created_by; ?>"><?php echo $item->author; ?></a></span>
 							
 							
 						</td>
-						<td class="center"><?php echo $row->assignedevents; ?></td>
+						<td class="center"><?php echo $item->assignedevents; ?></td>
 						<td class="order">
 						<?php if ($canChange) : ?>
-							<?php if ($saveOrder) :?>
-								<?php if ($listDirn == 'asc') : ?>
-									<span><?php echo $this->pagination->orderUpIcon( $i, true, 'venues.orderup', 'JLIB_HTML_MOVE_UP', $ordering ); ?></span>
-									<span><?php echo $this->pagination->orderDownIcon( $i,$this->pagination->total, true, 'venues.orderdown', 'JLIB_HTML_MOVE_DOWN', $ordering ); ?></span>
+                            <?php $disabled = $saveOrder ?  '' : 'disabled="disabled"'; ?>
+                            <div style="display:-webkit-box">
+                            	<div><input type="text" style="text-align: center; margin: auto 0; min-width: 50px;" name="order[]" size="5" value="<?php echo $item->ordering;?>" <?php echo $disabled ?> class="text-area-order" /></div>
+
+							<?php if ($saveOrder) :
+								if ($listDirn == 'asc') : ?>
+									<div><?php if($i) : ?>
+										<span><?php echo $this->pagination->orderUpIcon( $i, true, 'venues.orderup', 'JLIB_HTML_MOVE_UP', $ordering ); ?></span>
+	                                <?php else : ?>
+	                                        <div style='width:32px;'>&nbsp;</div>
+	                                <?php endif; ?></div>
+	                                <div><?php if($countItems != $i+1) : ?>
+										<span><?php echo $this->pagination->orderDownIcon( $i,$this->pagination->total, true, 'venues.orderdown', 'JLIB_HTML_MOVE_DOWN', $ordering ); ?></span>
+	                                <?php else : ?>
+	                                    <div style='width:32px;'>&nbsp;</div>
+	                                <?php endif; ?></div>
 								<?php elseif ($listDirn == 'desc') : ?>
-									<span><?php echo $this->pagination->orderUpIcon( $i, true, 'venues.orderdown', 'JLIB_HTML_MOVE_UP', $ordering ); ?></span>
-									<span><?php echo $this->pagination->orderDownIcon( $i,$this->pagination->total, true, 'venues.orderup', 'JLIB_HTML_MOVE_DOWN', $ordering ); ?></span>
+									<div><?php if($i) : ?>
+										<span><?php echo $this->pagination->orderUpIcon( $i, true, 'venues.orderdown', 'JLIB_HTML_MOVE_UP', $ordering ); ?></span>
+	                                <?php else : ?>
+	                                    <div style='width:32px;'>&nbsp;</div>
+	                                <?php endif; ?></div>
+	                                <div><?php if($countItems != $i+1) : ?>
+										<span><?php echo $this->pagination->orderDownIcon( $i,$this->pagination->total, true, 'venues.orderup', 'JLIB_HTML_MOVE_DOWN', $ordering ); ?></span>
+	                                <?php else : ?>
+	                                    <div style='width:32px;'>&nbsp;</div>
+	                                <?php endif; ?></div>
 								<?php endif; ?>
 							<?php endif; ?>
-							<?php $disabled = $saveOrder ?  '' : 'disabled="disabled"'; ?>
-							<input type="text" name="order[]" size="5" value="<?php echo $row->ordering;?>" <?php echo $disabled ?> class="text-area-order" />
+                            </div>
 						<?php else : ?>
 							<?php echo $item->ordering; ?>
 						<?php endif; ?>
 						</td>
-						<td class="center"><?php echo $row->id; ?></td>
+						<td class="center">
+							<?php echo (int) $item->id; ?>
+						</td>
 					</tr>
 				<?php endforeach; ?>
 			</tbody>
 		</table>
 	</div>
 	<?php //if (isset($this->sidebar)) : ?>
-	<?php // endif; ?>
+	<?php //endif; ?>
 
 	<div>
-		<input type="hidden" name="boxchecked" value="0" />
 		<input type="hidden" name="task" value="" />
+		<input type="hidden" name="boxchecked" value="0" />
 		<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>" />
 		<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>" />
 		<?php echo HTMLHelper::_('form.token'); ?>
