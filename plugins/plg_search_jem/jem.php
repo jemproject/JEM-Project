12c02db74f2aeddaa82d2a4cbcab6fc67596ae37
jojo12 <jojo.murer@bluewin.ch>
2024-05-06 19:23:40 +0200

    Update jem.php
    replaced JRegistry with Registry

--- plugins/plg_search_jem/jem.php
+++ plugins/plg_search_jem/jem.php
@@ -14,6 +14,7 @@ use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Plugin\PluginHelper;
 use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\Registry\Registry;
 
 jimport('joomla.html.parameter');
 
@@ -83,7 +84,7 @@ class plgSearchJEM extends CMSPlugin
 
         // load plugin params info
         $plugin       = PluginHelper::getPlugin('search', 'jem');
-        $pluginParams = new JRegistry($plugin->params);
+        $pluginParams = new Registry($plugin->params);
 
         $limit = $pluginParams->def('search_limit', 50);
 
