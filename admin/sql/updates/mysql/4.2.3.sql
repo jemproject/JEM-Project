981cebc2b92117ee2abeb336f75959103cc6dde8
McKillo <mckillo@hotmail.com>
2024-06-17 20:26:18 +0200

    Update 4.2.3.sql
    Delete sufix joomla in table

--- admin/sql/updates/mysql/4.2.3.sql
+++ admin/sql/updates/mysql/4.2.3.sql
@@ -1,5 +1,5 @@
 -- update values
-DELETE FROM `#__jem_config` WHERE `ujrfe_jem_config`.`keyname` = 'recurrence_anticipation';
+DELETE FROM `#__jem_config` WHERE `keyname` = 'recurrence_anticipation';
 INSERT INTO `#__jem_config` (`keyname`, `value`, `access`) VALUES ('recurrence_anticipation_day', '3', '0');
 INSERT INTO `#__jem_config` (`keyname`, `value`, `access`) VALUES ('recurrence_anticipation_week', '12', '0');
 INSERT INTO `#__jem_config` (`keyname`, `value`, `access`) VALUES ('recurrence_anticipation_month', '60', '0');
