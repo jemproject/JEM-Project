e9da2523636584946e703f010c97cb15976f3ee5
Michael Anderau <webstuff@sternenaugen.net>
2024-06-21 03:17:17 +0200

    quickicon rewritten for J5 #1766

---
+++ plugins/plg_quickicon_jem/src/Extension/JemQuickIcon.php
@@ -0,0 +1,31 @@
+<?php
+namespace Joomla\Plugin\Quickicon\Jem\Extension;
+
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Uri\Uri;
+
+defined('_JEXEC') or die;
+
+class JemQuickIcon extends CMSPlugin
+{
+    protected $autoloadLanguage = true;
+
+    public function onGetIcons($context)
+    {
+        if ($context !== 'mod_quickicon' || !$this->getApplication()->getIdentity()->authorise('core.manage', 'com_jem')) {
+            return [];
+        }
+
+        return [
+            [
+                'link'    => 'index.php?option=com_jem',
+                'linkadd' => 'index.php?option=com_jem&task=event.add',
+                'image'   => 'icon-calendar',
+                'icon'    => 'icon-calendar',
+                'text'    => Text::_('PLG_QUICKICON_JEMQUICKICON_JEM'),
+                'id'      => 'plg_quickicon_jem'
+            ]
+        ];
+    }
+}
