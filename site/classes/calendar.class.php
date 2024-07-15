46a33bca442aebb6da5b67644bf95ef0b4d5f002
McKillo <mckillo@hotmail.com>
2024-07-15 08:36:02 +0200

    Update navigation button in the calendar view #1782
    The navigation control for the calendar are icons of this type
     https://fontawesome.com/icons/forward-step?f=classic&s=solid

--- site/classes/calendar.class.php
+++ site/classes/calendar.class.php
@@ -63,10 +63,10 @@ class JemCalendar
 	You can change below the default year's and month's view navigation controls
 	********************************************************************************
 	*/
-	var $yearNavBack=" &laquo; "; // Previous year, this could be an image link
-	var $yearNavForw=" &raquo; "; // Next year, this could be an image link
-	var $monthNavBack=" &laquo; "; // Previous month, this could be an image link
-	var $monthNavForw=" &raquo; "; // Next month, this could be an image link
+	var $yearNavBack=" <i class='fa-solid fa-backward'></i> "; // Previous year
+	var $yearNavForw=" <i class='fa-solid fa-forward'></i> "; // Next year
+	var $monthNavBack=" <i class='fa-solid fa-backward-step'></i> "; // Previous month
+	var $monthNavForw=" <i class='fa-solid fa-forward-step'></i> "; // Next month
 	var $selBtn="Go"; // value of the date picker button (if enabled)
 	var $monthYearDivider=" "; // the divider between month and year in the month`s title
 	/*
