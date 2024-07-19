48f6c37285ef8676a3621d13395bd1998a6f0a1d
McKillo <mckillo@hotmail.com>
2024-07-19 18:19:27 +0200

    Change position of new table into registration form #1764
    The HTML table shows a user's registrations for an event. Added the places and status columns.

--- site/models/event.php
+++ site/models/event.php
@@ -270,7 +270,7 @@ class JemModelEvent extends ItemModel
 					'CASE WHEN a.modified = 0 THEN a.created ELSE a.modified END as modified, a.modified_by, ' .
 					'a.checked_out, a.checked_out_time, a.datimage,  a.version, a.featured, ' .
 					'a.seriesbooking, a.singlebooking, a.meta_keywords, a.meta_description, a.created_by_alias, a.introtext, a.fulltext, a.maxplaces, a.reservedplaces, a.minbookeduser, a.maxbookeduser, a.waitinglist, a.requestanswer, ' .
-					'a.hits, a.language, a.recurrence_type, a.recurrence_first_id' . ($iduser? ', r.waiting' . ($status? ', r.status':''):'')));
+					'a.hits, a.language, a.recurrence_type, a.recurrence_first_id' . ($iduser? ', r.waiting, r.places' . ($status? ', r.status':''):'')));
 			$query->from('#__jem_events AS a');
 
 			# Author
