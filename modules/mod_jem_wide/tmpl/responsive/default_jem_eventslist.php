d3e1c777e7b5a44d27e1cc229501fa2dbd158c92
Jojo Murer <jojo.murer@bluewin.ch>
2024-07-26 16:40:07 +0200

    Update default_jem_eventslist.php
    typo line 90 forgotten <
    I added

--- modules/mod_jem_wide/tmpl/responsive/default_jem_eventslist.php
+++ modules/mod_jem_wide/tmpl/responsive/default_jem_eventslist.php
@@ -87,7 +87,7 @@ $jemsettings = JemHelper::config();
         <?php if (!empty($item->featured)) :   ?>
           <li class="jem-event jem-row jem-justify-start jem-featured <?php echo ' event_id'.$item->eventid; ?> jem-featured" <?php if ($params->get('linkevent') == 1 && (!$isSafari)) : echo 'onclick=location.href="'.$item->eventlink.'"'; endif; ?> >
 				<?php else : ?>
-          li class="jem-event jem-row jem-justify-start <?php echo ' event_id'.$item->eventid; ?>" <?php if ($params->get('linkevent') == 1 && (!$isSafari)) : echo 'onclick=location.href="'.$item->eventlink.'"'; endif; ?> >
+          <li class="jem-event jem-row jem-justify-start <?php echo ' event_id'.$item->eventid; ?>" <?php if ($params->get('linkevent') == 1 && (!$isSafari)) : echo 'onclick=location.href="'.$item->eventlink.'"'; endif; ?> >
 				<?php endif; ?>       
           <div class="jem-event-details" <?php if ($params->get('linkevent') == 1 && (!$isSafari)) : echo 'onclick=location.href="'.$item->eventlink.'"'; endif; ?>>
             <?php if ($params->get('linkevent') == 1) : // Display title as title of jem-event with link ?>
