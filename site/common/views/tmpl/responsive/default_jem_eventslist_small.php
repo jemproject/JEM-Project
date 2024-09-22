394597123f1b62847faa6541d7675c976786cce7
Hekla Terriol <webstuff@sternenaugen.net>
2024-09-22 17:07:46 +0200

    add placeholder für YYYY-MM #1828

--- site/common/views/tmpl/responsive/default_jem_eventslist_small.php
+++ site/common/views/tmpl/responsive/default_jem_eventslist_small.php
@@ -119,7 +119,7 @@ function jem_common_show_filter(&$obj) {
             <label for="month"><?php echo Text::_('COM_JEM_SEARCH_MONTH'); ?></label>
         </div>
         <div class="jem-row jem-justify-start jem-nowrap">
-            <input type="month" name="filter_month" id="filter_month" class="inputbox form-control" placeholder="YYYY-MM" size="7" value="<?php echo $this->lists['month'] ?? '';?>">
+            <input type="month" name="filter_month" id="filter_month" class="inputbox form-control" placeholder="<?php echo Text::_('COM_JEM_SEARCH_YYYY-MM'); ?>" size="7" value="<?php echo $this->lists['month'] ?? '';?>">
         </div>
     <div class="jem-row jem-justify-start jem-nowrap">
       <button class="btn btn-primary" type="submit"><?php echo Text::_('JSEARCH_FILTER_SUBMIT'); ?></button>
