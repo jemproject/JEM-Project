a160499775335e82c911b1735fe9a479500d0782
Hekla Terriol <webstuff@sternenaugen.net>
2024-10-07 20:54:35 +0200

    improve update page (rm tables etc.)

--- admin/views/updatecheck/tmpl/default.php
+++ admin/views/updatecheck/tmpl/default.php
@@ -22,7 +22,7 @@ use Joomla\CMS\Router\Route;
 	<?php endif; ?>
 
 		<?php if ($this->updatedata->failed == 0) : ?>
-		<p>
+		<div class="update-info">
 				<?php
 					if ($this->updatedata->current == 0 ) {
 						echo HTMLHelper::_('image', 'com_jem/icon-48-latest-version.png', NULL, NULL, true);
@@ -34,66 +34,47 @@ use Joomla\CMS\Router\Route;
 				?>
 				<?php
 					if ($this->updatedata->current == 0) {
-						echo '<b style="color:green">'.Text::_('COM_JEM_UPDATECHECK_LATEST_VERSION').'</b>';
+						echo '<p style="color:green;font-weight: bold;">'.Text::_('COM_JEM_UPDATECHECK_LATEST_VERSION').'</p>';
 					} elseif( $this->updatedata->current == -1 ) {
-						echo '<b style="color:red">'.Text::_('COM_JEM_UPDATECHECK_OLD_VERSION').'</b>';
+						echo '<p style="color:red;font-weight: bold;">'.Text::_('COM_JEM_UPDATECHECK_OLD_VERSION').'</p>';
 					} else {
-						echo '<b style="color:orange">'.Text::_('COM_JEM_UPDATECHECK_NEWER_VERSION').'</b>';
+						echo '<p style="color:orange;font-weight: bold;">'.Text::_('COM_JEM_UPDATECHECK_NEWER_VERSION').'</p>';
 					}
 				?>
-		</p>
-		<br />
-		<table class="updatecheck">
-			<tr>
-				<td><b><?php echo Text::_('COM_JEM_UPDATECHECK_VERSION').':'; ?></b></td>
-				<td><?php
-					echo $this->updatedata->versiondetail;
-					?>
-				</td>
-			</tr>
-			<tr>
-		  		<td><b><?php echo Text::_('COM_JEM_UPDATECHECK_INSTALLED_VERSION').':'; ?></b></td>
-		  		<td><?php echo $this->updatedata->installedversion; ?>
-		  		</td>
-			</tr>
-			<tr>
-				<td><b><?php echo Text::_('COM_JEM_UPDATECHECK_RELEASE_DATE').':'; ?></b></td>
-				<td><?php
-					echo $this->updatedata->date;
-					?>
-				</td>
-			</tr>
-			<tr>
-				<td><b><?php echo Text::_('COM_JEM_UPDATECHECK_CHANGES').':'; ?></b></td>
-				<td><ul>
-					<?php
+		</div>
+
+	    <div class="update-details">
+			<div class="detail-item">
+				<strong><?php echo Text::_('COM_JEM_UPDATECHECK_VERSION').':'; ?></strong>
+            	<span><?php echo $this->updatedata->versiondetail; ?></span>
+	        </div>
+			<div class="detail-item">
+				<strong><?php echo Text::_('COM_JEM_UPDATECHECK_INSTALLED_VERSION').':'; ?></strong>
+            	<span><?php echo $this->updatedata->installedversion; ?></span>
+	        </div>
+			<div class="detail-item">
+				<strong><?php echo Text::_('COM_JEM_UPDATECHECK_RELEASE_DATE').':'; ?></strong>
+            	<span><?php echo $this->updatedata->date; ?></span>
+	        </div>
+			<div class="detail-item">
+				<strong><?php echo Text::_('COM_JEM_UPDATECHECK_CHANGES').':'; ?></strong>
+            	<span>
+            		<ul><?php
 					foreach ($this->updatedata->changes as $change) {
 						echo '<li>'.$change.'</li>';
-					}
-					?>
+						} ?>
 					</ul>
-				</td>
-			</tr>
-			<tr>
-				<td><b><?php echo Text::_('COM_JEM_UPDATECHECK_INFORMATION').':'; ?></b></td>
-				<td>
-					<a href="<?php echo $this->updatedata->info; ?>" target="_blank"><?php echo Text::_('COM_JEM_UPDATECHECK_INFORMATION'); ?></a>
-				</td>
-			</tr>
-			<tr>
-				<td><b><?php echo Text::_('COM_JEM_UPDATECHECK_FILES').':'; ?></b></td>
-				<td>
-					<a href="<?php echo $this->updatedata->download; ?>" target="_blank"><?php echo Text::_('COM_JEM_UPDATECHECK_DOWNLOAD'); ?></a>
-				</td>
-			</tr>
-			<tr>
-				<td><b><?php echo Text::_('COM_JEM_UPDATECHECK_NOTES').':'; ?></b></td>
-				<td><?php
-					echo $this->updatedata->notes;
-					?>
-				</td>
-			</tr>
-		</table>
+					<a href="<?php echo $this->updatedata->info; ?>" target="_blank"><?php echo Text::_('COM_JEM_UPDATECHECK_INFORMATION'); ?></a></span>
+        	</div></span>
+			<div class="detail-item">
+				<strong><?php echo Text::_('COM_JEM_UPDATECHECK_FILES').':'; ?></strong>
+      	      <span><a href="<?php echo $this->updatedata->download; ?>" target="_blank"><?php echo Text::_('COM_JEM_UPDATECHECK_DOWNLOAD'); ?></a></span>
+			</div>
+			<div class="detail-item">
+				<strong><?php echo Text::_('COM_JEM_UPDATECHECK_NOTES').':'; ?></strong>
+    	        <span><?php echo $this->updatedata->notes; ?></span>
+    	    </div>
+    	</div>
 
 		<?php else : ?>
 
@@ -107,9 +88,8 @@ use Joomla\CMS\Router\Route;
 		  		<td>
 		  		<?php
 		  			echo '<b style="color:red">'.Text::_('COM_JEM_UPDATECHECK_CONNECTION_FAILED').'</b>';
-		  		?>
-		  		</td>
-			</tr>
+		  		?></span>
+        </div>
 		</table>
 		<?php endif; ?>
 
