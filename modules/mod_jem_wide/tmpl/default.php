90b75990a4feb249da25a4aab5e060e961e285a1
Hekla Terriol <webstuff@sternenaugen.net>
2024-11-03 00:14:03 +0100

    add microdata to wide module (table layout) #1334

--- modules/mod_jem_wide/tmpl/default.php
+++ modules/mod_jem_wide/tmpl/default.php
@@ -22,32 +22,32 @@ use Joomla\CMS\Uri\Uri;
 	<table class="eventset">
 
 		<colgroup>
-			<col width="30%" class="jemmodw_col_title" />
-			<col width="20%" class="jemmodw_col_category" />
-			<col width="20%" class="jemmodw_col_venue" />
-			<col width="15%" class="jemmodw_col_eventimage" />
-			<col width="15%" class="jemmodw_col_venueimage" />
+			<col style="width:30%" class="jemmodw_col_title" />
+			<col style="width:20%" class="jemmodw_col_category" />
+			<col style="width:20%" class="jemmodw_col_venue" />
+			<col style="width:15%" class="jemmodw_col_eventimage" />
+			<col style="width:15%" class="jemmodw_col_venueimage" />
 		</colgroup>
 
 		<?php foreach ($list as $item) : ?>
-		<tr class="event_id<?php echo $item->eventid; ?>">
-			<td valign="top">
+		<tr class="event_id<?php echo $item->eventid; ?>" itemprop="event" itemscope itemtype="https://schema.org/Event">
+			<td style="vertical-align: top;">
 				<?php if ($item->eventlink) : ?>
-				<span class="event-title">
-					<a href="<?php echo $item->eventlink; ?>" title="<?php echo $item->fulltitle; ?>"><?php echo $item->title; ?></a>
+				<span class="event-title" itemprop="name">
+					<a href="<?php echo $item->eventlink; ?>" itemprop="url" title="<?php echo $item->fulltitle; ?>"><?php echo $item->title; ?></a>
 				</span>
 				<?php else : ?>
-				<span class="event-title">
+				<span class="event-title" itemprop="name">
 					<?php echo $item->title; ?>
 				</span>
 				<?php endif; ?>
 				<br />
 				<span class="date" title="<?php echo strip_tags($item->dateinfo); ?>"><?php echo $item->date; ?></span>
-				<?php
-				if ($item->time && $params->get('datemethod', 1) == 1) :
+				<?php if ($item->time && $params->get('datemethod', 1) == 1) :
 				?>
 				<span class="time" title="<?php echo strip_tags($item->dateinfo); ?>"><?php echo $item->time; ?></span>
-				<?php endif; ?>
+				<?php endif;
+				echo $item->dateschema; ?>
 			</td>
 
 			<td>
@@ -56,17 +56,23 @@ use Joomla\CMS\Uri\Uri;
 			<?php endif; ?>
 			</td>
 
-			<td>
+			<td itemprop="location" itemscope itemtype="https://schema.org/Place">
 			<?php if (!empty($item->venue)) : ?>
 				<?php if ($item->venuelink) : ?>
-				<span class="venue-title"><a href="<?php echo $item->venuelink; ?>" title="<?php echo $item->venue; ?>"><?php echo $item->venue; ?></a></span>
+				<span class="venue-title" itemprop="name"><a href="<?php echo $item->venuelink; ?>" title="<?php echo $item->venue; ?>" itemprop="url"><?php echo $item->venue; ?></a></span>
 				<?php else : ?>
-				<span class="venue-title"><?php echo $item->venue; ?></span>
+				<span class="venue-title" itemprop="name"><?php echo $item->venue; ?></span>
 				<?php endif; ?>
+				<div class="address" itemprop="address" itemscope itemtype="https://schema.org/PostalAddress" style="display:none;">
+                  	<meta itemprop="streetAddress" content="<?php echo $item->street; ?>" />
+                  	<meta itemprop="addressLocality" content="<?php echo $item->city; ?>" />
+                  	<meta itemprop="addressRegion" content="<?php echo $item->state; ?>" />
+                  	<meta itemprop="postalCode" content="<?php echo $item->postalCode; ?>" />
+                </div>
 			<?php endif; ?>
 			</td>
 
-			<td align="center" class="event-image-cell">
+			<td class="event-image-cell">
 				<?php if ($params->get('use_modal')) : ?>
 					<?php if ($item->eventimageorig) {
 						$image = $item->eventimageorig;
@@ -83,13 +89,13 @@ use Joomla\CMS\Uri\Uri;
 				
 				<a href="<?php echo $image; ?>" class="flyermodal" rel="lightbox" data-lightbox="wide-flyerimage-<?php echo $item->eventid ?>"  data-title="<?php echo Text::_('COM_JEM_EVENT') .': ' . $item->title; ?>">
 				<?php endif; ?>
-                <img src="<?php echo $item->eventimage; ?>" alt="<?php echo $item->title; ?>" class="image-preview" title="<?php echo Text::_('COM_JEM_CLICK_TO_ENLARGE'); ?>" />
+                <img src="<?php echo $item->eventimage; ?>" alt="<?php echo $item->title; ?>" class="image-preview" title="<?php echo Text::_('COM_JEM_CLICK_TO_ENLARGE'); ?>" itemprop="image" />
               <?php if ($params->get('use_modal')) : ?>
 				</a>
 				<?php endif; ?>
 			</td>
 
-			<td align="center" class="event-image-cell">
+			<td class="event-image-cell">
 				<?php if ($params->get('use_modal')) : ?>
 				 <a href="<?php echo $item->venueimageorig; ?>" class="flyermodal" rel="lightbox" data-lightbox="wide-flyerimage-<?php echo $item->eventid ?>" title="<?php echo $item->venue; ?>" data-title="<?php echo Text::_('COM_JEM_VENUE') .': ' . $item->venue; ?>">
 				<?php endif; ?>
