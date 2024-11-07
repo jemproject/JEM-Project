e1b32ca237284d941819e39e3ef39f11e2e58f87
McKillo <mckillo@hotmail.com>
2024-11-07 05:57:01 +0100

    Fix convert int to datetime in 4.3.1.sql #1850

--- admin/sql/updates/mysql/4.3.1.sql
+++ admin/sql/updates/mysql/4.3.1.sql
@@ -10,7 +10,7 @@ ALTER TABLE `#__jem_events` CHANGE `unregistra_until` `unregistra_until` INT(11)
 UPDATE `#__jem_events` SET unregistra_until = NULL WHERE unregistra_until = 0;
 ALTER TABLE `#__jem_events` CHANGE `unregistra_until` `unregistra_until` VARCHAR(20) NULL;
 UPDATE `#__jem_events` SET unregistra_until = NULL WHERE unregistra_until != 0 AND times IS NULL OR dates IS NULL;
-UPDATE `#__jem_events` SET unregistra_until = CONCAT(dates, ' ', TIME_FORMAT(SUBTIME(times, SEC_TO_TIME(unregistra_until * 3600)), '%H:%i:%s')) WHERE unregistra_until != 0 AND times IS NOT NULL AND dates IS NOT NULL;
+UPDATE `#__jem_events` SET unregistra_until = DATE_FORMAT(DATE_SUB(CONCAT(dates, ' ', times), INTERVAL unregistra_until HOUR),'%Y-%m-%d %H:%i:%s') WHERE unregistra_until != 0 AND times IS NOT NULL AND dates IS NOT NULL;
 ALTER TABLE `#__jem_events` CHANGE `unregistra_until` `unregistra_until` datetime DEFAULT NULL;
 
 -- update values
