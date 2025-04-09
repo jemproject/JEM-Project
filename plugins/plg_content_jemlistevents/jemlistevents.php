c605ea9c637cae14ad2ccc0eb1618d5495cf1c0e
Hekla Terriol <webstuff@sternenaugen.net>
2025-04-09 17:48:02 +0200

    Update jemlistevents.php

--- plugins/plg_content_jemlistevents/jemlistevents.php
+++ plugins/plg_content_jemlistevents/jemlistevents.php
@@ -79,7 +79,7 @@ class PlgContentJemlistevents extends CMSPlugin
      */
     protected static $tokenValues = array(    // {jemlistevents
             'type'        => array('today', 'unfinished', 'upcoming', 'ongoing', 'archived', 'newest', 'open', 'all'),
-            'featured'    => array('1', '0', '2'),
+            'featured'    => array('0', '1', '2'),
             'title'       => array('on', 'link', 'off'),
             'date'        => array('on', 'link', 'off'),
             'time'        => array('on', 'off'),
