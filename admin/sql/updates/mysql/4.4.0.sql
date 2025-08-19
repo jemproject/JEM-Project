07a076122adf32bfe7d709fb9f81b8844c24caa3
McKillo <mckillo@hotmail.com>
2025-08-19 11:10:34 +0200

    Create table _jem_types

---
+++ admin/sql/updates/mysql/4.4.0.sql
@@ -0,0 +1,22 @@
+-- delete values
+
+-- new values
+INSERT INTO `#__jem_config` (`keyname`, `value`, `access`) VALUES ('access_level_locked_events', '[\"1\"]', '0');
+INSERT INTO `#__jem_config` (`keyname`, `value`, `access`) VALUES ('access_level_locked_venues', '[\"1\"]', '0');
+INSERT INTO `#__jem_config` (`keyname`, `value`, `access`) VALUES ('access_level_locked_categories', '[\"1\"]', '0');
+
+CREATE TABLE IF NOT EXISTS `#__jem_types` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL,
+    `type` TINYINT(1) NOT NULL COMMENT '1 = Event, 2 = Category, 3 = Venue',
+    `icon` VARCHAR(255) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
+
+-- change values
+
+-- update values
+
+
+
+
