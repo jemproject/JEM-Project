01c23572357d1434352dddee9bbeed13d2cc1cce
McKillo <mckillo@hotmail.com>
2025-10-31 00:21:03 +0100

    Update settings.php
    Add JEM map module in the list "$known_extensions"

--- admin/models/settings.php
+++ admin/models/settings.php
@@ -220,7 +220,7 @@ class JemModelSettings extends AdminModel
         $extensions = $db->loadObjectList('name');
 
         $known_extensions = array('pkg_jem', 'com_jem', 'mod_jem', 'mod_jem_cal', 'mod_jem_calajax',
-                                  'mod_jem_banner', 'mod_jem_jubilee', 'mod_jem_teaser', 'mod_jem_wide',
+                                  'mod_jem_banner', 'mod_jem_jubilee', 'mod_jem_teaser', 'mod_jem_wide', 'mod_jem_map',
                                   'plg_content_jem', 'plg_content_jemlistevents', 'plg_content_jemembed',
                                   'plg_finder_jem', 'plg_search_jem',
                                   'plg_quickicon_jem', 'Quick Icon - JEM',
