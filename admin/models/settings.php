c0ed62320d95bf4e795be494b88a29c5d4fcddd7
Hekla Terriol <webstuff@sternenaugen.net>
2026-02-07 10:23:46 +0100

    Update settings.php

--- admin/models/settings.php
+++ admin/models/settings.php
@@ -212,8 +212,8 @@ class JemModelSettings extends AdminModel
 
         $known_extensions = [
             'pkg_jem', 'com_jem', 'mod_jem', 'mod_jem_cal', 'mod_jem_calajax',
-            'mod_jem_banner', 'mod_jem_jubilee', 'mod_jem_teaser', 'mod_jem_wide',
-            'plg_content_jem', 'plg_content_jemlistevents',
+            'mod_jem_banner', 'mod_jem_jubilee', 'mod_jem_teaser', 'mod_jem_wide', 'mod_jem_map',
+            'plg_content_jem', 'plg_content_jemlistevents', 'plg_content_jemembed',
             'plg_finder_jem', 'plg_search_jem',
             'plg_quickicon_jem', 'Quick Icon - JEM',
             'plg_jem_comments', 'plg_jem_mailer', 'plg_jem_demo'
