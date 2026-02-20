1558779a38caaf45a92182507d513a59d01b7141
McKillo <mckillo@hotmail.com>
2026-02-21 00:57:50 +0100

    New Venue Color in Teaser module (default layout) #2090

--- modules/mod_jem_teaser/tmpl/default.php
+++ modules/mod_jem_teaser/tmpl/default.php
@@ -24,15 +24,32 @@ if ($params->get('use_modal', 0)) {
 <div class="jemmoduleteaser<?php echo $params->get('moduleclass_sfx'); ?>" id="jemmoduleteaser">
     <div class="eventset" >
         <?php if (count($list)) : ?>
+            <?php
+            $titletag = '<h2 class="event-title" itemprop="name">';
+            $titleendtag = '</h2>';
+            if ($module->showtitle) {
+                $titletag = '<h3 class="event-title" itemprop="name">';
+                $titleendtag = '</h3>';
+            }
+            ?>
             <?php foreach ($list as $item) : ?>
+                <?php
+                // has user access
+                $eventaccess = '';
+                if (!$item->user_has_access_venue) {
+                    // show a closed lock icon
+                    $eventaccess = ' <span class="icon-lock jem-lockicon" aria-hidden="true"></span>';
+                }
+                ?>
                 <div class="event_id<?php echo $item->eventid; ?>" itemprop="event" itemscope itemtype="https://schema.org/Event">
-                    <h2 class="event-title" itemprop="name">
-                        <?php if ($item->eventlink) : ?>
-                            <a href="<?php echo $item->eventlink; ?>" title="<?php echo $item->fulltitle; ?>" itemprop="url"><?php echo $item->title; ?></a>
-                        <?php else : ?>
-                            <?php echo $item->title; ?>
-                        <?php endif; ?>
-                    </h2>
+                    <?php echo $titletag; ?>
+                    <?php if ($item->eventlink) : ?>
+                        <a href="<?php echo $item->eventlink; ?>" title="<?php echo $item->fulltitle; ?>" itemprop="url"><?php echo $item->title; ?></a>
+                    <?php else : ?>
+                        <?php echo $item->title; ?>
+                    <?php endif; ?>
+                    <?php echo $eventaccess; ?>
+                    <?php echo $titleendtag; ?>
 
                     <table>
                         <tr>
@@ -115,20 +132,23 @@ if ($params->get('use_modal', 0)) {
                                     </div>
                                 </div>
                             </td>
-                            <td class="event-vencat">
-                                <div class="block-event-vencat">
-                                    <?php if (!empty($item->venuename)) : ?>
-                                        <div class="venue">
-                                            <?php echo $item->venuename; ?>
-                                        </div>
+                            <?php if ($item->user_has_access_venue) : ?>
+                                <td class="event-vencat" style="display: flex; flex-wrap: wrap; gap: 8px; align-items: center;">
+                                    <?php if (!empty($item->venue)) : ?>
+                                        <?php if (!JemHelper::jemStringContains($params->get('moduleclass_sfx'), 'jem-novenue')) : ?>
+                                            <div class="venue-title" title="<?php echo Text::_('COM_JEM_TABLE_LOCATION').': '.strip_tags($item->venue); ?>">
+                                                <?php echo $item->venuename; ?>
+                                            </div>
+                                        <?php endif; ?>
                                     <?php endif; ?>
-                                    <?php if (!empty($item->catname)) : ?>
-                                        <div class="category">
+
+                                    <?php if (!JemHelper::jemStringContains($params->get('moduleclass_sfx'), 'jem-nocats')) : ?>
+                                        <div class="category" title="<?php echo Text::_('COM_JEM_TABLE_CATEGORY').': '.strip_tags($item->catname); ?>">
                                             <?php echo $item->catname; ?>
                                         </div>
                                     <?php endif; ?>
-                                </div>
-                            </td>
+                                </td>
+                            <?php endif; ?>
                         </tr>
                     </table>
                 </div>
