0de9c787b26d16b8bb1ce75793893803b312ceff
Hekla Terriol <webstuff@sternenaugen.net>
2026-03-01 00:47:59 +0100

    fix db table row order (J6L) #2096

---
+++ admin/sql/updates/mysql/4.4.2.sql
@@ -0,0 +1,10 @@
+-- delete values
+
+-- new values
+
+-- change values
+
+-- update values
+
+-- update row odrder
+ALTER TABLE `#__jem_events` MODIFY COLUMN `fulltext` mediumtext NOT NULL AFTER `introtext`;
