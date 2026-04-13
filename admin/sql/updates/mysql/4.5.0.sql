c1b5342cf47ab86edd405446c616881ec8d2b735
McKillo <mckillo@hotmail.com>
2026-04-13 09:34:44 +0100

    Migrate legacy JEM menu params to 4.5.0 format #2121
    Updates ''tablefiltereventuntil' in the #__menu table by replacing "0" values with empty strings.

--- admin/sql/updates/mysql/4.5.0.sql
+++ admin/sql/updates/mysql/4.5.0.sql
@@ -11,3 +11,4 @@ ALTER TABLE `#__jem_events` MODIFY `contactid` VARCHAR(100);
 
 -- update values
 UPDATE `#__jem_events` SET `contactid` = '' WHERE `contactid` = 0;
+UPDATE `#__menu` SET `params` = REPLACE(`params`, '"tablefiltereventuntil":"0"', '"tablefiltereventuntil":""') WHERE `link` LIKE '%com_jem&view=eventslist%' AND `params` LIKE '%"tablefiltereventuntil":"0"%';
