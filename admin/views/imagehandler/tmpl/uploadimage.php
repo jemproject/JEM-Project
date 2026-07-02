c80b36c356051505e7ce5c0038b78821c105aa22
McKillo <mckillo@hotmail.com>
2026-07-02 16:08:00 +0200

    pkg_jem_v5.0.0rc4.zip

--- admin/views/imagehandler/tmpl/uploadimage.php
+++ admin/views/imagehandler/tmpl/uploadimage.php
@@ -10,143 +10,207 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\HTML\HTMLHelper;
+
+$targetDirectory = '/images/jem/events/';
+
+if ($this->task == 'venueimg') {
+    $targetDirectory = '/images/jem/venues/';
+    $this->task = 'imagehandler.venueimgup';
+} else if ($this->task == 'eventimg') {
+    $targetDirectory = '/images/jem/events/';
+    $this->task = 'imagehandler.eventimgup';
+} else if ($this->task == 'categoriesimg') {
+    $targetDirectory = '/images/jem/categories/';
+    $this->task = 'imagehandler.categoriesimgup';
+}
+
+$imageTypes = [
+    ['supported' => ($this->jemsettings->gddisabled == 0 || (imagetypes() & IMG_PNG)), 'label' => Text::_('COM_JEM_PNG_SUPPORT'), 'missing' => Text::_('COM_JEM_NO_PNG_SUPPORT')],
+    ['supported' => ($this->jemsettings->gddisabled == 0 || (imagetypes() & IMG_JPEG)), 'label' => Text::_('COM_JEM_JPG_SUPPORT'), 'missing' => Text::_('COM_JEM_NO_JPG_SUPPORT')],
+    ['supported' => ($this->jemsettings->gddisabled == 0 || (imagetypes() & IMG_GIF)), 'label' => Text::_('COM_JEM_GIF_SUPPORT'), 'missing' => Text::_('COM_JEM_NO_GIF_SUPPORT')],
+    ['supported' => ($this->jemsettings->gddisabled == 0 || (imagetypes() & IMG_WEBP)), 'label' => Text::_('COM_JEM_WEBP_SUPPORT'), 'missing' => Text::_('COM_JEM_NO_WEBP_SUPPORT')],
+];
 ?>
+
+<style>
+    #jem.jem-image-upload {
+        padding: 1.5rem;
+    }
+
+    #jem.jem-image-upload .jem-upload-panel {
+        display: grid;
+        grid-template-columns: minmax(18rem, 1fr) minmax(18rem, 0.9fr);
+        gap: 1rem;
+        align-items: stretch;
+        max-width: 58rem;
+    }
+
+    #jem.jem-image-upload .jem-upload-card {
+        border: 1px solid #ccd6e3;
+        background: #fff;
+        padding: 1rem;
+    }
+
+    #jem.jem-image-upload .jem-upload-card h2 {
+        margin: 0 0 0.75rem;
+        font-size: 1.1rem;
+        line-height: 1.25;
+    }
+
+    #jem.jem-image-upload .jem-upload-file {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 0.75rem;
+        align-items: center;
+    }
+
+    #jem.jem-image-upload input[type="file"] {
+        max-width: 100%;
+    }
+
+    #jem.jem-image-upload .jem-upload-actions {
+        margin-top: 1rem;
+    }
+
+    #jem.jem-image-upload .jem-upload-meta {
+        display: grid;
+        grid-template-columns: max-content 1fr;
+        gap: 0.45rem 0.75rem;
+        margin: 0 0 1rem;
+    }
+
+    #jem.jem-image-upload .jem-upload-meta dt {
+        font-weight: 700;
+    }
+
+    #jem.jem-image-upload .jem-upload-meta dd {
+        margin: 0;
+    }
+
+    #jem.jem-image-upload .jem-upload-format-list {
+        display: grid;
+        gap: 0.35rem;
+        margin: 0;
+        padding: 0;
+        list-style: none;
+    }
+
+    #jem.jem-image-upload .jem-upload-format-list li {
+        display: flex;
+        gap: 0.45rem;
+        align-items: center;
+    }
+
+    #jem.jem-image-upload .jem-upload-format-ok {
+        color: #2f7d32;
+    }
+
+    #jem.jem-image-upload .jem-upload-format-error {
+        color: #b42318;
+    }
+
+    #jem.jem-image-upload .jem-upload-note {
+        max-width: 58rem;
+        margin-top: 1rem;
+        border-left: 4px solid #5579b7;
+        background: #f4f7fb;
+        padding: 0.8rem 1rem;
+        color: #344054;
+    }
+
+    #jem.jem-image-upload .jem-upload-note strong {
+        display: block;
+        margin-bottom: 0.25rem;
+    }
+
+    @media (max-width: 720px) {
+        #jem.jem-image-upload {
+            padding: 1rem;
+        }
+
+        #jem.jem-image-upload .jem-upload-panel {
+            grid-template-columns: 1fr;
+        }
+
+        #jem.jem-image-upload .jem-upload-meta {
+            grid-template-columns: 1fr;
+        }
+    }
+</style>
 
 <form method="post" action="<?php echo htmlspecialchars($this->request_url); ?>" enctype="multipart/form-data" name="adminForm" id="adminForm">
 
-<table class="noshow">
-    <tr>
-        <td style="width: 50%; vertical-align: top;">
+<div id="jem" class="jem-image-upload">
+    <?php if($this->ftp): ?>
+        <fieldset class="adminform">
+            <legend><?php echo Text::_('COM_JEM_FTP_TITLE'); ?></legend>
 
-            <?php if($this->ftp): ?>
-                <fieldset class="adminform">
-                    <legend><?php echo Text::_('COM_JEM_FTP_TITLE'); ?></legend>
+            <?php echo Text::_('COM_JEM_FTP_DESC'); ?>
 
-                    <?php echo Text::_('COM_JEM_FTP_DESC'); ?>
-
-                    <?php if($this->ftp INSTANCEOF Exception): ?>
-                        <p><?php echo Text::_($this->ftp->message); ?></p>
-                    <?php endif; ?>
-
-                    <table class="adminform nospace">
-                        <tbody>
-                            <tr>
-                                <td style="width: 120px">
-                                    <label for="username"><?php echo Text::_('COM_JEM_USERNAME'); ?>:</label>
-                                </td>
-                                <td>
-                                    <input type="text" id="username" name="username" class="input_box" size="70" value="" />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td style="width: 120px">
-                                    <label for="password"><?php echo Text::_('COM_JEM_PASSWORD'); ?>:</label>
-                                </td>
-                                <td>
-                                    <input type="password" id="password" name="password" class="input_box" size="70" value="" />
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </fieldset>
+            <?php if($this->ftp INSTANCEOF Exception): ?>
+                <p><?php echo Text::_($this->ftp->message); ?></p>
             <?php endif; ?>
 
-            <fieldset class="adminform">
-                <legend><?php echo Text::_('COM_JEM_SELECT_IMAGE_UPLOAD'); ?></legend>
-                <table class="admintable">
-                    <tbody>
-                        <tr>
-                            <td>
-                                <input class="inputbox" name="userfile" id="userfile" type="file" />
-                                <br><br>
-                                <input class="btn btn-primary" type="submit" value="<?php echo Text::_('COM_JEM_UPLOAD') ?>" name="adminForm" />
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </fieldset>
+            <table class="adminform nospace">
+                <tbody>
+                    <tr>
+                        <td style="width: 120px">
+                            <label for="username"><?php echo Text::_('COM_JEM_USERNAME'); ?>:</label>
+                        </td>
+                        <td>
+                            <input type="text" id="username" name="username" class="input_box" size="70" value="" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="width: 120px">
+                            <label for="password"><?php echo Text::_('COM_JEM_PASSWORD'); ?>:</label>
+                        </td>
+                        <td>
+                            <input type="password" id="password" name="password" class="input_box" size="70" value="" />
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </fieldset>
+    <?php endif; ?>
 
-        </td>
-        <td style="width: 50%; vertical-align: top;">
+    <div class="jem-upload-panel">
+        <section class="jem-upload-card">
+            <h2><?php echo Text::_('COM_JEM_SELECT_IMAGE_UPLOAD'); ?></h2>
+            <div class="jem-upload-file">
+                <input class="inputbox" name="userfile" id="userfile" type="file" />
+            </div>
+            <div class="jem-upload-actions">
+                <input class="btn btn-primary" type="submit" value="<?php echo Text::_('COM_JEM_UPLOAD') ?>" name="adminForm" />
+            </div>
+        </section>
 
-            <fieldset class="adminform">
-                <legend><?php echo Text::_('COM_JEM_ATTENTION'); ?></legend>
-                <table class="admintable">
-                    <tbody>
-                        <tr>
-                            <td>
-                                <b><?php echo Text::_('COM_JEM_TARGET_DIRECTORY').':'; ?></b>
-                                <?php
-                                if($this->task == 'venueimg') {
-                                    echo "/images/jem/venues/";
-                                    $this->task = 'imagehandler.venueimgup';
-                                } else if($this->task == 'eventimg') {
-                                    echo "/images/jem/events/";
-                                    $this->task = 'imagehandler.eventimgup';
-                                } else if($this->task == 'categoriesimg') {
-                                    echo "/images/jem/categories/";
-                                    $this->task = 'imagehandler.categoriesimgup';
-                                }
-                                ?>
-                                <br>
-                                <b><?php echo Text::_('COM_JEM_IMAGE_FILESIZE').':'; ?></b> <?php echo $this->jemsettings->sizelimit; ?> kb<br>
+        <section class="jem-upload-card">
+            <h2><?php echo Text::_('COM_JEM_UPLOAD_IMAGE'); ?></h2>
+            <dl class="jem-upload-meta">
+                <dt><?php echo Text::_('COM_JEM_TARGET_DIRECTORY'); ?></dt>
+                <dd><?php echo $targetDirectory; ?></dd>
+                <dt><?php echo Text::_('COM_JEM_IMAGE_FILESIZE'); ?></dt>
+                <dd><?php echo (int) $this->jemsettings->sizelimit; ?> kb</dd>
+            </dl>
+            <ul class="jem-upload-format-list">
+                <?php foreach ($imageTypes as $type): ?>
+                    <li class="<?php echo $type['supported'] ? 'jem-upload-format-ok' : 'jem-upload-format-error'; ?>">
+                        <span aria-hidden="true"><?php echo $type['supported'] ? '&#10003;' : '&#10007;'; ?></span>
+                        <span><?php echo $type['supported'] ? $type['label'] : $type['missing']; ?></span>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </section>
+    </div>
 
-                                <?php
-                                if($this->jemsettings->gddisabled == 0 || (imagetypes() & IMG_PNG)) {
-                                    echo "<br><span style='color:green'>".Text::_('COM_JEM_PNG_SUPPORT')."</span>";
-                                } else {
-                                    echo "<br><span style='color:red'>".Text::_('COM_JEM_NO_PNG_SUPPORT')."</span>";
-                                }
-                                if($this->jemsettings->gddisabled == 0 || (imagetypes() & IMG_JPEG)) {
-                                    echo "<br><span style='color:green'>".Text::_('COM_JEM_JPG_SUPPORT')."</span>";
-                                } else {
-                                    echo "<br><span style='color:red'>".Text::_('COM_JEM_NO_JPG_SUPPORT')."</span>";
-                                }
-                                if($this->jemsettings->gddisabled == 0 || (imagetypes() & IMG_GIF)) {
-                                    echo "<br><span style='color:green'>".Text::_('COM_JEM_GIF_SUPPORT')."</span>";
-                                } else {
-                                    echo "<br><span style='color:red'>".Text::_('COM_JEM_NO_GIF_SUPPORT')."</span>";
-                                }
-                                if($this->jemsettings->gddisabled == 0 || (imagetypes() & IMG_WEBP)) {
-                                    echo "<br><span style='color:green'>".Text::_('COM_JEM_WEBP_SUPPORT')."</span>";
-                                } else {
-                                    echo "<br><span style='color:red'>".Text::_('COM_JEM_NO_WEBP_SUPPORT')."</span>";
-                                }
-                                ?>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </fieldset>
-
-        </td>
-    </tr>
-</table>
-
-<?php if($this->jemsettings->gddisabled) { ?>
-
-<table class="noshow">
-    <tr>
-        <td>
-
-            <fieldset class="adminform">
-                <legend><?php echo Text::_('COM_JEM_ATTENTION'); ?></legend>
-                <table class="admintable">
-                    <tbody>
-                        <tr>
-                            <td class="center">
-                                <?php echo Text::_('COM_JEM_GD_WARNING'); ?>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </fieldset>
-
-        </td>
-    </tr>
-</table>
-
-<?php } ?>
+    <?php if($this->jemsettings->gddisabled) : ?>
+        <div class="jem-upload-note">
+            <strong><?php echo Text::_('COM_JEM_ATTENTION'); ?></strong>
+            <?php echo Text::_('COM_JEM_GD_WARNING'); ?>
+        </div>
+    <?php endif; ?>
+</div>
 
 <?php echo HTMLHelper::_('form.token'); ?>
 <input type="hidden" name="option" value="com_jem" />
---
+++ scripts/build-packages.php
@@ -0,0 +1,306 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Build JEM package ZIP files without requiring Ant.
+ *
+ * Usage:
+ *   php scripts/build-packages.php
+ *   php scripts/build-packages.php C:/GitHub/JEM-Project C:/GitHub/JEM-Project-4.5
+ */
+
+final class JemPackageBuilder
+{
+    private const MODULES = [
+        'mod_jem',
+        'mod_jem_banner',
+        'mod_jem_cal',
+        'mod_jem_jubilee',
+        'mod_jem_teaser',
+        'mod_jem_wide',
+        'mod_jem_map',
+        'mod_jem_types',
+    ];
+
+    private const PLUGINS = [
+        'plugins/plg_content_jemembed'     => 'plg_content_jemembed.zip',
+        'plugins/plg_content_jemlistevents' => 'plg_content_jemlistevents.zip',
+        'plugins/plg_finder_jem'           => 'plg_finder_jem.zip',
+        'plugins/plg_jem_comments'         => 'plg_jem_comments.zip',
+        'plugins/plg_jem_mailer'           => 'plg_jem_mailer.zip',
+        'plugins/plg_quickicon_jem'        => 'plg_quickicon_jem.zip',
+        'plugins/plg_actionlog_jem'        => 'plg_actionlog_jem.zip',
+    ];
+
+    public function build(string $root): string
+    {
+        $root = $this->normalizeRoot($root);
+        $version = $this->readPackageVersion($root);
+        $buildDir = $this->newBuildDir($root);
+
+        try {
+            $componentZip = $buildDir . '/components/com_jem.zip';
+            $this->ensureDir(dirname($componentZip));
+            $this->zipDirectory($root, $componentZip, fn (string $relative): bool => $this->includeComponentEntry($relative));
+
+            $this->ensureDir($buildDir . '/modules');
+            foreach (self::MODULES as $module) {
+                $source = $root . '/modules/' . $module;
+                if (is_dir($source)) {
+                    $this->zipDirectory($source, $buildDir . '/modules/' . $module . '.zip');
+                }
+            }
+
+            $this->ensureDir($buildDir . '/plugins');
+            foreach (self::PLUGINS as $sourceRelative => $zipName) {
+                $source = $root . '/' . $sourceRelative;
+                if (is_dir($source)) {
+                    $this->zipDirectory($source, $buildDir . '/plugins/' . $zipName);
+                }
+            }
+
+            $package = $buildDir . '/pkg_jem_v' . $version . '.zip';
+            $this->zipPackage($root, $buildDir, $package);
+            $this->validate($package);
+
+            $target = $root . '/pkg_jem_v' . $version . '.zip';
+            if (is_file($target)) {
+                unlink($target);
+            }
+            if (!rename($package, $target)) {
+                throw new RuntimeException('Could not move package to ' . $target);
+            }
+
+            return $target;
+        } finally {
+            $this->removeDir($buildDir);
+        }
+    }
+
+    private function normalizeRoot(string $root): string
+    {
+        $real = realpath($root);
+        if ($real === false || !is_dir($real)) {
+            throw new RuntimeException('Invalid project root: ' . $root);
+        }
+
+        return rtrim(str_replace('\\', '/', $real), '/');
+    }
+
+    private function readPackageVersion(string $root): string
+    {
+        $manifest = $root . '/package/pkg_jem.xml';
+        if (!is_file($manifest)) {
+            throw new RuntimeException('Missing package manifest: ' . $manifest);
+        }
+
+        $xml = simplexml_load_file($manifest);
+        if (!$xml || empty($xml->version)) {
+            throw new RuntimeException('Could not read package version from ' . $manifest);
+        }
+
+        return trim((string) $xml->version);
+    }
+
+    private function newBuildDir(string $root): string
+    {
+        $base = rtrim(str_replace('\\', '/', sys_get_temp_dir()), '/') . '/jem-package-build-' . md5($root);
+        $this->removeDir($base);
+        $this->ensureDir($base);
+
+        return $base;
+    }
+
+    private function zipPackage(string $root, string $buildDir, string $target): void
+    {
+        $zip = $this->openZip($target);
+
+        foreach (glob($buildDir . '/components/*.zip') ?: [] as $file) {
+            $zip->addFile($file, 'packages/' . basename($file));
+        }
+        foreach (glob($buildDir . '/modules/*.zip') ?: [] as $file) {
+            $zip->addFile($file, 'packages/' . basename($file));
+        }
+        foreach (glob($buildDir . '/plugins/*.zip') ?: [] as $file) {
+            $zip->addFile($file, 'packages/' . basename($file));
+        }
+
+        $this->addDirectoryToZip($zip, $root . '/package', '', fn (string $relative): bool => $this->includeCommonEntry($relative));
+        $zip->close();
+    }
+
+    private function zipDirectory(string $source, string $target, ?callable $include = null): void
+    {
+        $zip = $this->openZip($target);
+        $this->addDirectoryToZip($zip, $source, '', $include ?? fn (string $relative): bool => $this->includeCommonEntry($relative));
+        $zip->close();
+    }
+
+    private function addDirectoryToZip(ZipArchive $zip, string $source, string $prefix, callable $include): void
+    {
+        $source = rtrim(str_replace('\\', '/', realpath($source) ?: $source), '/');
+        $iterator = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator($source, FilesystemIterator::SKIP_DOTS),
+            RecursiveIteratorIterator::SELF_FIRST
+        );
+
+        foreach ($iterator as $fileInfo) {
+            $path = str_replace('\\', '/', $fileInfo->getPathname());
+            $relative = ltrim(substr($path, strlen($source)), '/');
+            if ($relative === '' || !$include($relative)) {
+                continue;
+            }
+
+            $entry = ltrim($prefix . '/' . $relative, '/');
+            if ($fileInfo->isDir()) {
+                $zip->addEmptyDir($entry);
+            } else {
+                $zip->addFile($path, $entry);
+            }
+        }
+    }
+
+    private function includeCommonEntry(string $relative): bool
+    {
+        $relative = str_replace('\\', '/', $relative);
+        $basename = basename($relative);
+
+        if ($basename === '' || $basename === '.' || str_starts_with($basename, '.tmp-')) {
+            return false;
+        }
+
+        return !preg_match('#(^|/)(\.DS_Store|Thumbs\.db|desktop\.ini|.*\.(bak|orig|log|tmp)|.*~)$#i', $relative);
+    }
+
+    private function includeComponentEntry(string $relative): bool
+    {
+        $relative = str_replace('\\', '/', $relative);
+        $basename = basename($relative);
+
+        if (!$this->includeCommonEntry($relative)) {
+            return false;
+        }
+
+        if ($basename === 'CODEX_PENDING_DIARY.md' || $basename === 'import-catalog.xml') {
+            return false;
+        }
+
+        if (preg_match('#^(\.git|\.settings|\.tmp|\.phpunit\.cache|\.agents|\.claude|\.codex|\.cursor|\.github/copilot|3rd|build|docs|modules|package|plugins|scripts|tests|tools|vendor|_old[^/]*|old[^/]*)(/|$)#', $relative)) {
+            return false;
+        }
+
+        return !preg_match('#(^|/)(pkg_jem_v.*\.zip.*|update_pkg_.*\.xml|composer\.(json|lock)|phpunit(\.progress)?\.xml(\.dist)?|build\..*|\.env(\..*)?|.*\.(pem|key|crt|pfx)|.*\.code-workspace)$#', $relative);
+    }
+
+    private function validate(string $package): void
+    {
+        $outer = new ZipArchive();
+        if ($outer->open($package) !== true) {
+            throw new RuntimeException('Could not validate package: ' . $package);
+        }
+
+        foreach (['pkg_jem.xml', 'pkg_install.php', 'packages/com_jem.zip', 'packages/mod_jem_types.zip', 'packages/plg_actionlog_jem.zip'] as $entry) {
+            if ($outer->locateName($entry) === false) {
+                throw new RuntimeException($package . ' is missing ' . $entry);
+            }
+        }
+
+        $componentData = $outer->getFromName('packages/com_jem.zip');
+        $outer->close();
+
+        if ($componentData === false) {
+            throw new RuntimeException($package . ' has no component archive');
+        }
+
+        $tmpComponent = tempnam(sys_get_temp_dir(), 'jem_component_');
+        file_put_contents($tmpComponent, $componentData);
+
+        $component = new ZipArchive();
+        if ($component->open($tmpComponent) !== true) {
+            @unlink($tmpComponent);
+            throw new RuntimeException('Could not validate component archive in ' . $package);
+        }
+
+        foreach (['jem.xml', 'script.php', 'admin/jem.php', 'site/jem.php', 'site/classes/icalcreator/autoload.php', 'media/index.html', 'media/vendor/index.html', 'admin/assets/sampledata.zip'] as $entry) {
+            if ($component->locateName($entry) === false) {
+                $component->close();
+                @unlink($tmpComponent);
+                throw new RuntimeException($package . ':packages/com_jem.zip is missing ' . $entry);
+            }
+        }
+
+        foreach (['import-catalog.xml', 'composer.json', 'composer.lock', 'vendor/autoload.php'] as $forbidden) {
+            if ($component->locateName($forbidden) !== false) {
+                $component->close();
+                @unlink($tmpComponent);
+                throw new RuntimeException($package . ':packages/com_jem.zip contains forbidden entry ' . $forbidden);
+            }
+        }
+
+        for ($i = 0; $i < $component->numFiles; $i++) {
+            $name = $component->getNameIndex($i);
+            if (basename($name) !== '' && str_starts_with(basename($name), '.tmp-')) {
+                $component->close();
+                @unlink($tmpComponent);
+                throw new RuntimeException($package . ':packages/com_jem.zip contains temporary entry ' . $name);
+            }
+        }
+
+        $component->close();
+        @unlink($tmpComponent);
+    }
+
+    private function openZip(string $target): ZipArchive
+    {
+        $this->ensureDir(dirname($target));
+        $zip = new ZipArchive();
+        if ($zip->open($target, ZipArchive::CREATE | ZipArchive::OVERWRITE) !== true) {
+            throw new RuntimeException('Could not create ZIP: ' . $target);
+        }
+
+        return $zip;
+    }
+
+    private function ensureDir(string $dir): void
+    {
+        if (!is_dir($dir) && !mkdir($dir, 0777, true) && !is_dir($dir)) {
+            throw new RuntimeException('Could not create directory: ' . $dir);
+        }
+    }
+
+    private function removeDir(string $dir): void
+    {
+        if (!is_dir($dir)) {
+            return;
+        }
+
+        $iterator = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator($dir, FilesystemIterator::SKIP_DOTS),
+            RecursiveIteratorIterator::CHILD_FIRST
+        );
+
+        foreach ($iterator as $fileInfo) {
+            if ($fileInfo->isDir()) {
+                rmdir($fileInfo->getPathname());
+            } else {
+                unlink($fileInfo->getPathname());
+            }
+        }
+
+        rmdir($dir);
+    }
+}
+
+$roots = array_slice($argv, 1);
+if (!$roots) {
+    $roots = [
+        dirname(__DIR__),
+        dirname(__DIR__) . '/../JEM-Project-4.5',
+    ];
+}
+
+$builder = new JemPackageBuilder();
+foreach ($roots as $root) {
+    echo $builder->build($root) . PHP_EOL;
+}
