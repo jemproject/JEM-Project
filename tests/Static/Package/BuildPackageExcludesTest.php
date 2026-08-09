a6e6e7e6df4385cf8d5a90162ea390a1701bfbd9
McKillo <mckillo@hotmail.com>
2026-08-10 00:00:01 +0200

    PKG JEM 5.0.1 beta 2 (Refresh)

--- tests/Static/Package/BuildPackageExcludesTest.php
+++ tests/Static/Package/BuildPackageExcludesTest.php
@@ -24,6 +24,7 @@ final class BuildPackageExcludesTest extends TestCase
 
         foreach (array(
             'tests/**',
+            'tmp/**',
             'vendor/**',
             '.phpunit.cache/**',
             '.agents/**',
