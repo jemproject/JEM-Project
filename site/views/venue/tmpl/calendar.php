a4ce5a5a67c95759110e66c75c51f2c704c7a1f5
McKillo <mckillo@hotmail.com>
2026-09-13 01:27:14 +0200

    More unit testFix undefined category color in calendar views #2346
    - initialize the category color output in Venue Calendar layouts
    - initialize the category color output in the legacy Week Calendar
    - prevent PHP warnings when event categories have no configured color

--- site/views/venue/tmpl/calendar.php
+++ site/views/venue/tmpl/calendar.php
@@ -193,6 +193,7 @@ use Joomla\CMS\Factory;
 
         //initialize variables
         $multicatname = '';
+        $color = '';
         $colorpic = '';
         $nr = is_array($row->categories) ? count($row->categories) : 0;
         $ix = 0;
